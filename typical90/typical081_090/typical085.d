7f7967685988f1a643021c997e2cd53156f61f50
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-11-24 12:03:21 +0000

    solve typical085

---
+++ typical90/typical081_090/typical085.d
@@ -0,0 +1,29 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    long CB = N.to!real.cbrt.floor.to!long;
+
+    long res;
+    foreach (a; 1 .. CB+1) {
+        if (N % a != 0) {
+            continue;
+        }
+
+        long D = N / a;
+
+        foreach (b; a .. D+1) {
+            if (b > (D / b)) {
+                break;
+            }
+
+            if (D % b == 0) {
+                ++res;
+            }
+        }
+    }
+
+    res.writeln;
+}
