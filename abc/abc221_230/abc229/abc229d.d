53162137c46802e9eaefed66809702263072ada9
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-11-27 13:30:08 +0000

    solve abc229d

---
+++ abc/abc221_230/abc229/abc229d.d
@@ -0,0 +1,55 @@
+import std;
+
+void main() {
+    string S = readln.chomp;
+    
+    uint K;
+    readf("%d\n", K);
+
+    uint C = S.count('.').to!uint;
+    if (C <= K) {
+        S.length.writeln;
+        return;
+    }
+
+    auto T = S.group.array;
+    
+    uint res, chg, len;
+    uint[] num, cnt;
+    foreach (t; T) {
+        if (t[0] == 'X') {
+            len += t[1];
+            num ~= t[1];
+            cnt ~= 0;
+        }
+        else {
+            if (K - chg >= t[1]) {
+                chg += t[1];
+                len += t[1];
+                num ~= t[1];
+                cnt ~= t[1];
+            }
+            else {
+                res = max(res, len+K-chg);
+
+                while (!cnt.empty && K - chg < t[1]) {
+                    chg -= cnt.front;
+                    cnt.popFront;
+
+                    len -= num.front;
+                    num.popFront;
+                }
+
+                uint tmp = min(K, t[1]);
+
+                chg += tmp;
+                len += tmp;
+                num ~= tmp;
+                cnt ~= tmp;
+            }
+        }
+    }
+    res = max(res, len);
+
+    res.writeln;
+}
