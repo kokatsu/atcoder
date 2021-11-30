668dd962375efdd577ab0205ce54e835d3fac880
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-11-30 12:06:05 +0000

    solve typical030

---
+++ typical90/typical021_030/typical030.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto factors = new int[](N+1);
+    foreach (i; 2 .. N+1) {
+        if (factors[i] > 0) {
+            continue;
+        }
+
+        foreach (j; iota(i, N+1, i)) {
+            ++factors[j];
+        }
+    }
+
+    int res;
+    foreach (f; factors) {
+        if (f >= K) {
+            ++res;
+        }
+    }
+
+    res.writeln;
+}
