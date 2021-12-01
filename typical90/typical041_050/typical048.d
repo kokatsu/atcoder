ce4b7d9ca4c9afeae50851e561595bf5e9dabb06
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-01 14:32:36 +0000

    solve typical048

---
+++ typical90/typical041_050/typical048.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto list = new long[](N*2);
+    foreach (i; 0 .. N) {
+        long A, B;
+        readf("%d %d\n", A, B);
+
+        list[i*2] = B, list[i*2+1] = A - B;
+    }
+
+    list.sort!"a > b";
+
+    list[0..K].sum.writeln;
+}
