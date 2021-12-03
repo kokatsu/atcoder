ca8a15e84bb540f96faed0540a04849bfbc0704f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-03 12:11:02 +0000

    solve abc230b

---
+++ abc/abc221_230/abc230/abc230b.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    string S = readln.chomp;
+
+    dchar[] l = ['o', 'x', 'x'];
+    
+    ulong len = S.length;
+    auto T = l.cycle.take(13).array;
+
+    bool isOK;
+    foreach (i; 0 .. 3) {
+        if (S == T[i..i+len].to!string) {
+            isOK = true;
+        }
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
