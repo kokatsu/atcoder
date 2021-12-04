3a0e742b13fae3e560b09204fb52ace2b036c135
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-04 14:09:48 +0000

    solve abc182d

---
+++ abc/abc181_190/abc182/abc182d.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    auto B = A.cumulativeFold!"a + b".array;
+    auto C = B.cumulativeFold!max.array;
+
+    long res, pos;
+    foreach (i; 0 .. N) {
+        res = max(res, pos+C[i]);
+        pos += B[i];
+    }
+
+    res.writeln;
+}
