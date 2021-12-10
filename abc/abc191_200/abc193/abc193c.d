d029ba895f9178a8a4b56e216b5c3814bce6c8d8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-10 13:11:55 +0000

    solve abc193c

---
+++ abc/abc191_200/abc193/abc193c.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    long S = N.to!real.sqrt.floor.to!long;
+    
+    bool[long] list;
+    foreach (i; 2 .. S+1) {
+        long num = i;
+        while (num * i <= N) {
+            num *= i;
+            list[num] = true;
+        }
+    }
+
+    long res = N - list.length;
+    res.writeln;
+}
