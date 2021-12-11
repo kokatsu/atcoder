4b320031dda5812f421c55aeacca1b3c1bb89099
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-11 12:30:50 +0000

    solve abc231d

---
+++ abc/abc231_240/abc231/abc231d.d
@@ -0,0 +1,46 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    bool isOK = true;
+    auto list = new int[][](N+1);
+    foreach (_; 0 .. M) {
+        int A, B;
+        readf("%d %d\n", A, B);
+
+        list[A] ~= B;
+        list[B] ~= A;
+
+        if (list[A].length > 2 || list[B].length > 2) {
+            isOK = false;
+        } 
+    }
+
+    auto seen = new bool[](N+1);
+    void dfs(int pos, int pre, ref bool flag) {
+        seen[pos] = true;
+
+        foreach (l; list[pos]) {
+            if (l == pre) {
+                continue;
+            }
+
+            if (seen[l]) {
+                flag = false;
+            }
+            else {
+                dfs(l, pos, flag);
+            }
+        }
+    }
+
+    foreach (i; 1 .. N+1) {
+        if (isOK && !seen[i]) {
+            dfs(i, 0, isOK);
+        }
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
