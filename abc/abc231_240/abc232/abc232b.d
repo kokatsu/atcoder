9cba361484764002f2284523bfbe3cd794970ced
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-19 12:09:11 +0000

    solve abc232b

---
+++ abc/abc231_240/abc232/abc232b.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    string S = readln.chomp;
+    string T = readln.chomp;
+
+    bool isOK;
+    foreach (i; 0 .. 26) {
+        bool flag = true;
+
+        foreach (j, s; S) {
+            dchar d = ((((s - 'a') + i) % 26) + 'a').to!dchar;
+            
+            if (d != T[j]) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag) {
+            isOK = true;
+        }
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
