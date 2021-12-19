c3b32031d279fd9b3e2506e462b3dd7d09e8572c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-19 12:54:00 +0000

    solve abc232c

---
+++ abc/abc231_240/abc232/abc232c.d
@@ -0,0 +1,44 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto P = new bool[][](N, N);
+    foreach (_; 0 .. M) {
+        int A, B;
+        readf("%d %d\n", A, B);
+
+        --A, --B;
+        P[A][B] = P[B][A] = true;
+    }
+
+    auto Q = new bool[][](N, N);
+    foreach (_; 0 .. M) {
+        int C, D;
+        readf("%d %d\n", C, D);
+
+        --C, --D;
+        Q[C][D] = Q[D][C] = true;
+    }
+
+    bool isOK;
+    auto arr = iota(N).array;
+    do {
+        bool flag = true;
+        foreach (i; 0 .. N) {
+            foreach (j; 0 .. N) {
+                if (P[i][j] != Q[arr[i]][arr[j]]) {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+
+        if (flag) {
+            isOK = true;
+        }
+    } while (arr.nextPermutation);
+
+    writeln(isOK ? "Yes" : "No");
+}
