982628e360d78f3c8ddb612800d82ed25131562e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-25 12:07:16 +0000

    solve abc233b

---
+++ abc/abc231_240/abc233/abc233b.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int L, R;
+    readf("%d %d\n", L, R);
+
+    auto S = readln.chomp.to!(dchar[]);
+
+    --L;
+
+    dchar[] l = S[0..L], r = S[R..$];
+    dchar[] m = S[L..R];
+    m.reverse;
+
+    dchar[] res = l ~ m ~ r;
+    res.writeln;
+}
