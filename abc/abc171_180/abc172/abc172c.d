95534db6bd796a02372d6ebdb9c925632658f145
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-26 12:47:58 +0000

    solve abc172c

---
+++ abc/abc171_180/abc172/abc172c.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    long N, M, K;
+    readf("%d %d %d\n", N, M, K);
+
+    auto A = [0L] ~ readln.chomp.split.to!(long[]);
+    auto B = readln.chomp.split.to!(long[]);
+
+    auto C = A.cumulativeFold!"a + b".array;
+    auto D = B.cumulativeFold!"a + b".array.assumeSorted;
+
+    ulong res;
+    foreach (i, c; C) {
+        if (c > K) break;
+
+        ulong num = i + D.lowerBound(K-c+1).length;
+        res = max(res, num);
+    }
+
+    res.writeln;
+}
