2dbab20d13e88e261e3d511c39cc3a9884f199ac
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-27 10:57:48 +0000

    solve abc171d

---
+++ abc/abc171_180/abc171/abc171d.d
@@ -0,0 +1,29 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    long S;
+    auto list = new long[]((10^^5)+1);
+    foreach (a; A) {
+        S += a;
+        ++list[a];
+    }
+
+    long Q;
+    readf("%d\n", Q);
+
+    foreach (_; 0 .. Q) {
+        long B, C;
+        readf("%d %d\n", B, C);
+
+        S += (C - B) * list[B];
+        list[C] += list[B];
+        list[B] = 0;
+
+        S.writeln;
+    }
+}
