c452c54e138bd6ca46c0861435d27de6880168fa
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-29 13:16:34 +0000

    improve time complexity of abc172d from O(NlogN) to O(N)

--- abc/abc171_180/abc172/abc172d.d
+++ abc/abc171_180/abc172/abc172d.d
@@ -4,17 +4,11 @@ void main() {
     long N;
     readf("%d\n", N);
 
-    auto f = new long[](N+1);
-    f[1..$] = 1;
-    foreach (i; 2 .. N+1) {
-        foreach (j; iota(i, N+1, i)) {
-            ++f[j];
-        }
-    }
-
     long res;
     foreach (i; 1 .. N+1) {
-        res += i * f[i];
+        long d = N / i;
+        long l = i * d;
+        res += d * (i + l) / 2;
     }
 
     res.writeln;
