99024a2a5c5528d5cb93d23e1e94e58f4b1cac45
kokatsu <66732063+kokatsu@users.noreply.github.com>
2021-12-29 13:05:03 +0000

    solve abc174c

---
+++ abc/abc171_180/abc174/abc174c.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int K;
+    readf("%d\n", K);
+
+    int N, res = -1;
+    foreach (i; 1 .. K+1) {
+        N = (N * 10 + 7) % K;
+
+        if (N == 0) {
+            res = i;
+            break;
+        }
+    }
+
+    res.writeln;
+}
