5326afe2acec4b507792095daecc3585afbd229d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-02 14:13:06 +0000

    solve abc169d

---
+++ abc/abc161_170/abc169/abc169d.d
@@ -0,0 +1,31 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    long[long] factors;
+    long d = 2;
+    while (d * d <= N) {
+        while (N % d == 0) {
+            ++factors[d];
+            N /= d;
+        }
+
+        ++d;
+    }
+    if (N > 1) ++factors[N];
+
+    long res;
+    foreach (v; factors.byValue) {
+        long num = v;
+        foreach (i; 1 .. v+1) {
+            if (num < i) break;
+
+            num -= i;
+            ++res;
+        }
+    }
+
+    res.writeln;
+}
