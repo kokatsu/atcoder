38d3b2178249f5dae1f4ed2790dce7722075e469
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-03 13:23:55 +0000

    solve abc170d

---
+++ abc/abc161_170/abc170/abc170d.d
@@ -0,0 +1,33 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int lim = A.maxElement + 1;
+
+    auto cnts = new int[](lim);
+    foreach (a; A) {
+        ++cnts[a];
+    }
+
+    auto div = new bool[](lim);
+
+    foreach (i; 1 .. lim) {
+        if (cnts[i] == 0) continue;
+
+        if (cnts[i] > 1) div[i] = true;
+        foreach (j; iota(i*2, lim, i)) {
+            div[j] = true;
+        }
+    }
+
+    int res;
+    foreach (a; A) {
+        if (!div[a]) ++res;
+    }
+
+    res.writeln;
+}
