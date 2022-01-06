b7f179c4d920d546943d61edbf0b6b51269114f1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-06 13:55:37 +0000

    solve abc162e

---
+++ abc/abc161_170/abc162/abc162e.d
@@ -0,0 +1,25 @@
+import std;
+
+void main() {
+    ulong N, K;
+    readf("%d %d\n", N, K);
+
+    ulong M = 10 ^^ 9 + 7;
+
+    auto cnts = new ulong[](K+1);
+    foreach_reverse (i; 1 .. K+1) {
+        cnts[i] = powmod(K/i, N, M);
+
+        foreach (j; iota(i*2, K+1, i)) {
+            if (cnts[i] < cnts[j]) cnts[i] += M;
+            cnts[i] -= cnts[j];
+        }
+    }
+
+    ulong res;
+    foreach (i; 1 .. K+1) {
+        res = (res + cnts[i] * i) % M;
+    }
+
+    res.writeln;
+}
