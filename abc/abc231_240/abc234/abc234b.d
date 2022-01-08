9847680770d4565b2699eb291e3747af3d0b4561
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-08 12:07:58 +0000

    solve abc234b

---
+++ abc/abc231_240/abc234/abc234b.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto x = new real[](N), y = new real[](N);
+    foreach (i; 0 .. N) {
+        readf("%f %f\n", x[i], y[i]);
+    }
+
+    real res = 0.0;
+    foreach (i; 0 .. N-1) {
+        foreach (j; i+1 .. N) {
+            res = max(res, hypot(x[i]-x[j], y[i]-y[j]));
+        }
+    }
+
+    format("%.10f", res).writeln;
+}
