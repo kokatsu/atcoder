f4226d1c33a4ad1269112ceeef3c5fd5d934454b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-08 12:27:28 +0000

    solve abc234d

---
+++ abc/abc231_240/abc234/abc234d.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto P = readln.chomp.split.to!(int[]);
+
+    auto heap = new BinaryHeap!(Array!int, "a > b")();
+
+    foreach (i, p; P) {
+        heap.insert(p);
+
+        if (heap.length > K) heap.popFront;
+        
+        if (i >= K - 1) heap.front.writeln;
+    }
+}
