5947cc253c58471df4edc677d365578a53e93337
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-15 12:25:20 +0000

    solve abc235c

---
+++ abc/abc231_240/abc235/abc235c.d
@@ -0,0 +1,46 @@
+module abc.abc231_240.abc235.abc235c;
+
+import std;
+
+void main() {
+    long N, Q;
+    readf("%d %d\n", N, Q);
+
+    auto a = readln.chomp.split.to!(long[]);
+
+    auto b = a.dup;
+
+    void f(ref long[] arr) {
+        auto tmp = arr.dup.sort.uniq.array.assumeSorted;
+        foreach (ref a; arr) {
+            a = tmp.lowerBound(a).length;
+        }
+    }
+
+    f(b);
+
+    long[long] num;
+    foreach (i, A; a) {
+        if (A in num) continue;
+        num[A] = b[i];
+    }
+
+    auto list = new long[][](N);
+    foreach (i, B; b) {
+        list[B] ~= i + 1;
+    }
+
+    foreach (_; 0 .. Q) {
+        long x, k;
+        readf("%d %d\n", x, k);
+
+        long res = -1;
+        if (x in num) {
+            if (list[num[x]].length >= k) {
+                res = list[num[x]][k-1];
+            }
+        }
+
+        res.writeln;
+    }
+}
