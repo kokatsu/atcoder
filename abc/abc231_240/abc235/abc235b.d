0bd267b6020713348b93ea4fd3936504e4ad291c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-15 12:05:24 +0000

    solve abc235b

---
+++ abc/abc231_240/abc235/abc235b.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto H = readln.chomp.split.to!(long[]);
+
+    long now;
+    while (now < N - 1) {
+        if (H[now] < H[now+1]) {
+            ++now;
+        }
+        else {
+            break;
+        }
+    }
+
+    H[now].writeln;
+}
