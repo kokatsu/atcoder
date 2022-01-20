05eac94a97e34237473c70a2f53d74b5e82a28b2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-20 12:11:12 +0000

    solve abc137d

---
+++ abc/abc131_140/abc137/abc137d.d
@@ -0,0 +1,27 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto list = new int[][](M+1);
+    foreach (_; 0 .. N) {
+        int A, B;
+        readf("%d %d\n", A, B);
+
+        if (A <= M) list[A] ~= B;
+    }
+
+    auto heap = new BinaryHeap!(Array!int, "a < b")();
+    int res;
+    foreach (i; 1 .. M+1) {
+        foreach (l; list[i]) heap.insert(l);
+
+        if (!heap.empty) {
+            res += heap.front;
+            heap.popFront;
+        }
+    }
+
+    res.writeln;
+}
