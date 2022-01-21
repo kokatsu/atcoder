47703cee09ae4c5f175ed4b26a602258075a4c41
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-21 10:13:38 +0000

    solve abc151e

---
+++ abc/abc151_160/abc151/abc151e.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    ulong N, K;
+    readf("%d %d\n", N, K);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    ulong M = 10 ^^ 9 + 7;
+
+    auto P = new ulong[](N+1);
+    P[K] = 1;
+    foreach (i; K+1 .. N+1) {
+        long c = powmod(i-K, M-2, M).to!long;
+        P[i] = ((P[i-1] * (i - 1)) % M) * c % M;
+    }
+
+    A.sort;
+
+    long res;
+    foreach (i, a; A) {
+        res = (res + ((a + M) % M) * (P[i+1] - P[N-i] + M) % M) % M;
+    }
+
+    res.writeln;
+}
