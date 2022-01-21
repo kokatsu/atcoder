4ae088815e0380a066d805ed3cf080123f60aecf
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-21 11:26:20 +0000

    solve abc134e

---
+++ abc/abc131_140/abc134/abc134e.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto rbt = new RedBlackTree!(int, "a < b", true)();
+    foreach (_; 0 .. N) {
+        int A;
+        readf("%d\n", A);
+
+        auto l = rbt.lowerBound(A);
+        if (!l.empty) rbt.removeKey(l.back);
+
+        rbt.insert(A);
+    }
+
+    rbt.length.writeln;
+}
