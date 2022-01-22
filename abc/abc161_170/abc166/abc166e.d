fbd9f1646c3db21f0aac85b472a673fdd76ce1ef
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-22 14:26:31 +0000

    solve abc166e

---
+++ abc/abc161_170/abc166/abc166e.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    long[long] list;
+    long res;
+    foreach (i, a; A) {
+        long num = a - i - 1;
+        if (num in list) res += list[num];
+
+        num -= a * 2;
+        ++list[num];
+    }
+
+    res.writeln;
+}
