9be947cfe1b87c5c2b1c346acfb16d77345fe5ed
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-22 08:33:49 +0000

    solve abc146e

---
+++ abc/abc141_150/abc146/abc146e.d
@@ -0,0 +1,28 @@
+import std;
+
+void main() {
+    long N, K;
+    readf("%d %d\n", N, K);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    auto B = A.cumulativeFold!"a + b".array;
+    
+    long[long] cnts;
+    cnts[0] = 1;
+
+    auto list = new long[](N+1);
+
+    long res;
+    foreach (i, b; B) {
+        long num = (b + K - i - 1) % K;
+
+        if (i >= K - 1) --cnts[list[i-K+1]];
+
+        if (num in cnts) res += cnts[num];
+        ++cnts[num];
+        list[i+1] = num;
+    }
+
+    res.writeln;
+}
