cb2b20f800dfbbf85dbb6538073291672952bbcf
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-23 12:04:56 +0000

    solve abc236b

---
+++ abc/abc231_240/abc236/abc236b.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    auto cnts = new int[](N+1);
+    foreach (a; A) ++cnts[a];
+
+    foreach (i; 1 .. N+1) {
+        if (cnts[i] != 4) {
+            i.writeln;
+            return;
+        }
+    }
+}
