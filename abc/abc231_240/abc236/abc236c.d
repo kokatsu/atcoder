6b86d33537f484e0a3fa63aba2365bbcd102645b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-23 12:08:47 +0000

    solve abc236c

---
+++ abc/abc231_240/abc236/abc236c.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto S = readln.chomp.split.to!(string[]);
+    auto T = readln.chomp.split.to!(string[]);
+
+    bool[string] list;
+    foreach (t; T) list[t] = true;
+
+    foreach (s; S) {
+        writeln(s in list ? "Yes" : "No"); 
+    }
+}
