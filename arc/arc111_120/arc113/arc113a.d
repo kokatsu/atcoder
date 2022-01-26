0c7745083ce9876e7cda706e63ceda38455c388f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-26 13:46:26 +0000

    solve arc113a

---
+++ arc/arc111_120/arc113/arc113a.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    long K;
+    readf("%d\n", K);
+
+    long res;
+    foreach (i; 1 .. K+1) {
+        long N = K / i;
+        
+        foreach (j; 1 .. N+1) {
+            res += N / j;
+        }
+    }
+
+    res.writeln;
+}
