065381354ebf56653b663b62b5b4c3615c4c1e55
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-27 14:57:24 +0000

    solve abc150d

---
+++ abc/abc141_150/abc150/abc150d.d
@@ -0,0 +1,31 @@
+import std;
+import core.checkedint;
+
+void main() {
+    long N, M;
+    readf("%d %d\n", N, M);
+
+    auto a = readln.chomp.split.to!(long[]);
+
+    a[] /= 2;
+
+    bool isZero;
+    long l = 1;
+    foreach (A; a) {
+        if (isZero) break;
+
+        long g = gcd(l, A);
+        l /= g;
+
+        l = muls(l, A, isZero);
+    }
+
+    foreach (A; a) {
+        isZero |= ((l / A) % 2 == 0);
+    }
+
+    long res;
+    if (!isZero) res = ((M / l) + 1) / 2;
+
+    res.writeln;
+}
