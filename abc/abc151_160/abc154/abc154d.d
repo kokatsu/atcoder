ced1b1567f31aea85117f0168647efeffca8c7ed
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-29 12:32:23 +0000

    solve abc154d

---
+++ abc/abc151_160/abc154/abc154d.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto p = readln.chomp.split.to!(int[]);
+
+    int M, S;
+    foreach (i, P; p) {
+        S += P + 1;
+        M = max(M, S);
+
+        if (i >= K - 1) S -= p[i-K+1] + 1;
+    }
+
+    real res = M.to!real / 2.0;
+    format("%.10f", res).writeln;
+}
