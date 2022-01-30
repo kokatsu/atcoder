2a39b2e62d356b42cc4c1811862cd17614be4f47
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-30 12:19:53 +0000

    solve abc237c

---
+++ abc/abc231_240/abc237/abc237c.d
@@ -0,0 +1,29 @@
+import std;
+
+void main() {
+    string S = readln.chomp;
+
+    int cnt1;
+    foreach (s; S) {
+        if (s == 'a') ++cnt1;
+        else break;
+    }
+
+    int cnt2;
+    foreach_reverse (s; S) {
+        if (s == 'a') ++cnt2;
+        else break;
+    }
+
+    dchar[] F;
+    if (cnt1 < cnt2) {
+        foreach (i; 0 .. cnt2-cnt1) F ~= 'a';
+    }
+
+    dchar[] T = F ~ S.dup.to!(dchar[]);
+
+    dchar[] U = T.dup;
+    U.reverse;
+
+    writeln(T == U ? "Yes" : "No");
+}
