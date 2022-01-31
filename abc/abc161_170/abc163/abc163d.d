6469869a864ea3545bad49d1e12dc4a4e82970b2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-01-31 13:36:08 +0000

    solve abc163d

---
+++ abc/abc161_170/abc163/abc163d.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    long N, K;
+    readf("%d %d\n", N, K);
+
+    long M = 10 ^^ 9 + 7;
+
+    long res;
+    long l, r;
+    foreach (i; 0 .. N+1) {
+        l += i, r += N - i;
+
+        if (i >= K - 1) res = (res + r - l + 1) % M;
+    }
+
+    res.writeln;
+}
