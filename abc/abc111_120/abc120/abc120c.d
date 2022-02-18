a7093b757eab148f30b3c0a9099e52e1123ec14f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-02-18 11:52:44 +0000

    solve abc120c

---
+++ abc/abc111_120/abc120/abc120c.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    string S = readln.chomp;
+
+    auto cnt = new int[](2);
+    foreach (s; S) {
+        ++cnt[s-'0'];
+    }
+
+    int res = cnt.minElement * 2;
+    res.writeln;
+}
