8c63c92901e7c2f3547b3fb93513fc17ea40c64d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-02-20 12:04:52 +0000

    solve abc240b

---
+++ abc/abc231_240/abc240/abc240b.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto a = readln.chomp.split.to!(int[]);
+
+    int[int] list;
+    foreach (A; a) ++list[A];
+
+    list.length.writeln;
+}
