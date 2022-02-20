8f478244bcb3958c7b242953a36bca11434b7ed6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-02-20 12:16:03 +0000

    solve abc240c

---
+++ abc/abc231_240/abc240/abc240c.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N, X;
+    readf("%d %d\n", N, X);
+
+    auto list = new bool[](X+1);
+    list[0] = true;
+    foreach (_; 0 .. N) {
+        int a, b;
+        readf("%d %d\n", a, b);
+        foreach_reverse (i; 0 .. X+1) {
+            if (list[i]) {
+                if (i + a <= X) list[i+a] = true;
+                if (i + b <= X) list[i+b] = true;
+                list[i] = false;
+            }
+        }
+    }
+
+    writeln(list[X] ? "Yes" : "No");
+}
