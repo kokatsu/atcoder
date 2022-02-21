5568a89b209306f55d9f66c442934a01ba13bbb5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-02-21 12:05:55 +0000

    solve abc150c

---
+++ abc/abc141_150/abc150/abc150c.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto P = readln.chomp.split.to!(int[]);
+    auto Q = readln.chomp.split.to!(int[]);
+
+    P[] -= 1, Q[] -= 1;
+
+    auto D = N.iota.array;
+
+    int res, cnt;
+    do {
+        ++cnt;
+        if (D == P) res += cnt;
+        if (D == Q) res -= cnt;
+    } while (D.nextPermutation);
+
+    if (res < 0) res *= -1;
+
+    res.writeln;
+}
