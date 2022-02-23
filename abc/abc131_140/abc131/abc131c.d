d69e07e3df632b52b0c9b8c772853d926d5f2947
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-02-23 13:21:19 +0000

    solve abc131c

---
+++ abc/abc131_140/abc131/abc131c.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    long A, B, C, D;
+    readf("%d %d %d %d\n", A, B, C, D);
+
+    long f(long x, long y, long z) {
+        return x - x / y - x / z + x / ((y * z) / gcd(y, z));
+    }
+
+    long res = f(B, C, D) - f(A-1, C, D);
+    res.writeln;
+}
