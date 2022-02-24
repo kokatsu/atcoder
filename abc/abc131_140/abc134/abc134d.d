9678cf2d3004f15630e302b37b3925e5636c894f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-02-24 11:55:02 +0000

    solve abc134d

---
+++ abc/abc131_140/abc134/abc134d.d
@@ -0,0 +1,28 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto a = readln.chomp.split.to!(int[]);
+
+    auto factors = new int[][](N+1);
+    foreach (i; 1 .. N+1) {
+        foreach (j; iota(i, N+1, i)) {
+            factors[j] ~= i;
+        }
+    }
+
+    int[] b;
+    foreach_reverse (i; 0 .. N) {
+        if (a[i] == 0) continue;
+
+        b ~= i + 1;
+        foreach (f; factors[i+1]) {
+            a[f-1] = (a[f-1] + 1) % 2;
+        }
+    }
+
+    b.length.writeln;
+    if (b.length > 0) writefln("%(%s %)", b);
+}
