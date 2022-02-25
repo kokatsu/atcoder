440cddf971c27922f8d900b45262cc4d6523a4d9
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-02-25 10:20:42 +0000

    solve abc142d

---
+++ abc/abc141_150/abc142/abc142d.d
@@ -0,0 +1,27 @@
+import std;
+
+void main() {
+    long A, B;
+    readf("%d %d\n", A, B);
+
+    long S = A.to!real.sqrt.floor.to!long;
+    long[] divisors;
+    foreach (i; 1 .. S+1) {
+        if (A % i != 0) continue;
+
+        divisors ~= i;
+        if (i * i != A) divisors ~= A / i;
+    }
+
+    divisors.sort;
+
+    long res;
+    foreach (d; divisors) {
+        if (B % d != 0) continue;
+
+        ++res;
+        while (d > 1 && B % d == 0) B /= d;
+    }
+
+    res.writeln;
+}
