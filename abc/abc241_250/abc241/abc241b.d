a63786cdaeca8ea148a8fd5b59cbe962b4e6dda5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-02-26 12:07:50 +0000

    solve abc241b

---
+++ abc/abc241_250/abc241/abc241b.d
@@ -0,0 +1,25 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = readln.chomp.split.to!(int[]);
+    auto B = readln.chomp.split.to!(int[]);
+
+    int[int] list;
+    foreach (a; A) ++list[a];
+
+    bool isOK = true;
+    foreach (b; B) {
+        if (!(b in list) || list[b] == 0) {
+            isOK = false;
+            break;
+        }
+        else {
+            --list[b];
+        }
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
