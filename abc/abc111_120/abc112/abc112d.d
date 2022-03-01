c5828502317ae7701b091783991a200d95c4d521
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-03-01 12:24:16 +0000

    solve abc112d

---
+++ abc/abc111_120/abc112/abc112d.d
@@ -0,0 +1,30 @@
+import std;
+
+void main() {
+    long N, M;
+    readf("%d %d\n", N, M);
+
+    long S = M.to!real.sqrt.floor.to!long;
+    long[] factors;
+    foreach (i; 1 .. S+1) {
+        if (M % i != 0) continue;
+
+        factors ~= i;
+        if (i * i != M) factors ~= M / i;
+    }
+
+    factors.sort!"a > b";
+
+    long res;
+    foreach (f; factors) {
+        long num = f * N;
+        if (num > M) continue;
+
+        if ((M - num) % f == 0) {
+            res = f;
+            break;
+        }
+    }
+
+    res.writeln;
+}
