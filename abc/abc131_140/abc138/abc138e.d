7c3119930e8f29fcb0b94ed3a030177ab78c03c1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-03-07 11:58:33 +0000

    solve abc138e

---
+++ abc/abc131_140/abc138/abc138e.d
@@ -0,0 +1,33 @@
+import std;
+
+void main() {
+    auto s = readln.chomp;
+    auto t = readln.chomp;
+
+    auto p = new long[][](26);
+    foreach (i, x; s) p[x-'a'] ~= i + 1;
+
+    auto pos = new SortedRange!(long[], "a < b")[](26);
+    foreach (i; 0 .. 26) pos[i] = p[i].assumeSorted;
+
+    long len = s.length.to!long;
+    long idx, cnt;
+    foreach (x; t) {
+        if (pos[x-'a'].empty) {
+            cnt = -1;
+            break;
+        }
+
+        auto u = pos[x-'a'].upperBound(idx);
+        if (u.empty) {
+            ++cnt;
+            idx = pos[x-'a'].front;
+        }
+        else {
+            idx = u.front;
+        }
+    }
+
+    long res = (cnt == - 1 ? -1 : cnt * len + idx);
+    res.writeln;
+}
