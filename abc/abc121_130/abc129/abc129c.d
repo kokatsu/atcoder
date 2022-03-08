e73be404c6e23181197c6df3fae4a13b0fc5dd55
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-03-08 12:20:04 +0000

    solve abc129c

---
+++ abc/abc121_130/abc129/abc129c.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    long N, M;
+    readf("%d %d\n", N, M);
+
+    auto broken = new bool[](N+1);
+    foreach (i; 0 .. M) {
+        long a;
+        readf("%d\n", a);
+
+        broken[a] = true;
+    }
+
+    long MOD = 10 ^^ 9 + 7;
+    auto dp = new int[](N+1);
+    dp[0] = 1;
+    foreach (i; 0 .. N) {
+        if (!broken[i+1]) dp[i+1] = (dp[i+1] + dp[i]) % MOD;
+        if (i + 2 <= N && !broken[i+2]) dp[i+2] = (dp[i+2] + dp[i]) % MOD;
+    }
+
+    dp[N].writeln;
+}
