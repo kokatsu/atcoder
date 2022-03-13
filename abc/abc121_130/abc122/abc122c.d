d2cdae97a9494b29c36370a8117a95091ddf867d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-03-13 12:00:25 +0000

    solve abc122c

---
+++ abc/abc121_130/abc122/abc122c.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    int N, Q;
+    readf("%d %d\n", N, Q);
+
+    auto S = readln.chomp;
+
+    auto dp = new int[](N+1);
+    foreach (i; 1 .. N) {
+        dp[i+1] = dp[i];
+
+        if (S[i-1..i+1] == "AC") ++dp[i+1];
+    }
+
+    foreach (_; 0 .. Q) {
+        int L, R;
+        readf("%d %d\n", L, R);
+
+        int res = dp[R] - dp[L];
+        res.writeln;
+    }
+}
