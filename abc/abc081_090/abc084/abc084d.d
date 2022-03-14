02950bc15861ca62a9cf12751d0cdbc7c92d3ce1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-03-14 11:27:47 +0000

    solve abc084d

---
+++ abc/abc081_090/abc084/abc084d.d
@@ -0,0 +1,42 @@
+import std;
+
+void main() {
+    long Q;
+    readf("%d\n", Q);
+
+    bool[] primeSieve(long x) {
+        auto sieve = new bool[](x+1);
+        sieve[2..x] = true;
+        long d = 2;
+        while (d * d <= x) {
+            if (sieve[d]) {
+                foreach (i; iota(d*d, x+1, d)) {
+                    sieve[i] = false;
+                }
+            }
+            ++d;
+        }
+        return sieve;
+    }
+
+    long lim = 10 ^^ 5;
+
+    auto sieve = primeSieve(lim);
+
+    auto dp = new long[](lim);
+    foreach (i; 1 .. lim) {
+        dp[i] = dp[i-1];
+
+        if (i % 2 == 0) continue;
+
+        if (sieve[i] && sieve[(i+1)/2]) ++dp[i];
+    }
+
+    foreach (_; 0 .. Q) {
+        long l, r;
+        readf("%d %d\n", l, r);
+
+        long res = dp[r] - dp[l-1];
+        res.writeln;
+    }
+}
