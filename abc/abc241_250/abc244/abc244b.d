75f1c4ec29fbca6904abb094fd6b3da95ba25a9b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-03-20 12:05:43 +0000

    solve abc244b

---
+++ abc/abc241_250/abc244/abc244b.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto T = readln.chomp;
+
+    int x, y, d;
+    foreach (t; T) {
+        if (t == 'S') {
+            if (d == 0) ++x;
+            else if (d == 1) --y;
+            else if (d == 2) --x;
+            else ++y;
+        }
+        else {
+            d = (d + 1) % 4;
+        }
+    }
+
+    writeln(x, " ", y);
+}
