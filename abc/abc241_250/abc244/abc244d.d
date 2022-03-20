5e8f88d6504bb1264fa311d90b796b597a12bcc1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-03-20 12:22:25 +0000

    solve abc244d

---
+++ abc/abc241_250/abc244/abc244d.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    auto S = readln.chomp.split;
+
+    auto T = readln.chomp.split;
+
+    int cnt;
+    foreach (i; 0 .. 3) {
+        if (S[i] == T[i]) ++cnt;
+    }
+
+    writeln((cnt == 0 || cnt == 3) ? "Yes" : "No");
+}
