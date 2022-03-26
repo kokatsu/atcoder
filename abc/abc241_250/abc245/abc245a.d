842e1812bbb526f63000e47d1b5e418138cd04be
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-03-26 12:03:28 +0000

    solve abc245a

---
+++ abc/abc241_250/abc245/abc245a.d
@@ -0,0 +1,12 @@
+import std;
+
+void main() {
+    int A, B, C, D;
+    readf("%d %d %d %d\n", A, B, C, D);
+
+    bool isOK;
+    if (A < C) isOK = true;
+    if (A == C && B <= D) isOK = true;
+
+    writeln(isOK ? "Takahashi": "Aoki");
+}
