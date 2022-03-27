a82affa56670edf237acb20e20d5deea03c80c49
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-03-27 10:04:22 +0000

    solve abc245f

---
+++ abc/abc241_250/abc245/abc245f.d
@@ -0,0 +1,43 @@
+import std;
+
+void main() {
+    long N, M;
+    readf("%d %d\n", N, M);
+
+    auto edge = new long[][](N);
+    foreach (_; 0 .. M) {
+        long U, V;
+        readf("%d %d\n", U, V);
+
+        --U, --V;
+        edge[V] ~= U;
+    }
+
+    auto cnts = new long[](N);
+    foreach (i; 0 .. N) {
+        foreach (e; edge[i]) {
+            ++cnts[e];
+        }
+    }
+
+    long[] stack;
+    foreach (i, c; cnts) {
+        if (c == 0) stack ~= i;
+    }
+
+    long res = N;
+    while (!stack.empty) {
+        auto f = stack.front;
+        stack.popFront;
+        --res;
+
+        foreach (e; edge[f]) {
+            --cnts[e];
+            if (cnts[e] == 0) {
+                stack ~= e;
+            }
+        }
+    }
+
+    res.writeln;
+}
