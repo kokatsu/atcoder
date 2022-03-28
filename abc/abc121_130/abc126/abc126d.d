3880ed1dc900ebefde2eb160a1db4aee144d815f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-03-28 13:06:04 +0000

    solve abc126d.d

---
+++ abc/abc121_130/abc126/abc126d.d
@@ -0,0 +1,34 @@
+import std;
+
+struct Edge {
+    long to, dist;
+}
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto edge = new Edge[][](N);
+    foreach (_; 0 .. N-1) {
+        long u, v, w;
+        readf("%d %d %d\n", u, v, w);
+
+        --u, --v;
+        edge[u] ~= Edge(v, w), edge[v] ~= Edge(u, w);
+    }
+
+    auto color = new long[](N);
+
+    void f(long pos, long pre = -1) {
+        foreach (e; edge[pos]) {
+            if (e.to == pre) continue;
+
+            color[e.to] = (e.dist % 2 == 0 ? color[pos] : (color[pos] + 1) % 2);
+            f(e.to, pos);
+        }
+    }
+
+    f(0);
+
+    foreach (c; color) c.writeln;
+}
