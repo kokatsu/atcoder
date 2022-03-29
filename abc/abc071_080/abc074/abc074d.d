f73036398a09c10929c5afadf660475c2cf3e0f9
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-03-29 11:42:02 +0000

    solve abc074d.d

---
+++ abc/abc071_080/abc074/abc074d.d
@@ -0,0 +1,40 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = new long[][](N);
+    foreach (i; 0 .. N) A[i] = readln.chomp.split.to!(long[]);
+
+    auto ign = new bool[][](N, N);
+    foreach (i; 0 .. N-1) {
+        foreach (j; i+1 .. N) {
+            long C = A[i][j];
+
+            foreach (k; 0 .. N) {
+                if (k == i || k == j) continue;
+
+                long D = A[i][k] + A[k][j];
+                if (C > D) {
+                    writeln(-1);
+                    return;
+                }
+                else if (C == D) {
+                    ign[i][j] = true;
+                }
+            }
+        }
+    }
+
+    long res;
+    foreach (i; 0 .. N-1) {
+        foreach (j; i+1 .. N) {
+            if (ign[i][j]) continue;
+
+            res += A[i][j];
+        }
+    }
+
+    res.writeln;
+}
