ee03472f7a4bcf3163e9ee2489b58d6ba2804663
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-01 13:28:50 +0000

    solve abc080d.d

---
+++ abc/abc071_080/abc080/abc080d.d
@@ -0,0 +1,27 @@
+import std;
+
+void main() {
+    int N, C;
+    readf("%d %d\n", N, C);
+
+    int L = 2 * 10 ^^ 5 + 1;
+    auto cnts = new long[][](C, L);
+    foreach (_; 0 .. N) {
+        int s, t, c;
+        readf("%d %d %d\n", s, t, c);
+
+        ++cnts[c-1][s*2-1], --cnts[c-1][t*2];
+    }
+
+    long res;
+    auto rec = new int[](L);
+    foreach (i; 0 .. C) {
+        foreach (j; 1 .. L) {
+            cnts[i][j] += cnts[i][j-1];
+
+            if (cnts[i][j] > 0) res = max(res, ++rec[j]);
+        }
+    }
+
+    res.writeln;
+}
