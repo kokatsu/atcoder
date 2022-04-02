1cc83d3605735ddb383a3407d9396e28d715e02b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-02 12:21:33 +0000

    solve abc052d.d

---
+++ abc/abc051_060/abc052/abc052d.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    long N, A, B;
+    readf("%d %d %d\n", N, A, B);
+
+    auto X = readln.chomp.split.to!(long[]);
+
+    long res;
+    foreach (i; 1 .. N) {
+        long D = X[i] - X[i-1];
+        res += min(D*A, B);
+    }
+
+    res.writeln;
+}
