d74ba37f9b90bcd8b5f3750c8fb81e52277e41ed
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-04 12:57:53 +0000

    solve abc052c.d

---
+++ abc/abc051_060/abc052/abc052c.d
@@ -0,0 +1,28 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    enum long M = 10 ^^ 9 + 7;
+
+    auto factors = new long[](N+1);
+    factors[] = 1;
+
+    foreach (i; 2 .. N+1) {
+        long num = i, d = 2;
+        while (d * d <= num) {
+            while (num % d == 0) {
+                ++factors[d];
+                num /= d;
+            }
+
+            ++d;
+        }
+
+        if (num > 1) ++factors[num];
+    }
+
+    long res = factors.fold!((a, b) => (a * b) % M);
+    res.writeln;
+}
