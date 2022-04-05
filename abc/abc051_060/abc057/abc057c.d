b06a03abaadca3a4399f0eebf4c587aaa879c4a3
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-05 14:01:43 +0000

    solve abc057c.d

---
+++ abc/abc051_060/abc057/abc057c.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto S = N.to!real.sqrt.floor.to!long;
+
+    long res = long.max;
+    foreach (i; 1 .. S+1) {
+        if (N % i == 0) {
+            auto A = i.log10.floor.to!long + 1;
+            auto B = log10(N/i).floor.to!long + 1;
+
+            res = min(res, max(A, B));
+        }
+    }
+
+    res.writeln;
+}
