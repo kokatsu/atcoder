68feefb7de5af284229214131b3ae3097818a5ad
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-07 13:14:52 +0000

    solve typical042

---
+++ typical90/typical041_050/typical042.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    long K;
+    readf("%d\n", K);
+
+    long M = 10 ^^ 9 + 7;
+
+    auto dp = new long[][](K+1, 9);
+    dp[0][0] = 1;
+    foreach (i; 0 .. K) {
+        foreach (j; 0 .. 9) {
+            foreach (k; 1 .. 10) {
+                if (i + k > K) break;
+
+                dp[i+k][(j+k)%9] = (dp[i+k][(j+k)%9] + dp[i][j]) % M;
+            }
+        }
+    }
+
+    dp[K][0].writeln;
+}
