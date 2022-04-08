1a9c5b01b3d989a02d45849c6d38c4287ba44a6a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-08 14:31:33 +0000

    solve abc154e.d with commentary

---
+++ abc/abc151_160/abc154/abc154e.d
@@ -0,0 +1,29 @@
+import std;
+
+void main() {
+    auto N = readln.chomp;
+
+    long K;
+    readf("%d\n", K);
+
+    auto len = N.length;
+    auto dp = new long[][][](len+1, K+2, 2);
+    dp[0][0][0] = 1;
+
+    foreach (i, n; N) {
+        long d = n - '0';
+
+        foreach (j; 0 .. K+1) {
+            foreach (k; 0 .. 2) {
+                long L = (k == 0 ? d + 1 : 10);
+
+                foreach (l; 0 .. L) {
+                    dp[i+1][l>0?j+1:j][k|(l<d)] += dp[i][j][k];
+                }
+            }
+        }
+    }
+
+    long res = dp[len][K].sum;
+    res.writeln;
+}
