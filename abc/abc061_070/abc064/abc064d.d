b9c3553a0124c25d7f9762a4ed3b2515558f1fd1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-11 13:00:02 +0000

    solve abc064d

---
+++ abc/abc061_070/abc064/abc064d.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto S = readln.chomp;
+
+    int l, r;
+    foreach (s; S) {
+        if (s == ')') {
+            if (r > 0) --r;
+            else ++l;
+        }
+        else {
+            ++r;
+        }
+    }
+
+    auto res = '('.repeat(l).to!string ~ S ~ ')'.repeat(r).to!string;
+    res.writeln;
+}
