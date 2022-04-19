4c211364ae41d54334566887fe9c44b7df2934c4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-19 13:24:35 +0000

    solve abc048c

---
+++ abc/abc041_050/abc048/abc048c.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    long N, x;
+    readf("%d %d\n", N, x);
+
+    auto a = readln.chomp.split.to!(long[]);
+
+    long res;
+    foreach (i; 1 .. N) {
+        long diff = a[i] + a[i-1] - x;
+        if (diff > 0) {
+            a[i] -= min(a[i], diff);
+            res += diff;
+        }
+    }
+
+    res.writeln;
+}
