ac703bc6bb4ff1a178bbc1bb3a78566100ccd4de
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-20 10:36:29 +0000

    solve abc054c

---
+++ abc/abc051_060/abc054/abc054c.d
@@ -0,0 +1,31 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto edge = new bool[][](N, N);
+    foreach (_; 0 .. M) {
+        int a, b;
+        readf("%d %d\n", a, b);
+
+        --a, --b;
+        edge[a][b] = true, edge[b][a] = true;
+    }
+
+    auto path = iota(1, N).array;
+
+    int res;
+    foreach (p; path.permutations) {
+        int x;
+        bool isOK = true;
+        foreach (y; p) {
+            isOK &= edge[x][y];
+            x = y;
+        }
+
+        if (isOK) ++res;
+    }
+
+    res.writeln;
+}
