384c1e8e95bf563849e69c898629841d0f044602
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-23 12:19:20 +0000

    solve abc249c

---
+++ abc/abc241_250/abc249/abc249c.d
@@ -0,0 +1,33 @@
+import std;
+
+void main() {
+    long N, K;
+    readf("%d %d\n", N, K);
+
+    auto S = new string[](N);
+    foreach (i; 0 .. N) S[i] = readln.chomp;
+
+    long res;
+
+    long L = 1 << N;
+    foreach (i; 1 .. L) {
+        auto use = new long[](26);
+
+        foreach (j; 0 .. N) {
+            if ((i >> j) & 1) {
+                foreach (s; S[j]) {
+                    ++use[s-'a'];
+                }
+            }
+        }
+
+        long cnt;
+        foreach (u; use) {
+            if (u == K) ++cnt;
+        }
+
+        res = max(res, cnt);
+    }
+
+    res.writeln;
+}
