3f5ca86efeaa58904424e64e44011096d646272b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-23 14:37:25 +0000

    solve typical084

---
+++ typical90/typical081_090/typical084.d
@@ -0,0 +1,21 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto S = readln.chomp;
+
+    long res, r, cnto, cntx;
+    foreach (l; 0 .. N) {
+        while (r < N && cnto * cntx == 0) {
+            (S[r++] == 'o' ? cnto : cntx) += 1;
+        }
+
+        if (cnto * cntx > 0) res += N - r + 1;
+
+        (S[l] == 'o' ? cnto : cntx) -= 1;
+    }
+
+    res.writeln;
+}
