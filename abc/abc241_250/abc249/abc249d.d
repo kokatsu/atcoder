004c32980f6af64f42cec19a1da563734d2864e5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-23 13:18:24 +0000

    solve abc249d

---
+++ abc/abc241_250/abc249/abc249d.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    long mx = A.maxElement;
+
+    auto cnts = new long[](mx+1);
+    foreach (a; A) ++cnts[a];
+
+    long res;
+    foreach (i; 1 .. mx+1) {
+        if (i * i <= mx) {
+            res += cnts[i*i] * cnts[i] * cnts[i];
+        }
+
+        foreach (j; iota(i*(i+1), mx+1, i)) {
+            res += cnts[j] * cnts[i] * cnts[j/i] * 2;
+        }
+    }
+
+    res.writeln;
+}
