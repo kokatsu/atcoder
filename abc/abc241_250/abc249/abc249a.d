38ea27ca87071b38c1dcc74552d3226a41685829
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-04-23 12:07:15 +0000

    solve abc249a

---
+++ abc/abc241_250/abc249/abc249a.d
@@ -0,0 +1,32 @@
+import std;
+
+void main() {
+    long A, B, C, D, E, F, X;
+    readf("%d %d %d %d %d %d %d\n", A, B, C, D, E, F, X);
+
+    long f(long x, long y, long z) {
+        long res, num, cnt;
+        bool isWalk = true;
+        while (num < X) {
+            if (isWalk) res += y;
+            ++num, ++cnt;
+
+            if (isWalk && cnt >= x) {
+                isWalk = false;
+                cnt = 0;
+            }
+            else if (!isWalk && cnt >= z) {
+                isWalk = true;
+                cnt = 0;
+            }
+        }
+
+        return res;
+    }
+
+    long S = f(A, B, C) - f(D, E, F);
+
+    if (S > 0) writeln("Takahashi");
+    else if (S < 0) writeln("Aoki");
+    else writeln("Draw");
+}
