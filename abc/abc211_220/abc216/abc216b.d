2ad55b3922c381a65681c2c1269aa0d1cc4aa868
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-01 14:10:55 +0000

    solve abc216b

---
+++ abc/abc211_220/abc216/abc216b.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    bool isOK;
+
+    auto S = new string[](N), T = new string[](N);
+    foreach (i; 0 .. N) {
+        auto input = readln.chomp.split;
+        S[i] = input[0], T[i] = input[1];
+
+        foreach (j; 0 .. i-1) {
+            isOK |= (S[i] == S[j] && T[i] == T[j]);
+        }
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
