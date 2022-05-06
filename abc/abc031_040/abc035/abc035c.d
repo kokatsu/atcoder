ac5728ac565ef3944537cdf70a2c120ee24d7b05
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-06 12:14:30 +0000

    solve abc035c

---
+++ abc/abc031_040/abc035/abc035c.d
@@ -0,0 +1,21 @@
+import std;
+
+void main() {
+    int N, Q;
+    readf("%d %d\n", N, Q);
+
+    auto disks = new int[](N+1);
+    foreach (_; 0 .. Q) {
+        int l, r;
+        readf("%d %d\n", l, r);
+
+        ++disks[l-1], --disks[r];
+    }
+
+    foreach (i; 0 .. N) {
+        disks[i+1] += disks[i];
+    }
+
+    auto res = iota(N).map!(x => disks[x] % 2 == 0 ? 0 : 1);
+    writefln("%(%s%)", res);
+}
