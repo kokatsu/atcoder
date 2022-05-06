138b31eeb1908dfe5153077ce1b8189d9fa607cd
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-06 12:03:34 +0000

    solve abc036c

---
+++ abc/abc031_040/abc036/abc036c.d
@@ -0,0 +1,21 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto a = new long[](N);
+    foreach (i; 0 .. N) readf("%d\n", a[i]);
+
+    void f(ref long[] arr) {
+        auto tmp = arr.dup.sort.uniq.array.assumeSorted;
+        foreach (ref x; arr) {
+            x = tmp.lowerBound(x).length;
+        }
+    }
+
+    auto b = a.dup;
+    f(b);
+
+    foreach (x; b) x.writeln;
+}
