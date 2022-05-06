f3b99f9e483833dd9e78a56b7a7d76bfe03338e5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-06 11:16:16 +0000

    solve abc096d

---
+++ abc/abc091_100/abc096/abc096d.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    long L = 55555;
+
+    auto sieve = new bool[](L+1);
+    sieve[2..L+1] = true;
+    long d = 2;
+    while (d * d <= L) {
+        if (sieve[d]) {
+            foreach (i; iota(d*d, L+1, d)) {
+                sieve[i] = false;
+            }
+        }
+        ++d;
+    }
+
+    auto res = iota(1, L+1).stride(5).filter!(x => sieve[x]).take(N);
+    writefln("%(%s %)", res);
+}
