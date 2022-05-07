1b985c6c910ecb2abdafc6c9cea2e090a2f04df6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-07 12:17:44 +0000

    solve abc246c

---
+++ abc/abc241_250/abc246/abc246c.d
@@ -0,0 +1,28 @@
+import std;
+
+void main() {
+    long N, K, X;
+    readf("%d %d %d\n", N, K, X);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    foreach (ref a; A) {
+        long C = min(K, a/X);
+        a -= C * X;
+        K -= C;
+    }
+
+    if (K > 0) {
+        A.sort!"a > b";
+
+        foreach (ref a; A) {
+            a = 0;
+            --K;
+
+            if (K <= 0) break;
+        }
+    }
+
+    long res = A.sum;
+    res.writeln;
+}
