73b038cf1e30f5438916be3858ede211cabf8faf
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-08 12:06:29 +0000

    solve abc250a

---
+++ abc/abc241_250/abc250/abc250a.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int H, W;
+    readf("%d %d\n", H, W);
+
+    int R, C;
+    readf("%d %d\n", R, C);
+
+    int res;
+
+    if (H > 1) res += (R <= 1 || R >= H ? 1 : 2);
+    if (W > 1) res += (C <= 1 || C >= W ? 1 : 2);
+
+    res.writeln;
+}
