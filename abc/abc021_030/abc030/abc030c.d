cdfd91d324a46d1804c449db02970a8406f67d98
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-09 13:25:06 +0000

    solve abc030c

---
+++ abc/abc021_030/abc030/abc030c.d
@@ -0,0 +1,34 @@
+import std;
+
+void main() {
+    long N, M;
+    readf("%d %d\n", N, M);
+
+    long X, Y;
+    readf("%d %d\n", X, Y);
+
+    auto a = readln.chomp.split.to!(long[]);
+    auto b = readln.chomp.split.to!(long[]);
+
+    auto as = a.assumeSorted;
+    auto bs = b.assumeSorted;
+
+    long cnt, pos;
+    while (true) {
+        if (cnt % 2 == 0) {
+            auto uba = as.upperBound(pos-1);
+            if (uba.empty) break;
+            pos = uba.front + X;
+        }
+        else {
+            auto ubb = bs.upperBound(pos-1);
+            if (ubb.empty) break;
+            pos = ubb.front + Y;
+        }
+
+        ++cnt;
+    }
+
+    long res = cnt / 2;
+    res.writeln;
+}
