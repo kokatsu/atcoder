72a2fb21b34a66d065bec4b07910079277b83821
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-10 14:28:52 +0000

    solve abc211b

---
+++ abc/abc211_220/abc211/abc211b.d
@@ -0,0 +1,11 @@
+import std;
+
+void main() {
+    auto S = new string[](4);
+    foreach (i; 0 .. 4) S[i] = readln.chomp;
+
+    S.sort;
+
+    bool isOK = (S.uniq.array.length == 4);
+    writeln(isOK ? "Yes" : "No");
+}
