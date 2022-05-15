8ab2a01c32162f86fbde3d88c90f5d83c3146d34
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-15 12:16:26 +0000

    solve abc063d

---
+++ abc/abc061_070/abc063/abc063d.d
@@ -0,0 +1,25 @@
+import std;
+
+void main() {
+    long N, A, B;
+    readf("%d %d %d\n", N, A, B);
+
+    auto h = new long[](N);
+    foreach (i; 0 ..N) readf("%d\n", h[i]);
+
+    long D = A - B;
+    long ok = h.maxElement, ng;
+    while (ok - ng > 1) {
+        long mid = (ok + ng) / 2;
+
+        long cnt;
+        foreach (x; h) {
+            long rem = x - B * mid;
+            if (rem > 0) cnt += (rem + D - 1) / D;
+        }
+
+        (cnt <= mid ? ok : ng) = mid;
+    }
+
+    ok.writeln;
+}
