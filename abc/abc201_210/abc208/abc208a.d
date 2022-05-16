6ae8a745142e8b9a0a3d18b5504ed06d3eeca978
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-16 14:14:51 +0000

    solve abc208a

---
+++ abc/abc201_210/abc208/abc208a.d
@@ -0,0 +1,11 @@
+import std;
+
+void main() {
+    int A, B;
+    readf("%d %d\n", A, B);
+
+    int mn = A, mx = A * 6;
+
+    bool isOK = (mn <= B && B <=mx);
+    writeln(isOK ? "Yes" : "No");
+}
