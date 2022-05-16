486a8d0f78bdcf6c37ad895d89f385d51646acc4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-16 14:30:28 +0000

    solve abc207b

---
+++ abc/abc201_210/abc207/abc207b.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    long A, B, C, D;
+    readf("%d %d %d %d\n", A, B, C, D);
+
+    long X = C * D - B;
+
+    long res = -1;
+    if (X > 0) res = (A + X - 1) / X;
+
+    res.writeln;
+}
