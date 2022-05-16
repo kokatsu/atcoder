4f1dce6db37fa6cf24449edd47642603bcc16721
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-16 14:21:06 +0000

    solve abc208b

---
+++ abc/abc201_210/abc208/abc208b.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    long P;
+    readf("%d\n", P);
+
+    auto fact = new long[](10);
+    fact[0] = 1;
+    foreach (i; 1 .. 10) fact[i] = fact[i-1] * (i + 1);
+
+    long res;
+    foreach_reverse (i, f; fact) {
+        long c = min(100, P/f);
+        res += c;
+        P -= f * c;
+    }
+
+    res.writeln;
+}
