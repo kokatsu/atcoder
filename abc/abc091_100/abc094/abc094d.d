5e0df1261e992d6f1b848d1ec08900904604fb84
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-17 13:52:17 +0000

    solve abc094d

---
+++ abc/abc091_100/abc094/abc094d.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    long n;
+    readf("%d\n", n);
+
+    auto a = readln.chomp.split.to!(long[]);
+
+    a.sort!"a > b";
+
+    long p = a[0], q = long.max / 2, r = p / 2;
+    a.popFront;
+    foreach (x; a) {
+        if (abs(x-r) < abs(q-r)) q = x;
+    }
+
+    writeln(p, " ", q);
+}
