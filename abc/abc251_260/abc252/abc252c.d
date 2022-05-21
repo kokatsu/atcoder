79a96a927e99496d54a67e5ff0d5e6ace0c2ee08
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-21 12:17:14 +0000

    solve abc252c

---
+++ abc/abc251_260/abc252/abc252c.d
@@ -0,0 +1,32 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto list = new int[][](10);
+    foreach (_; 0 .. N) {
+        auto S = readln.chomp;
+        foreach (i, s; S) {
+            list[s-'0'] ~= i.to!int;
+        }
+    }
+
+    int res = int.max;
+    foreach (i; 0 .. 10) {
+        list[i].sort;
+
+        int mx, pos;
+        auto g = list[i].group;
+        foreach (x; g) {
+            if (x[1] >= mx) {
+                mx = x[1], pos = x[0];
+            }
+        }
+
+        int num = 10 * (mx - 1) + pos;
+        res = min(res, num);
+    }
+
+    res.writeln;
+}
