6d54ef6a2312d7ba0d81f87f33605c0e9215a752
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-21 12:06:27 +0000

    solve abc252b

---
+++ abc/abc251_260/abc252/abc252b.d
@@ -0,0 +1,21 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto A = readln.chomp.split.to!(int[]);
+    auto B = readln.chomp.split.to!(int[]);
+
+    B[] -= 1;
+    auto list = new bool[](N);
+    foreach (b; B) list[b] = true;
+
+    int M =  A.maxElement;
+    bool isOK;
+    foreach (i, a; A) {
+        if (a == M && list[i]) isOK = true;
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
