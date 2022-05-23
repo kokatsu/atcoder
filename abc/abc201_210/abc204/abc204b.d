093665c13f6feb619ee2d14d6e394c39b3b5b126
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-23 13:05:31 +0000

    solve abc204b

---
+++ abc/abc201_210/abc204/abc204b.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int res;
+    foreach (a; A) res += max(0, a-10);
+
+    res.writeln;
+}
