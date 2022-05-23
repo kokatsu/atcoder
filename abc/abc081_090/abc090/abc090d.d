6450176ea7df1f95c8c504855ffe3ef5e48e274e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-23 11:25:38 +0000

    solve abc090d

---
+++ abc/abc081_090/abc090/abc090d.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    long N, K;
+    readf("%d %d\n", N, K);
+
+    long res;
+    if (K > 0) {
+        foreach (i; K+1 .. N+1) {
+            long d = N / i, r = N % i;
+            res += d * (i - K) + max(0, r-K+1);
+        }
+    }
+    else {
+        res = N * N;
+    }
+
+    res.writeln;
+}
