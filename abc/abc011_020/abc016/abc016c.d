8b4d507c9afffa07df115f7e138f83b2a3d192d3
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-23 12:54:42 +0000

    solve abc016c

---
+++ abc/abc011_020/abc016/abc016c.d
@@ -0,0 +1,33 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto isFriend = new bool[][](N, N);
+    foreach (_; 0 .. M) {
+        int A, B;
+        readf("%d %d\n", A, B);
+
+        --A, --B;
+        isFriend[A][B] = isFriend[B][A] = true;
+    }
+
+    foreach (i; 0 .. N) {
+        int res;
+        foreach (j; 0 .. N) {
+            if (i == j || isFriend[i][j]) continue;
+
+            bool isOK;
+            foreach (k; 0 .. N) {
+                if (i == k || j == k) continue;
+
+                if (isFriend[i][k] && isFriend[k][j]) isOK = true;
+            }
+
+            if (isOK) ++res;
+        }
+
+        res.writeln;
+    }
+}
