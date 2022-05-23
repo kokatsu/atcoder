ad3bfe83c45fb461c96f64d1644af645832a9dab
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-23 13:10:41 +0000

    solve abc203a

---
+++ abc/abc201_210/abc203/abc203a.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    auto a = readln.chomp.split.to!(int[]);
+
+    auto g = a.sort.group.array;
+    g.sort!"a[1] > b[1]";
+
+    int res;
+    if (g[0][1] == 2) res = g[1][0];
+    if (g[0][1] == 3) res = g[0][0];
+
+    res.writeln;
+}
