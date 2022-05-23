ed8ffb147d5cac1ebcd4944cac9a68aad12fc38d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-23 12:22:21 +0000

    solve kupc2020a

---
+++ others/kupc/kupc2020/kupc2020a.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int res, u, v;
+    foreach (i; 0 .. N) {
+        int x, y;
+        readf("%d %d\n", x, y);
+
+        if (i > 0) {
+            res += abs(u-x) + abs(v-y);
+        }
+
+        u = x, v = y;
+    }
+
+    res.writeln;
+}
