4b0c93e9b4de32bf4e44af9b32f67d157911e5ca
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-24 13:59:49 +0000

    solve abc202b

---
+++ abc/abc201_210/abc202/abc202b.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    auto S = readln.chomp;
+
+    foreach_reverse (s; S) {
+        dchar d;
+        if (s == '6') d = '9';
+        else if (s == '9') d = '6';
+        else d = s;
+
+        d.write;
+    }
+    writeln;
+}
