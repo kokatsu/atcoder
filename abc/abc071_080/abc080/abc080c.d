a5654457519f4584e1c7a4fced306697cad394df
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-25 12:17:49 +0000

    solve abc080c

---
+++ abc/abc071_080/abc080/abc080c.d
@@ -0,0 +1,30 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto F = new int[][](N);
+    foreach (i; 0 .. N) F[i] = readln.chomp.split.to!(int[]);
+
+    auto P = new int[][](N);
+    foreach (i; 0 .. N) P[i] = readln.chomp.split.to!(int[]);
+
+    int res = int.min, M = 10;
+    foreach (i; 1 .. 1<<M) {
+        int num;
+
+        foreach (j; 0 .. N) {
+            int cnt;
+            foreach (k; 0 .. M) {
+                if (((i >> k) & 1) && F[j][k] == 1) ++cnt;
+            }
+
+            num += P[j][cnt];
+        }
+
+        res = max(res, num);
+    }
+
+    res.writeln;
+}
