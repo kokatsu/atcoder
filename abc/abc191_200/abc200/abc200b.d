98a6ac55bffca1cfe859169dd1edb0351e586957
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-25 14:30:44 +0000

    solve abc200b

---
+++ abc/abc191_200/abc200/abc200b.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    long N, K;
+    readf("%d %d\n", N, K);
+
+    long res = N, M = 200, L = 1000;
+    foreach (i; 0 .. K) {
+        if (res % M == 0) res /= M;
+        else res = res * L + M;
+    }
+
+    res.writeln;
+}
