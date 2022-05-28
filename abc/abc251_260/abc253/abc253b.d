9b62f6f65ada48d00b25cef35e3d4138619aeecb
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-28 12:06:43 +0000

    solve abc253b

---
+++ abc/abc251_260/abc253/abc253b.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int H, W;
+    readf("%d %d\n", H, W);
+
+    int[] x, y;
+    foreach (i; 0 .. H) {
+        auto S = readln.chomp;
+        foreach (j, s; S) {
+            if (s == 'o') x ~= i, y ~= j.to!int;
+        }
+    }
+
+    int res = abs(x[0] - x[1]) + abs(y[0] - y[1]);
+    res.writeln;
+}
