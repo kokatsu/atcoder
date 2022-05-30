818ef18d236763d4e270ddf9e32f468861661ee9
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-05-30 11:30:38 +0000

    solve agc029b

---
+++ agc/agc021_030/agc029/agc029b.d
@@ -0,0 +1,32 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    long[long] cnts;
+    foreach (a; A) ++cnts[a];
+
+    A.sort!"a > b";
+
+    long res, mx = (1L << 32);
+    foreach (a; A) {
+        if (cnts[a] == 0) continue;
+        --cnts[a];
+
+        long num = 1;
+        while (num < mx) {
+            long diff = num - a;
+            if ((diff in cnts) && cnts[diff] > 0) {
+                ++res, --cnts[diff];
+                break;
+            }
+
+            num <<= 1;
+        }
+    }
+
+    res.writeln;
+}
