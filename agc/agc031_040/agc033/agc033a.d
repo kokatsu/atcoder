0e90e1c15ac5b2e54c9cc05b44e09cb46a4d8981
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-06-01 12:24:31 +0000

    solve agc033a

---
+++ agc/agc031_040/agc033/agc033a.d
@@ -0,0 +1,45 @@
+import std;
+
+struct Coord {
+    int x, y;
+}
+
+void main() {
+    int H, W;
+    readf("%d %d\n", H, W);
+
+    Coord[] coord;
+    auto dists = new int[][](H, W);
+    foreach (i; 0 .. H) {
+        dists[i][] = -1;
+
+        auto A = readln.chomp;
+        foreach (j, a; A) {
+            if (a == '#') {
+                dists[i][j] = 0;
+                coord ~= Coord(i, j.to!int);
+            }
+        }
+    }
+
+    int[] dx = [-1, 0, 1, 0], dy = [0, 1, 0, -1];
+    int res;
+    while (!coord.empty) {
+        auto f = coord.front;
+        coord.popFront;
+
+        res = max(res, dists[f.x][f.y]);
+
+        foreach (i; 0 .. 4) {
+            int x = f.x + dx[i], y = f.y + dy[i];
+            if (x < 0 || H <= x || y < 0 || W <= y) continue;
+
+            if (dists[x][y] >= 0) continue;
+
+            dists[x][y] = dists[f.x][f.y] + 1;
+            coord ~= Coord(x, y);
+        }
+    }
+
+    res.writeln;
+}
