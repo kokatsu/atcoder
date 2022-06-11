f5d2ebe8d3daa2de73cba4fa9265fd4ec0011b05
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-06-11 14:40:45 +0000

    solve abc255d with commentary

---
+++ abc/abc251_260/abc255/abc255d.d
@@ -0,0 +1,28 @@
+import std;
+
+void main() {
+    long N, Q;
+    readf("%d %d\n", N, Q);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    auto B = A.sort;
+    auto C = cumulativeFold!"a + b"([0L] ~ A).array;
+
+    foreach (_; 0 .. Q) {
+        long X;
+        readf("%d\n", X);
+
+        auto T = B.trisect(X);
+
+        long res;
+
+        long lenL = T[0].length.to!long;
+        res += lenL * X - C[lenL];
+
+        long lenU = T[2].length.to!long;
+        res += C[N] - C[N-lenU] - lenU * X;
+
+        res.writeln;
+    }
+}
