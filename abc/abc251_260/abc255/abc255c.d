2a1a7610fa09edd0e085b645b6ff3f201db0cb9f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-06-11 12:40:09 +0000

    solve abc255c

---
+++ abc/abc251_260/abc255/abc255c.d
@@ -0,0 +1,29 @@
+import std;
+
+void main() {
+    long X, A, D, N;
+    readf("%d %d %d %d\n", X, A, D, N);
+
+    long L = A, R = A + D * (N - 1);
+    if (L > R) swap(L, R);
+
+    long res;
+    if (D == 0) {
+        res = abs(X-A);
+    }
+    else if (X <= L) {
+        res = abs(L-X);
+    }
+    else if (X >= R) {
+        res = abs(R-X);
+    }
+    else {
+        long Y = X - A;
+        if (D < 0) D = -D, Y = -Y;
+
+        long M = Y / D;
+        res = min(Y-D*M, D*(M+1)-Y);
+    }
+
+    res.writeln;
+}
