f8269ee3320f38e4beca4ed614ba0a5269696f9d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-06-11 12:02:31 +0000

    solve abc255a

---
+++ abc/abc251_260/abc255/abc255a.d
@@ -0,0 +1,12 @@
+import std;
+
+void main() {
+    int R, C;
+    readf("%d %d\n", R, C);
+
+    auto A = new int[][](2, 2);
+    foreach (i; 0 .. 2) readf("%d %d\n", A[i][0], A[i][1]);
+
+    int res = A[R-1][C-1];
+    res.writeln;
+}
