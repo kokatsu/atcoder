0c67996986df5a5dbd26d4d097ab142d39bfdb79
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-06-20 10:53:20 +0000

    solve abc047d

---
+++ abc/abc041_050/abc047/abc047d.d
@@ -0,0 +1,25 @@
+import std;
+
+void main() {
+    int N, T;
+    readf("%d %d\n", N, T);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int m = A.front;
+    A.popFront;
+
+    int res, d;
+    foreach (a; A) {
+        if (m > a) {
+            m = a;
+        }
+        else {
+            int t = a - m;
+            if (d < t) res = 1, d = t;
+            else if (d == t) ++res;
+        }
+    }
+
+    res.writeln;
+}
