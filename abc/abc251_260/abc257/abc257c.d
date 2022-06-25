3a408ca9fb613afba98a164628181975aaa25311
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-06-25 13:12:05 +0000

    solve abc257c

---
+++ abc/abc251_260/abc257/abc257c.d
@@ -0,0 +1,39 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto S = readln.chomp;
+
+    auto W = readln.chomp.split.to!(long[]);
+
+    W[] *= 2;
+
+    long[] A, B;
+    foreach (i, s; S) {
+        (s == '1' ? A : B) ~= W[i];
+    }
+
+    auto C = A.sort;
+    auto D = B.sort;
+
+    long res;
+    foreach (a; A) {
+        long num;
+        num += C.upperBound(a-1).length;
+        num += D.lowerBound(a).length;
+
+        res = max(res, num);
+    }
+
+    foreach (b; B) {
+        long num;
+        num += C.upperBound(b).length;
+        num += D.lowerBound(b+1).length;
+
+        res = max(res, num);
+    }
+
+    res.writeln;
+}
