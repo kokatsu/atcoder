02fb86a7cb4a1b861165f12a32de3d67abfbf48a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-06-25 14:26:58 +0000

    improve abc257d execution time

--- abc/abc251_260/abc257/abc257d.d
+++ abc/abc251_260/abc257/abc257d.d
@@ -1,9 +1,5 @@
 import std;
 
-struct Edge {
-    long to, num;
-}
-
 void main() {
     int N;
     readf("%d\n", N);
@@ -26,39 +22,13 @@ void main() {
     foreach (i; 0 .. N) {
         foreach (j; 0 .. N) {
             foreach (k; 0 .. N) {
-                mat[j][k] = min(mat[j][k], mat[j][i]+mat[i][k]);
+                mat[j][k] = min(mat[j][k], max(mat[j][i], mat[i][k]));
             }
         }
     }
 
     long res = long.max;
-
-    void f(long x) {
-        auto arr = new long[](N);
-        arr[] = long.max / 2;
-        arr[x] = 0;
-
-        auto heap = new BinaryHeap!(Array!Edge, "a.num > b.num")();
-        heap.insert(Edge(x, 0));
-        while (!heap.empty) {
-            auto f = heap.front;
-            heap.popFront;
-
-            foreach (i; 0 .. N) {
-                if (i == f.to) continue;
-
-                long t = max(f.num, mat[f.to][i]);
-                if (arr[i] > t) {
-                    arr[i] = t;
-                    heap.insert(Edge(i, t));
-                }
-            }
-        }
-
-        res = min(res, arr.maxElement);
-    }
-
-    foreach (i; 0 .. N) f(i);
+    foreach (i; 0 .. N) res = min(res, mat[i].maxElement);
 
     res.writeln;
 }
