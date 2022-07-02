e895c81c1d112d54f8bab18dd5fabe469d297f5b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-07-02 12:28:16 +0000

    solve abc258d

---
+++ abc/abc251_260/abc258/abc258d.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    long N, X;
+    readf("%d %d\n", N, X);
+
+    auto A = new long[](N), B = new long[](N);
+    foreach (i; 0 .. N) readf("%d %d\n", A[i], B[i]);
+
+    auto T = new long[](N+1), M = new long[](N+1);
+    M[0] = long.max;
+    foreach (i; 0 .. N) {
+        T[i+1] = T[i] + A[i] + B[i];
+        M[i+1] = min(M[i], B[i]);
+    }
+
+    long res = long.max;
+    foreach_reverse (i; 1 .. N+1) {
+        if (i > X) continue;
+
+        long num = T[i] + (X - i) * M[i];
+        res = min(res, num);
+    }
+
+    res.writeln;
+}
