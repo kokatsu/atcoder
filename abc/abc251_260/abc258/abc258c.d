635552597f9e1eb6685736f3ee0e286f4017da40
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-07-02 12:18:00 +0000

    solve abc258c

---
+++ abc/abc251_260/abc258/abc258c.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    long N, Q;
+    readf("%d %d\n", N, Q);
+
+    auto S = readln.chomp;
+
+    long M;
+    foreach (_; 0 .. Q) {
+        long t, x;
+        readf("%d %d\n", t, x);
+
+        if (t == 1) M = (M + x) % N;
+        else S[(x-1+N-M)%N].writeln;
+    }
+}
