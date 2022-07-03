9bea154ef29269742cd8d7e2348fec0848da60fa
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-07-03 13:53:29 +0000

    solve abc178d

---
+++ abc/abc171_180/abc178/abc178d.d
@@ -0,0 +1,21 @@
+import std;
+
+void main() {
+    long S;
+    readf("%d\n", S);
+
+    long MOD = 10 ^^ 9 + 7;
+
+    void addMod(ref long x, long y) {
+        x = (x + y) % MOD;
+    }
+
+    auto dp = new long[](S+1);
+    dp[0] = 1;
+    foreach (i; 3 .. S+1) {
+        addMod(dp[i], dp[i-1]+dp[i-3]);
+    }
+
+    long res = dp[S];
+    res.writeln;
+}
