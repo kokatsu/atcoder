6b2b2b52d49af1fddf8bc1499594531f8b7f2d2e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-07-08 11:14:09 +0000

    solve abc206d

---
+++ abc/abc201_210/abc206/abc206d.d
@@ -0,0 +1,81 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int M = A.maxElement, L = N / 2;
+    auto uf = new UnionFind!int(M+1);
+    foreach (i; 0 .. L) {
+        uf.unite(A[i], A[N-1-i]);
+    }
+
+    int res;
+    foreach (i; 1 .. M+1) {
+        if (uf.root(i) == i) res += uf.size(i) - 1;
+    }
+
+    res.writeln;
+}
+
+/// Union-Find
+struct UnionFind(T)
+if (isIntegral!T) {
+
+    /// Constructor
+    this(T n) nothrow @safe {
+        len = n;
+        par.length = len;
+        cnt.length = len;
+        foreach (i; 0 .. len) {
+            par[i] = i;
+        }
+        cnt[] = 1;
+    }
+
+    /// Returns the root of x.
+    T root(T x) nothrow @nogc @safe
+    in (0 <= x && x < len) {
+        if (par[x] == x) {
+            return x;
+        }
+        else {
+            return par[x] = root(par[x]);
+        }
+    }
+
+    /// Returns whether x and y have the same root.
+    bool isSame(T x, T y) nothrow @nogc @safe
+    in (0 <= x && x < len && 0 <= y && y < len) {
+        return root(x) == root(y);
+    }
+
+    /// Unites x tree and y tree.
+    void unite(T x, T y) nothrow @nogc @safe
+    in (0 <= x && x < len && 0 <= y && y < len) {
+        x = root(x), y = root(y);
+        if (x == y) {
+            return;
+        }
+
+        if (cnt[x] > cnt[y]) {
+            swap(x, y);
+        }
+
+        cnt[y] += cnt[x];
+        par[x] = y;
+    }
+
+    /// Returns the size of the x tree.
+    T size(T x) nothrow @nogc @safe
+    in (0 <= x && x < len) {
+        return cnt[root(x)];
+    }
+
+private:
+    T len;
+    T[] par;
+    T[] cnt;
+}
