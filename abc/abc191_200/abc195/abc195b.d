f4215c02cfd2e9dcbb8219de89332461a607ada5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-07-16 14:47:53 +0000

    solve abc195b

---
+++ abc/abc191_200/abc195/abc195b.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int A, B, W;
+    readf("%d %d %d\n", A, B, W);
+
+    W *= 10 ^^ 3;
+
+    int mn = int.max, mx = int.min;
+    foreach (i; 1 .. W+1) {
+        int l = A * i, u = B * i;
+        if (l <= W && W <= u) {
+            mn = min(mn, i), mx = max(mx, i);
+        }
+    }
+
+    if (mn > mx) writeln("UNSATISFIABLE");
+    else writeln(mn, " ", mx);
+}
