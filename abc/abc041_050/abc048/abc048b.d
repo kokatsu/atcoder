4100527e41a05aa04118f26536daba21f172d1c7
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-07-16 14:00:39 +0000

    solve abc048b

---
+++ abc/abc041_050/abc048/abc048b.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    long a, b, x;
+    readf("%d %d %d\n", a, b, x);
+
+    long f(long u) {
+        if (u < 0) return u;
+        return u / x;
+    }
+
+    long res = f(b) - f(a-1);
+    res.writeln;
+}
