873b738c7922850b0d2bbe849b93604e139361c3
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-07-17 12:21:21 +0000

    solve abc260c

---
+++ abc/abc251_260/abc260/abc260c.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    long N, X, Y;
+    readf("%d %d %d\n", N, X, Y);
+
+    long R = 1, B;
+    foreach_reverse (i; 2 .. N+1) {
+        B += R * X;
+
+        R += B;
+        B *= Y;
+    }
+
+    B.writeln;
+}
