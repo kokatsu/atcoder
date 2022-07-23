7eca1e2eeac1ebfb368494a4a2f77dd114b0bd14
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-07-23 12:13:25 +0000

    solve abc261c

---
+++ abc/abc261_270/abc261c.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int[string] cnts;
+    foreach (i; 0 .. N) {
+        auto S = readln.chomp;
+
+        if (S in cnts) writefln("%s(%d)", S, cnts[S]);
+        else S.writeln;
+
+        ++cnts[S];
+    }
+}
