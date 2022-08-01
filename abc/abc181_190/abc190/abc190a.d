3d565291f87c84f886062748f0896f007b32e696
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-01 13:43:49 +0000

    solve abc190a

---
+++ abc/abc181_190/abc190/abc190a.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    int A, B, C;
+    readf("%d %d %d\n", A, B, C);
+
+    string S = "Takahashi", T = "Aoki";
+
+    string res;
+    if (A > B) res = S;
+    else if (A < B) res = T;
+    else res = (C == 1 ? S : T);
+
+    res.writeln;
+}
