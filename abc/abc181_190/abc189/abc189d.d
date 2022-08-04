a30fd4ea65218686ad7a7c387110c1c7b5a13ec0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-04 12:21:04 +0000

    solve abc189d

---
+++ abc/abc181_190/abc189/abc189d.d
@@ -0,0 +1,28 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    long f(long x, long y, string op) {
+        if (op == "AND") return x & y;
+        else return x | y;
+    }
+
+    long M = 2;
+    auto dp = new long[][](N+1, M);
+    dp[0][] = 1;
+    foreach (i; 0 .. N) {
+        auto S = readln.chomp;
+
+        foreach (j; 0 .. M) {
+            foreach (k; 0 .. M) {
+                long l = f(j, k, S);
+                dp[i+1][l] += dp[i][j];
+            }
+        }
+    }
+
+    long res = dp[N][1];
+    res.writeln;
+}
