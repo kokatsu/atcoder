66f6257116cdc8968246af0ba00e2d6fe2574332
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-04 10:49:37 +0000

    solve abc189a

---
+++ abc/abc181_190/abc189/abc189a.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    auto C = readln.chomp;
+
+    bool isWon = true;
+    auto len = C.length;
+    foreach (i; 1 .. len) {
+        isWon &= (C[i-1] == C[i]);
+    }
+
+    writeln(isWon ? "Won" : "Lost");
+}
