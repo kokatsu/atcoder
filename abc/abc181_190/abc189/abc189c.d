82da980715a59926be1ee2d269d626075f885dbd
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-04 11:58:37 +0000

    solve abc189c

---
+++ abc/abc181_190/abc189/abc189c.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int res = N;
+
+    foreach (i; 0 .. N) {
+        int num = A[i];
+        res = max(res, num);
+
+        foreach (j; i+1 .. N) {
+            num = min(num, A[j]);
+            res = max(res, num*(j-i+1));
+        }
+    }
+
+    res.writeln;
+}
