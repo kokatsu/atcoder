b89dd9c25a217384b08f305bf7e233c27d080254
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-05 12:05:09 +0000

    solve abc187a

---
+++ abc/abc181_190/abc187/abc187a.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    int A, B;
+    readf("%d %d\n", A, B);
+
+    int f(int x) {
+        return x.to!(dchar[]).map!(a => a - '0').sum;
+    }
+
+    int res = max(f(A), f(B));
+    res.writeln;
+}
