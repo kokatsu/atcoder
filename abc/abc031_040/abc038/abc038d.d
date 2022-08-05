fd32c13a5abcae9ee18f91ff0b1286a8cb651cba
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-05 11:53:58 +0000

    solve abc038d

---
+++ abc/abc031_040/abc038/abc038d.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto h = new int[](N), w = new int[](N);
+    foreach (i; 0 .. N) readf("%d %d\n", h[i], w[i]);
+
+    zip(h, w).sort!"a[0] == b[0] ? a[1] > b[1] : a[0] < b[0]";
+
+    auto rbt = new RedBlackTree!(int, "a < b", true)();
+    foreach (x; w) {
+        auto ub = rbt.upperBound(x-1);
+        if (!ub.empty) rbt.removeKey(ub.front);
+
+        rbt.insert(x);
+    }
+
+    auto res = rbt.length;
+    res.writeln;
+}
