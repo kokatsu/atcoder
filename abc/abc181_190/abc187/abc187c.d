397f2cce73436059d2549fa636856cc21f8d65a2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-05 12:18:01 +0000

    solve abc187c

---
+++ abc/abc181_190/abc187/abc187c.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    bool[string] list0, list1;
+    string res = "satisfiable";
+    foreach (_; 0 .. N) {
+        auto S = readln.chomp;
+
+        bool isFront = (S.front == '!');
+        string T = (isFront ? S[1..$] : S);
+        (isFront ? list1[T] : list0[T]) = true;
+
+        if ((T in list0) && (T in list1)) {
+            res = T;
+        }
+    }
+
+    res.writeln;
+}
