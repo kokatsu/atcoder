2003d3068c2b2d37a59823fe19c0984abb3536ce
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-06 12:47:05 +0000

    solve abc263d

---
+++ abc/abc261_270/abc263/abc263d.d
@@ -0,0 +1,35 @@
+import std;
+
+void main() {
+    long N, L, R;
+    readf("%d %d %d\n", N, L, R);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    long u = L;
+    auto X = new long[][](N+1, 2);
+    foreach (i, a; A) {
+        X[i+1][0] = X[i].minElement + a;
+        X[i+1][1] = u;
+        u += L;
+    }
+
+    long v = R;
+    auto Y = new long[][](N+1, 2);
+    foreach_reverse (i, a; A) {
+        Y[i][0] = Y[i+1].minElement + a;
+        Y[i][1] = v;
+        v += R;
+    }
+
+    long res = long.max;
+    foreach (i; 0 .. N+1) {
+        foreach (j; 0 .. 2) {
+            foreach (k; 0 .. 2) {
+                res = min(res, X[i][j]+Y[i][k]);
+            }
+        }
+    }
+
+    res.writeln;
+}
