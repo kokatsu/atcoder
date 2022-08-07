242dbf8fb494ea36f52d2ccee7ad68d6470bda64
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-07 13:48:25 +0000

    solve abc186b

---
+++ abc/abc181_190/abc186/abc186b.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    int H, W;
+    readf("%d %d\n", H, W);
+
+    int mn = 100, lim = 100;
+    auto cnts = new int[](lim+1);
+    foreach (i; 0 .. H) {
+        auto A = readln.chomp.split.to!(int[]);
+
+        foreach (a; A) {
+            mn = min(mn, a);
+            ++cnts[a];
+        }
+    }
+
+    int res;
+    foreach (i; mn+1 .. lim+1) {
+        res += (i - mn) * cnts[i];
+    }
+
+    res.writeln;
+}
