cb6a84620ed04648cac940ff480d0044dcdc9096
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-07 14:38:13 +0000

    solve abc185b

---
+++ abc/abc181_190/abc185/abc185b.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    int N, M, T;
+    readf("%d %d %d\n", N, M, T);
+
+    int R = N, P;
+    bool isOK = true;
+    foreach (_; 0 .. M) {
+        int A, B;
+        readf("%d %d\n", A, B);
+
+        R -= (A - P);
+        isOK &= (R > 0);
+
+        R = min(N, R+B-A);
+        P = B;
+    }
+
+    R -= (T - P);
+    isOK &= (R > 0);
+
+    writeln(isOK ? "Yes" : "No");
+}
