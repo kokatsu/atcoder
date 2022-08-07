c860c1d3a3ebab7464b3d3714100613ba9b41c3e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-07 13:52:30 +0000

    solve abc186c

---
+++ abc/abc181_190/abc186/abc186c.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int res;
+    foreach (i; 1 .. N+1) {
+        auto dec = i.to!string;
+        if (dec.canFind('7')) continue;
+
+        auto oct = i.to!string(8);
+        if (oct.canFind('7')) continue;
+
+        ++res;
+    }
+
+    res.writeln;
+}
