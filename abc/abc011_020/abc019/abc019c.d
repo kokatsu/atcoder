dec893271af4266c288fb37a68828786eedb017a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-07 13:39:09 +0000

    solve abc019c

---
+++ abc/abc011_020/abc019/abc019c.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto a = readln.chomp.split.to!(int[]);
+
+    int[int] list;
+    foreach (x; a) {
+        int num = x;
+        while (num % 2 == 0) num /= 2;
+
+        ++list[num];
+    }
+
+    auto res = list.length;
+    res.writeln;
+}
