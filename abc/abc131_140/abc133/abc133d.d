39de8162852f6a8f96b37fbd1dacb9bf2cb1d0b7
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-08 14:53:31 +0000

    solve abc133d

---
+++ abc/abc131_140/abc133/abc133d.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    auto res = new int[](N);
+    foreach (i, a; A) {
+        res[0] += (i % 2 == 0 ? 1 : -1) * a;
+    }
+
+    foreach (i; 1 .. N) {
+        res[i] = A[i-1] * 2 - res[i-1];
+    }
+
+    writefln("%(%s %)", res);
+}
