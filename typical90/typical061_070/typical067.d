4c2102c817051b6973413ce892f14e83988ca30b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-09 22:56:58 +0900

    solve typical067

---
+++ typical90/typical061_070/typical067.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    auto input = readln.chomp.split;
+    auto N = input[0];
+    auto K = input[1].to!long;
+
+    foreach (_; 0 .. K) {
+        auto oct = N.to!long(8);
+
+        auto non = oct.to!string(9).to!(dchar[]);
+        foreach (ref n; non) {
+            if (n == '8') n = '5';
+        }
+
+        N = non.to!string;
+    }
+
+    N.writeln;
+}
