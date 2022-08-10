a95ae11e047a1284fa2bb611bb05c6130d0edeb8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-10 23:15:10 +0900

    solve abc183c

---
+++ abc/abc181_190/abc183/abc183c.d
@@ -0,0 +1,25 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto T = new int[][](N);
+    foreach (i; 0 .. N) T[i] = readln.chomp.split.to!(int[]);
+
+    auto arr = iota(1, N).array;
+    int res;
+    foreach (paths; arr.permutations) {
+        int time, from;
+        foreach (path; paths) {
+            time += T[from][path];
+            from = path;
+        }
+
+        time += T[from][0];
+
+        if (time == K) ++res;
+    }
+
+    res.writeln;
+}
