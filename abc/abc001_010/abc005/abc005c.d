72d3e7b6fc8a5eb9a823861368a85a054af57588
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-10 22:48:32 +0900

    solve abc005c

---
+++ abc/abc001_010/abc005/abc005c.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    int T;
+    readf("%d\n", T);
+
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int M;
+    readf("%d\n", M);
+
+    auto B = readln.chomp.split.to!(int[]);
+
+    while (!B.empty) {
+        if (A.empty) break;
+
+        if (A[0] <= B[0] && B[0] <= A[0] + T) B.popFront;
+
+        A.popFront;
+    }
+
+    writeln(B.empty ? "yes" : "no");
+}
