d308c4273d12fbbd326c54984f82810b9b17cc5c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-11 18:15:01 +0900

    solve abc182b

---
+++ abc/abc181_190/abc182/abc182b.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int res, cnt, mx = A.maxElement;
+    foreach (i; 2 .. mx+1) {
+        int num;
+        foreach (a; A) {
+            if (a % i == 0) ++num;
+        }
+
+        if (cnt < num) res = i, cnt = num;
+    }
+
+    res.writeln;
+}
