714fc7ce1ce6ab20ed2579b66d02769bed2361b5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-12 12:51:22 +0900

    solve abc181b

---
+++ abc/abc181_190/abc181/abc181b.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    long f(long x) {
+        return x * (x + 1) / 2;
+    }
+
+    long res;
+    foreach (_; 0 .. N) {
+        long A, B;
+        readf("%d %d\n", A, B);
+
+        res += f(B) - f(A-1);
+    }
+
+    res.writeln;
+}
