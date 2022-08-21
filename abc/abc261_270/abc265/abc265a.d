838c8ffde7b12b268e59b175bc54a2acd19d14b1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-21 21:03:31 +0900

    solve abc265a

---
+++ abc/abc261_270/abc265/abc265a.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int X, Y, N;
+    readf("%d %d %d\n", X, Y, N);
+
+    auto dp = new int[](N+1);
+    dp[1..$] = int.max;
+    foreach (i; 0 .. N) {
+        dp[i+1] = min(dp[i+1], dp[i]+X);
+        if (i + 3 <= N) dp[i+3] = min(dp[i+3], dp[i]+Y);
+    }
+
+    int res = dp[N];
+    res.writeln;
+}
