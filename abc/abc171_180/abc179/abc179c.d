9380bb455200acd1c7c69afccee11931a407023c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-24 23:44:40 +0900

    solve abc179c

---
+++ abc/abc171_180/abc179/abc179c.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int res;
+    foreach (A; 1 .. N) {
+        int D = N / A, R = N % A;
+        if (R == 0) --D;
+
+        res += D;
+    }
+
+    res.writeln;
+}
