d57cbd65b0419b53f0b5c17d42e42eed9d4360d1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-25 23:02:39 +0900

    solve abc178b

---
+++ abc/abc171_180/abc178/abc178b.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    auto a = readln.chomp.split.to!(long[]);
+
+    long res = long.min;
+    foreach (i; 0 .. 4) {
+        int x = i & 1, y = ((i >> 1) & 1) + 2;
+        res = max(res, a[x]*a[y]);
+    }
+
+    res.writeln;
+}
