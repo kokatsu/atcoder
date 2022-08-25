a11ebbe860e7e90abc3a1e1fc0bd3acc17b32122
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-25 22:48:36 +0900

    solve agc023a

---
+++ agc/agc021_030/agc023/agc023a.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    long[long] cnts;
+    cnts[0] = 1;
+
+    long res, S;
+    foreach (a; A) {
+        S += a;
+
+        if (S in cnts) res += cnts[S];
+
+        ++cnts[S];
+    }
+
+    res.writeln;
+}
