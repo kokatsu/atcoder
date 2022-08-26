723267bd256bcb41a698e50c1beb66c9a35acfb6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-26 22:20:32 +0900

    solve abc242e

---
+++ abc/abc241_250/abc242/abc242e.d
@@ -0,0 +1,34 @@
+import std;
+
+enum long MOD = 998244353;
+
+void main() {
+    int T;
+    readf("%d\n", T);
+
+    foreach (_; 0 .. T) {
+        long N;
+        readf("%d\n", N);
+
+        string S;
+        readf("%s\n", S);
+
+        long res, H = (N + 1) / 2;
+        foreach (i; 0 .. H) {
+            long cnt = S[i] - 'A';
+            res = (res * 26 + cnt) % MOD;
+        }
+
+        bool isOK = true;
+        foreach (i; H .. N) {
+            if (S[i] == S[N-i-1]) continue;
+            else {
+                isOK = (S[i] > S[N-i-1]);
+                break;
+            }
+        }
+
+        if (isOK) res = (res + 1) % MOD;
+        res.writeln;
+    }
+}
