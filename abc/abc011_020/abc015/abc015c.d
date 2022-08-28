90fc8ac3f851a203077363d4463e2a38df5e9b1b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-28 21:45:01 +0900

    solve abc015c

---
+++ abc/abc011_020/abc015/abc015c.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto T = new int[][](N);
+    foreach (i; 0 .. N) T[i] = readln.chomp.split.to!(int[]);
+
+    bool prob;
+
+    void f(int times, int xor) {
+        if (times >= N) {
+            prob |= (xor == 0);
+        }
+        else {
+            foreach (t; T[times]) {
+                f(times+1, xor^t);
+            }
+        }
+    }
+
+    f(0, 0);
+
+    writeln(prob ? "Found" : "Nothing");
+}
