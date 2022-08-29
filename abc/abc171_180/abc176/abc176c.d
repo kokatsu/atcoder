a58374a68b11239366fb2ff5a72e0d412cb3bc45
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-29 21:11:25 +0900

    solve abc176c

---
+++ abc/abc171_180/abc176/abc176c.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    long res, mx = A.front;
+    A.popFront;
+
+    foreach (a; A) {
+        if (mx <= a) mx = a;
+        else res += mx - a;
+    }
+
+    res.writeln;
+}
