2592d47fec11c98cb0ab11048bba5f97dd426cc7
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-30 23:27:25 +0900

    solve abc015d

---
+++ abc/abc011_020/abc015/abc015d.d
@@ -0,0 +1,29 @@
+import std;
+
+void main() {
+    int W;
+    readf("%d\n", W);
+
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    void f(ref int x, int y) {
+        x = max(x, y);
+    }
+
+    int res;
+    auto dp = new int[][](W+1, K+1);
+    foreach (_; 0 .. N) {
+        int A, B;
+        readf("%d %d\n", A, B);
+
+        foreach_reverse (i; 0 .. W-A+1) {
+            foreach_reverse (j; 0 .. K) {
+                f(dp[i+A][j+1], dp[i][j]+B);
+                f(res, dp[i+A][j+1]);
+            }
+        }
+    }
+
+    res.writeln;
+}
