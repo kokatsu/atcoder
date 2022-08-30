dae79d4466fd60d4800cf588f2274d48b5b9e952
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-30 23:30:45 +0900

    solve abc175a

---
+++ abc/abc171_180/abc175/abc175a.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    int res, cnt;
+    foreach (s; S) {
+        if (s == 'S') cnt = 0;
+        else ++cnt;
+
+        res = max(res, cnt);
+    }
+
+    res.writeln;
+}
