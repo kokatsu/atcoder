d3aaa5418df32cdaeaa4b92c463565ed4af04af8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-31 23:21:21 +0900

    solve abc174b

---
+++ abc/abc171_180/abc174/abc174b.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    long N, D;
+    readf("%d %d\n", N, D);
+
+    long res, S = D * D;
+    foreach (_; 0 .. N) {
+        long X, Y;
+        readf("%d %d\n", X, Y);
+
+        if (X * X + Y * Y <= S) ++res;
+    }
+
+    res.writeln;
+}
