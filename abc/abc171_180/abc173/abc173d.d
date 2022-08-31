70128cd7ec4ecd9a72752d785b00216fdcccfa5d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-31 23:58:14 +0900

    solve abc173d

---
+++ abc/abc171_180/abc173/abc173d.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    A.sort;
+
+    long res;
+    foreach (i; 1 .. N) {
+        res += A[N-i/2-1];
+    }
+
+    res.writeln;
+}
