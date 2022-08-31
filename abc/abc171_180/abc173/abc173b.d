65515fdbf1c64e0c3cd76465cb5b95139d39dc7f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-08-31 23:46:03 +0900

    solve abc173b

---
+++ abc/abc171_180/abc173/abc173b.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    string[] list = ["AC", "WA", "TLE", "RE"];
+
+    int[string] cnts;
+    foreach (l; list) cnts[l] = 0;
+
+    foreach (_; 0 .. N) {
+        string S;
+        readf("%s\n", S);
+
+        ++cnts[S];
+    }
+
+    foreach (l; list) writeln(l, " x ", cnts[l]);
+}
