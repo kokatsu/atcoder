c5b9fec87fbe8ce9fd8c807568bc501d379880d1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-03 21:45:06 +0900

    solve abc267d

---
+++ abc/abc261_270/abc267/abc267d.d
@@ -0,0 +1,25 @@
+import std;
+
+void main() {
+    long N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    void f(ref long x, long y) {
+        x = max(x, y);
+    }
+
+    auto dp = new long[](M+2);
+    dp[1..$] = long.min;
+    foreach (i, a; A) {
+        long lim = min(i, M);
+
+        foreach_reverse (j; 0 .. lim+1) {
+            f(dp[j+1], dp[j]+(j+1)*a);
+        }
+    }
+
+    long res = dp[M];
+    res.writeln;
+}
