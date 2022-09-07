df4273c762b3ceae042b1441f0b88f2957ff8e7f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-07 21:59:27 +0900

    solve abc171b

---
+++ abc/abc171_180/abc171/abc171b.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto p = readln.chomp.split.to!(int[]);
+
+    p.sort;
+
+    int res = p[0..K].sum;
+    res.writeln;
+}
