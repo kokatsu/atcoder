4fce5957ff5aa799840d58bf269990d4c8d476ed
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-07 22:05:04 +0900

    solve abc170b

---
+++ abc/abc161_170/abc170/abc170b.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    int X, Y;
+    readf("%d %d\n", X, Y);
+
+    bool isOK;
+    foreach (u; 0 .. X+1) {
+        int v = X - u;
+
+        isOK |= (u * 2 + v * 4 == Y);
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
