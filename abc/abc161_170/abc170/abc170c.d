3abf6af0c163cc57e06b00f424041913fdd57b96
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-07 22:23:14 +0900

    solve abc170c

---
+++ abc/abc161_170/abc170/abc170c.d
@@ -0,0 +1,36 @@
+import std;
+
+void main() {
+    int X, N;
+    readf("%d %d\n", X, N);
+
+    auto p = readln.chomp.split.to!(int[]);
+
+    int len = 102;
+
+    auto isContained = new bool[](len);
+    foreach (x; p) isContained[x] = true;
+
+    auto q = len.iota.array.filter!(i => !isContained[i]).array;
+    auto s = q.sort;
+
+    int res, diff = int.max;
+
+    auto lb = s.lowerBound(X+1);
+    if (!lb.empty) {
+        if (X - lb.back < diff) {
+            res = lb.back;
+            diff = X - lb.back;
+        }
+    }
+
+    auto ub = s.upperBound(X-1);
+    if (!ub.empty) {
+        if (ub.front - X < diff) {
+            res = ub.front;
+            diff = ub.front - X;
+        }
+    }
+
+    res.writeln;
+}
