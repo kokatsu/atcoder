ee0cc503f17ed19ed5ac2fe3f134040ed4b2675b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-09 20:47:43 +0900

    solve arc147a

---
+++ arc/arc141_150/arc147/arc147a.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int res;
+    auto rbt = A.redBlackTree!("a < b", true);
+    while (rbt.length > 1) {
+        ++res;
+
+        int mn = rbt.front;
+
+        int mx = rbt.back;
+        rbt.removeBack;
+
+        int r = mx % mn;
+        if (r > 0) rbt.insert(r);
+    }
+
+    res.writeln;
+}
