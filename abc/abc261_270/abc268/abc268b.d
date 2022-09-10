fcc0e85429fc75f4b45418a782d7ef086ae7c89d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-10 21:04:29 +0900

    solve abc268b

---
+++ abc/abc261_270/abc268/abc268b.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    string S, T;
+    readf("%s\n%s\n", S, T);
+
+    auto slen = S.length, tlen = T.length;
+
+    bool isOK;
+    if (slen <= tlen) {
+        isOK = (S == T[0..slen]);
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
