4a2ff511cb8818cb11d3f1db280dc1a05002d48c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-10 21:19:53 +0900

    solve abc268c

---
+++ abc/abc261_270/abc268/abc268c.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto p = readln.chomp.split.to!(int[]);
+
+    auto a = new int[](N), b = new int[](N), c = new int[](N);
+    foreach (i, x; p) {
+        ++a[(N+x-i-1)%N], ++b[(N+x-i)%N], ++c[(N+x-i+1)%N];
+    }
+
+    int res;
+    foreach (i; 0 .. N) {
+        res = max(res, a[i]+b[i]+c[i]);
+    }
+
+    res.writeln;
+}
