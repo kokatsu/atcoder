7c1d9ec84d784b6df8fc6ca7a8a1ca823a7af2af
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-11 22:25:23 +0900

    solve abc031c

---
+++ abc/abc031_040/abc031/abc031c.d
@@ -0,0 +1,30 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto a = readln.chomp.split.to!(int[]);
+
+    int res = int.min;
+    foreach (i; 0 .. N) {
+        int Tak = int.min, Aoki = int.min;
+
+        foreach (j; 0 .. N) {
+            if (i == j) continue;
+
+            int cnt, l = min(i, j), r = max(i, j), even, odd;
+            foreach (k; l .. r+1) {
+                (cnt++ & 1 ? even : odd) += a[k];
+            }
+
+            if (Aoki < even) {
+                Tak = odd, Aoki = even;
+            }
+        }
+
+        res = max(res, Tak);
+    }
+
+    res.writeln;
+}
