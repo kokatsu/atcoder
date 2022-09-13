a27f96185fad15d06b64863d942508f1d313ce75
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-13 22:00:27 +0900

    solve abc162c

---
+++ abc/abc161_170/abc162/abc162c.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int K;
+    readf("%d\n", K);
+
+    int res;
+    foreach (i; 1 .. K+1) {
+        foreach (j; 1 .. K+1) {
+            int g = gcd(i, j);
+
+            foreach (k; 1 .. K+1) {
+                res += gcd(g, k);
+            }
+        }
+    }
+
+    res.writeln;
+}
