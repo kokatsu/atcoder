884081a436762e6e8bc21b3da1029e0331f81df8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-13 21:57:10 +0900

    solve abc162b

---
+++ abc/abc161_170/abc162/abc162b.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    long f(long x) {
+        long y = N / x;
+        return  y * (y + 1) / 2 * x;
+    }
+
+    long res = f(1) - f(3) - f(5) + f(15);
+    res.writeln;
+}
