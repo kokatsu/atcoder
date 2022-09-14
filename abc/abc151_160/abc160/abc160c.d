d3815c84305d42b69ec59a8f39fe34ef070fc775
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-14 23:38:42 +0900

    solve abc160c

---
+++ abc/abc151_160/abc160/abc160c.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int K, N;
+    readf("%d %d\n", K, N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    auto B = A ~ (A.dup[] += K);
+
+    int res = int.max;
+    foreach (i; 0 .. N) {
+        res = min(res, B[i+N-1]-B[i]);
+    }
+
+    res.writeln;
+}
