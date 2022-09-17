ee2da80263e138e48c185a8b6b4a8a699ded9481
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-17 23:48:24 +0900

    solve abc269e

---
+++ abc/abc261_270/abc269/abc269e.d
@@ -0,0 +1,36 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int A, B, C, D;
+    A = C = 1;
+    B = D = N + 1;
+
+    while (B - A > 1) {
+        int mid = (A + B) / 2;
+
+        writefln("? %d %d %d %d", A, mid-1, 1, N);
+        stdout.flush;
+
+        int ret;
+        readf("%d\n", ret);
+
+        (mid - A <= ret ? A : B) = mid;
+    }
+
+    while (D - C > 1) {
+        int mid = (C + D) / 2;
+
+        writefln("? %d %d %d %d", 1, N, C, mid-1);
+        stdout.flush;
+
+        int ret;
+        readf("%d\n", ret);
+
+        (mid - C <= ret ? C : D) = mid;
+    }
+
+    writefln("! %d %d", A, C);
+}
