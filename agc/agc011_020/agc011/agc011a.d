21bce49cdeeac6eea7b8d6956b44434641cb461a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-18 22:52:48 +0900

    solve agc011a

---
+++ agc/agc011_020/agc011/agc011a.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    int N, C, K;
+    readf("%d %d %d\n", N, C, K);
+
+    auto T = new int[](N);
+    foreach (i; 0 .. N) readf("%d\n", T[i]);
+
+    T.sort;
+
+    int res = 1, cnt, stp = T[0];
+    foreach (t; T) {
+        if (stp <= t && t <= stp + K && cnt < C) {
+            ++cnt;
+        }
+        else {
+            ++res, cnt = 1, stp = t;
+        }
+    }
+
+    res.writeln;
+}
