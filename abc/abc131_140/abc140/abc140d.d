430235d8476a2f7e9dede993e5c3410abbca6ede
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-20 20:21:44 +0900

    solve abc140d

---
+++ abc/abc131_140/abc140/abc140d.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    int N, K;
+    string S;
+    readf("%d %d\n%s\n", N, K, S);
+
+    int cnt;
+    foreach (i; 1 .. N) {
+        if (S[i] == S[i-1]) ++cnt;
+    }
+
+    int res = min(N-1, cnt+K*2);
+    res.writeln;
+}
