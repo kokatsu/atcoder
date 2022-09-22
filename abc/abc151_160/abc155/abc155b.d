93c7661efea9409d4ff439f8a1bf9b6691dba0dc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-22 22:27:39 +0900

    solve abc155b

---
+++ abc/abc151_160/abc155/abc155b.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    bool isOK = true;
+    foreach (a; A) {
+        if (a % 2 == 1) continue;
+
+        isOK &= (a % 3 == 0 || a % 5 == 0);
+    }
+
+    writeln(isOK ? "APPROVED" : "DENIED");
+}
