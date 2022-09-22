a1f6c2c5281d7058dc930f8710f6a16017c384b0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-22 22:33:15 +0900

    solve abc155c

---
+++ abc/abc151_160/abc155/abc155c.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int[string] list;
+    int mx;
+    foreach (_; 0 .. N) {
+        string S;
+        readf("%s\n", S);
+
+        mx = max(mx, ++list[S]);
+    }
+
+    string[] res;
+    foreach (key, val; list) {
+        if (val == mx) res ~= key;
+    }
+
+    res.sort;
+
+    foreach (r; res) r.writeln;
+}
