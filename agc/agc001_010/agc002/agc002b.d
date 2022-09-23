74ea879be032c4f299d5034889a2f95757b1f455
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-23 21:14:54 +0900

    solve agc002b

---
+++ agc/agc001_010/agc002/agc002b.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto cnts = new int[](N+1);
+    cnts[1..N+1] = 1;
+
+    auto boxes = new bool[](N+1);
+    boxes[1] = true;
+
+    foreach (_; 0 .. M) {
+        int x, y;
+        readf("%d %d\n", x, y);
+
+        --cnts[x], ++cnts[y];
+        if (boxes[x]) boxes[y] = true;
+        if (cnts[x] < 1) boxes[x] = false;
+    }
+
+    auto res = boxes.count(true);
+    res.writeln;
+}
