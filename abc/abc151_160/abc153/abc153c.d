f4abf54c25702a69a7207e90519c3f738279fb85
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-28 22:41:15 +0900

    solve abc153c

---
+++ abc/abc151_160/abc153/abc153c.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    long N, K;
+    readf("%d %d\n", N, K);
+
+    auto H = readln.chomp.split.to!(long[]);
+
+    H.sort;
+
+    long res, L = max(0, N-K);
+    foreach (i; 0 .. L) res += H[i];
+
+    res.writeln;
+}
