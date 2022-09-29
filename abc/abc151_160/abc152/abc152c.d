cde3d5bb336da7f28ddced9383f174142c47f704
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-29 22:07:46 +0900

    solve abc152c

---
+++ abc/abc151_160/abc152/abc152c.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto P = readln.chomp.split.to!(long[]);
+
+    long res, mx = P.front;
+    foreach (p; P) {
+        if (p <= mx) ++res;
+
+        mx = min(mx, p);
+    }
+
+    res.writeln;
+}
