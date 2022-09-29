9aae3a81c01d164e222279f4876c02495a152de2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-09-29 21:49:29 +0900

    solve abc054b

---
+++ abc/abc051_060/abc054/abc054b.d
@@ -0,0 +1,27 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = new string[](N);
+    foreach (i; 0 .. N) readf("%s\n", A[i]);
+
+    auto B = new string[](M);
+    foreach (i; 0 .. M) readf("%s\n", B[i]);
+
+    bool isOK;
+    foreach (i; 0 .. N-M+1) {
+        foreach (j; 0 .. N-M+1) {
+            bool check = true;
+
+            foreach (k; 0 .. M) {
+                check &= (A[j+k][i..i+M] == B[k]);
+            }
+
+            isOK |= check;
+        }
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
