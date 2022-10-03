b52dfd8f3b421f72f35dd1784194c76385c44eec
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-03 22:59:59 +0900

    solve abc006b

---
+++ abc/abc001_010/abc006/abc006b.d
@@ -0,0 +1,22 @@
+import std;
+
+enum int MOD = 10007;
+
+void main() {
+    int n;
+    readf("%d\n", n);
+
+    if (n < 3) {
+        writeln(0);
+        return;
+    }
+
+    auto a = new int[](n+1);
+    a[3] = 1;
+    foreach (i; 4 .. n+1) {
+        a[i] = (a[i-1] + a[i-2] + a[i-3]) % MOD;
+    }
+
+    int res = a[n];
+    res.writeln;
+}
