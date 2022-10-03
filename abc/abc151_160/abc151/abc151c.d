b79ddbc336721c8f831ca821c380806eb1781fab
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-03 23:12:01 +0900

    solve abc151c

---
+++ abc/abc151_160/abc151/abc151c.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    int AC, WA;
+    auto isAC = new bool[](N+1);
+    auto cntWA = new int[](N+1);
+    foreach (_; 0 .. M) {
+        int p;
+        string S;
+        readf("%d %s\n", p, S);
+
+        if (isAC[p]) continue;
+
+        if (S == "AC") isAC[p] = true, ++AC, WA += cntWA[p];
+        else ++cntWA[p];
+    }
+
+    writeln(AC, " ", WA);
+}
