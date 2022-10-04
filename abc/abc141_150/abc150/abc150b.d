8ef338f13164f70e6ff108a80d433fc310fb74d5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-05 00:14:22 +0900

    solve abc150b

---
+++ abc/abc141_150/abc150/abc150b.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int N;
+    string S;
+    readf("%d\n%s\n", N, S);
+
+    int res;
+    foreach (i; 0 .. N-2) {
+        if (S[i..i+3] == "ABC") ++res;
+    }
+
+    res.writeln;
+}
