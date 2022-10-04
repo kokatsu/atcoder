f7f1eff3e442c133f4a118cede805d2e914a043e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-05 00:06:18 +0900

    solve abc188d

---
+++ abc/abc181_190/abc188/abc188d.d
@@ -0,0 +1,34 @@
+import std;
+
+struct Plan {
+    long day, cost;
+}
+
+void main() {
+    long N, C;
+    readf("%d %d\n", N, C);
+
+    auto heap = new BinaryHeap!(Array!Plan, "a.day > b.day")();
+    foreach (_; 0 .. N) {
+        long a, b, c;
+        readf("%d %d %d\n", a, b, c);
+
+        heap.insert(Plan(a-1, c));
+        heap.insert(Plan(b, -c));
+    }
+
+    long res, day, cost;
+    while (!heap.empty) {
+        auto f = heap.front;
+        heap.popFront;
+
+        if (f.day > day) {
+            res += min(C, cost) * (f.day - day);
+            day = f.day;
+        }
+
+        cost += f.cost;
+    }
+
+    res.writeln;
+}
