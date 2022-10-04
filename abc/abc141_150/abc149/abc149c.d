448ef7de6475c3351fbf040aad25d782139f5100
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-05 00:27:58 +0900

    solve abc149c

---
+++ abc/abc141_150/abc149/abc149c.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    int X;
+    readf("%d\n", X);
+
+    int N = 2 * 10 ^^ 5;
+    auto sieve = new bool[](N+1);
+    sieve[2..N+1] = true;
+    int d = 2;
+    while (d * d <= N) {
+        if (sieve[d]) {
+            foreach (i; iota(d*d, N+1, d)) {
+                sieve[i] = false;
+            }
+        }
+        ++d;
+    }
+
+    int res = X;
+    while (!sieve[res]) {
+        ++res;
+    }
+
+    res.writeln;
+}
