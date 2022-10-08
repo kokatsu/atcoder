760abcf26e8d95d361259b9084d377f0189befa1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-08 21:10:57 +0900

    solve abc272c

---
+++ abc/abc271_280/abc272/abc272c.d
@@ -0,0 +1,27 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int[] odds, evens;
+    foreach (a; A) {
+        (a % 2 == 1 ? odds : evens) ~= a;
+    }
+
+    int res = -1;
+
+    if (odds.length > 1) {
+        odds.sort!"a > b";
+        res = max(res, odds[0]+odds[1]);
+    }
+
+    if (evens.length > 1) {
+        evens.sort!"a > b";
+        res = max(res, evens[0]+evens[1]);
+    }
+
+    res.writeln;
+}
