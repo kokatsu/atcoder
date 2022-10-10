6f6d6286a7bb7a9d3094391c76cd319ca6b24025
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-10 22:51:36 +0900

    solve abc146c

---
+++ abc/abc141_150/abc146/abc146c.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    long A, B, X;
+    readf("%d %d %d\n", A, B, X);
+
+    long f(long x, long y) {
+        return A * x + B * y;
+    }
+
+    long d = 1;
+    while (true) {
+        long l = 10 ^^ (d - 1) - 1, r = 10 ^^ d;
+        if (f(r-1, d) >= X) {
+            while (r - l > 1) {
+                long mid = (l + r) / 2;
+                (f(mid, d) <= X ? l : r) = mid;
+            }
+
+            min(10^^9, l).writeln;
+            return;
+        }
+
+        ++d;
+    }
+}
