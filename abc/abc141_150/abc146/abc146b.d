d904b557b82b0d6677310fa5a596570928ddaa45
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-10 22:39:51 +0900

    solve abc146b

---
+++ abc/abc141_150/abc146/abc146b.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int N;
+    string S;
+    readf("%d\n%s\n", N, S);
+
+    auto res = new dchar[](S.length);
+    foreach (i, s; S) {
+        res[i] = to!dchar('A'+((s-'A')+N)%26);
+    }
+
+    res.writeln;
+}
