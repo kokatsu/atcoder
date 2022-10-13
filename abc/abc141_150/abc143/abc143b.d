37d5bf9523d85ab4c17e7f90ec597f6645059819
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-14 01:31:30 +0900

    solve abc143b

---
+++ abc/abc141_150/abc143/abc143b.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto d = readln.chomp.split.to!(long[]);
+
+    long res;
+    foreach (i; 0 .. N-1) {
+        foreach (j; i+1 .. N) {
+            res += d[i] * d[j];
+        }
+    }
+
+    res.writeln;
+}
