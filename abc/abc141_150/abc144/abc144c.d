7ae477b173ac4f512e3ddd546ba8f5ac391d5a3d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-13 22:49:25 +0900

    solve abc144c

---
+++ abc/abc141_150/abc144/abc144c.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    long res = long.max, L = 10 ^^ 6 + 1;
+    foreach (i; 1 .. L) {
+        if (N % i != 0) continue;
+
+        res = min(res, i+N/i-2);
+    }
+
+    res.writeln;
+}
