501680add6a5e51b727178a9a872c5a1a9c59b86
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-16 21:23:33 +0900

    solve abc142c

---
+++ abc/abc141_150/abc142/abc142c.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    auto res = iota(1, N+1).array;
+
+    zip(A, res).sort!"a[0] < b[0]";
+
+    writefln("%(%s %)", res);
+}
