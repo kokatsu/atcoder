635ba76df743cf5d5fd38ecbcd4df5abb4383aa9
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-16 21:14:35 +0900

    solve arc006c

---
+++ arc/arc001_010/arc006/arc006c.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto rbt = new RedBlackTree!(int, "a < b", true)();
+    foreach (_; 0 .. N) {
+        int w;
+        readf("%d\n", w);
+
+        auto u = rbt.upperBound(w-1);
+        if (!u.empty) rbt.removeKey(u.front);
+
+        rbt.insert(w);
+    }
+
+    auto res = rbt.length;
+    res.writeln;
+}
