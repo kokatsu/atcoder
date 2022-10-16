0b449b0ce8d5d601d0084bbc16106dd00acb42eb
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-17 00:28:34 +0900

    solve agc038a

---
+++ agc/agc031_040/agc038/agc038a.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    int H, W, A, B;
+    readf("%d %d %d %d\n", H, W, A, B);
+
+    auto res = new int[][](H, W);
+    foreach (i; 0 .. H) {
+        foreach (j; 0 .. W) {
+            res[i][j] = (((i < B) ^ (j < A)) ? 1 : 0);
+        }
+    }
+
+    foreach (r; res) writefln("%(%s%)", r);
+}
