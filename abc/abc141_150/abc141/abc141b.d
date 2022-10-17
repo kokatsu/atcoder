573d672c91e96a8ac0287786524886b1bcc88c6f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-18 00:41:03 +0900

    solve abc141b

---
+++ abc/abc141_150/abc141/abc141b.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    bool isOK = true;
+    foreach (i, s; S) {
+        if (i % 2 == 0) isOK &= "RUD".canFind(s);
+        else isOK &= "LUD".canFind(s);
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
