5ecdaed826d4c65a566ce403066126740e81c306
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-18 00:48:04 +0900

    solve abc141c

---
+++ abc/abc141_150/abc141/abc141c.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    int N, K, Q;
+    readf("%d %d %d\n", N, K, Q);
+
+    auto cnts = new int[](N);
+    foreach (i; 0 .. Q) {
+        int A;
+        readf("%d\n", A);
+
+        ++cnts[A-1];
+    }
+
+    auto P = new int[](N);
+    P[] = K;
+    foreach (i; 0 .. N) {
+        P[i] -= Q - cnts[i];
+    }
+
+    foreach (i; 0 .. N) {
+        writeln(P[i] > 0 ? "Yes" : "No");
+    }
+}
