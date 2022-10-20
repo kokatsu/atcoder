c05cb24d61a3facbb0564b492bdedfa06c52f198
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-20 19:49:00 +0900

    solve abc140c

---
+++ abc/abc131_140/abc140/abc140c.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto B = readln.chomp.split.to!(int[]);
+
+    int res = B[N-2], pre = int.max;
+    foreach (b; B) {
+        res += min(pre, b);
+        pre = b;
+    }
+
+    res.writeln;
+}
