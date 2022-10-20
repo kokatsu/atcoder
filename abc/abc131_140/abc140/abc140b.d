ab36ae38a2544f07fcddf063dd1a1a6cf99ec4ea
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-20 19:43:02 +0900

    solve abc140b

---
+++ abc/abc131_140/abc140/abc140b.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+    auto B = readln.chomp.split.to!(int[]);
+    auto C = readln.chomp.split.to!(int[]);
+
+    A[] -= 1;
+
+    int res, pre = -2;
+    foreach (i; 0 .. N) {
+        res += B[A[i]];
+
+        if (pre + 1 == A[i]) res += C[pre];
+
+        pre = A[i];
+    }
+
+    res.writeln;
+}
