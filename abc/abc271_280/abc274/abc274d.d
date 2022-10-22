23b51362395c061feca2a4a46336fe866ee94f9f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-22 22:23:18 +0900

    solve abc274d

---
+++ abc/abc271_280/abc274/abc274d.d
@@ -0,0 +1,30 @@
+import std;
+
+void main() {
+    int N, x, y;
+    readf("%d %d %d\n", N, x, y);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    auto xMove = A[2..$].stride(2), yMove = A[1..$].stride(2);
+    bool[int] xList, yList;
+    xList[A[0]] = yList[0]= true;
+    foreach (xm; xMove) {
+        bool[int] nxt;
+        foreach (key; xList.keys) {
+            nxt[key-xm] = nxt[key+xm] = true;
+        }
+        xList = nxt;
+    }
+
+    foreach (ym; yMove) {
+        bool[int] nxt;
+        foreach (key; yList.keys) {
+            nxt[key-ym] = nxt[key+ym] = true;
+        }
+        yList = nxt;
+    }
+
+    bool isOK = (x in xList) && (y in yList);
+    writeln(isOK ? "Yes" : "No");
+}
