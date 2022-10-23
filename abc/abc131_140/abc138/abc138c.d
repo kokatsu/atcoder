079e5f358678160df96ec5ab6ed630a8980df511
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-23 16:35:27 +0900

    solve abc138c

---
+++ abc/abc131_140/abc138/abc138c.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto v = readln.chomp.split.to!(real[]);
+
+    v.sort;
+
+    real res = v[0];
+    foreach (i; 1 .. N) res = (res + v[i]) / 2.0;
+
+    writefln("%.10f", res);
+}
