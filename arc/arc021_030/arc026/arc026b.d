d59766b2a906559ff769fc4f59e0d5216e52ab27
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-24 21:12:02 +0900

    solve arc026b

---
+++ arc/arc021_030/arc026/arc026b.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    long S = (N == 1 ? 0 : 1), d = 2;
+    while (d * d <= N) {
+        if (N % d == 0) {
+            S += d;
+            if (d * d != N) S += N / d;
+        }
+
+        ++d;
+    }
+
+    string res = "Perfect";
+    if (S < N) res = "Deficient";
+    if (S > N) res = "Abundant";
+
+    res.writeln;
+}
