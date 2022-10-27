c11f6b1287865d0cdfb16a96ae2052f7bbe5b6ce
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-28 00:20:04 +0900

    solve abc136c

---
+++ abc/abc131_140/abc136/abc136c.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto H = readln.chomp.split.to!(int[]);
+
+    bool isOK = true;
+    int cnt = 1;
+    foreach (i; 1 .. N) {
+        if (H[i] > H[i-1]) {
+            ++cnt;
+        }
+        else if (H[i] < H[i-1]) {
+            isOK &= (H[i-1] - H[i] <= cnt);
+            cnt = 0;
+        }
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
