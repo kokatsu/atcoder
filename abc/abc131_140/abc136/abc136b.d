0e738aaf339c29c09727510dd2f17bfacf81db60
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-28 00:15:00 +0900

    solve abc136b

---
+++ abc/abc131_140/abc136/abc136b.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int res;
+    foreach (i; 1 .. N+1) {
+        auto S = i.to!string;
+        if (S.length % 2 == 1) ++res;
+    }
+
+    res.writeln;
+}
