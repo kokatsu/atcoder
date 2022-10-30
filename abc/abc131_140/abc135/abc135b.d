135b6218bd7dc412989fc7ba37c9ba25b67bffe6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-30 21:06:31 +0900

    solve abc135b

---
+++ abc/abc131_140/abc135/abc135b.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto p = readln.chomp.split.to!(int[]);
+
+    int cnt;
+    foreach (i, x; p) {
+        if (x != i + 1) ++cnt;
+    }
+
+    writeln(cnt <= 2 ? "YES" : "NO");
+}
