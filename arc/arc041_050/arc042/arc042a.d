7e7d07777093d6911671feea6ba6aa5192f5a02b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-31 20:57:22 +0900

    solve arc042a

---
+++ arc/arc041_050/arc042/arc042a.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto a = new int[](M);
+    foreach (i; 0 .. M) readf("%d\n", a[i]);
+
+    auto used = new bool[](N+1);
+    foreach_reverse (x; a) {
+        if (used[x]) continue;
+
+        x.writeln;
+        used[x] = true;
+    }
+
+    foreach (i; 1 .. N+1) {
+        if (used[i]) continue;
+
+        i.writeln;
+    }
+}
