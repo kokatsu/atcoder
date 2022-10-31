50455d6d1c690f6a5c1b1bd99c214cdcb5e2e900
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-10-31 21:06:09 +0900

    solve abc134c

---
+++ abc/abc131_140/abc134/abc134c.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = new int[](N);
+    foreach (i; 0 .. N) readf("%d\n", A[i]);
+
+    auto B = A.dup;
+
+    B.sort!"a > b";
+
+    foreach (a; A) {
+        writeln(a == B[0] ? B[1] : B[0]);
+    }
+}
