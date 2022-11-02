278b2689564051da1782ac87b8d528a432b4ecc3
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-02 22:35:36 +0900

    solve tenka1_2018c

---
+++ others/tenka1/tenka1_2018/tenka1_2018c.d
@@ -0,0 +1,34 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto A = new long[](N);
+    foreach (i; 0 .. N) readf("%d\n", A[i]);
+
+    long D = N / 2, R = N % 2;
+
+    long f() {
+        long x, y;
+
+        foreach (i; 0 .. D) {
+            x += A[D-i-1] * 2, y += A[D+i+R] * 2;
+        }
+
+        if (R == 1) y += A[D] - A[D+1];
+        else x -= A[D-1], y -= A[D];
+
+        return abs(x-y);
+    }
+
+    A.sort;
+
+    long res = f();
+
+    A.reverse;
+
+    res = max(res, f());
+
+    res.writeln;
+}
