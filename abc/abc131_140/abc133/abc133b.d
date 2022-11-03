70a5cb1fc9bc9021ceaa20e2b3e19de923816b8c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-03 21:50:26 +0900

    solve abc133b

---
+++ abc/abc131_140/abc133/abc133b.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    int N, D;
+    readf("%d %d\n", N, D);
+
+    auto X = new int[][](N);
+    foreach (i; 0 .. N) X[i] = readln.chomp.split.to!(int[]);
+
+    int res;
+    foreach (i; 0 .. N-1) {
+        foreach (j; i+1 .. N) {
+            int d;
+            foreach (a, b; zip(X[i], X[j])) {
+                d += abs(a-b) ^^ 2;
+            }
+
+            int r = d.to!real.sqrt.floor.to!int;
+            if (r * r == d) ++res;
+        }
+    }
+
+    res.writeln;
+}
