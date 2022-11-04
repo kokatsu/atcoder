ba42dcc3f084d6df5fd6937b0ba3d79cab8ffb91
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-04 23:19:50 +0900

    solve abc132c

---
+++ abc/abc131_140/abc132/abc132c.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto d = readln.chomp.split.to!(int[]);
+
+    auto s = d.sort;
+
+    int res;
+    foreach (i; 1 .. d.back+1) {
+        auto lb = s.lowerBound(i);
+
+        if (lb.length == N / 2) ++res;
+    }
+
+    res.writeln;
+}
