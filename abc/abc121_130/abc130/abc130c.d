6001e7299eeb0d75ecca689dc94063e038048407
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-07 22:11:30 +0900

    solve abc130c

---
+++ abc/abc121_130/abc130/abc130c.d
@@ -0,0 +1,11 @@
+import std;
+
+void main() {
+    long W, H, x, y;
+    readf("%d %d %d %d\n", W, H, x, y);
+
+    real S = to!real(W*H) / 2.0;
+    long ways = (x * 2 == W && y * 2 == H ? 1 : 0);
+
+    writefln("%.10f %d", S, ways);
+}
