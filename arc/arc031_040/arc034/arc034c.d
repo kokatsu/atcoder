9946d4ce0f9503229f3b0213b05fd68b72dc91f0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-07 21:50:05 +0900

    solve arc034c

---
+++ arc/arc031_040/arc034/arc034c.d
@@ -0,0 +1,34 @@
+import std;
+
+enum long MOD = 10 ^^ 9 + 7;
+
+void mulMod(ref long x, long y) {
+    x = (x * y) % MOD;
+}
+
+void main() {
+    long A, B;
+    readf("%d %d\n", A, B);
+
+    long[long] cnts;
+    foreach (i; B+1 .. A+1) {
+        long rem = i, d = 2;
+        while (d * d <= rem) {
+            while (rem % d == 0) {
+                ++cnts[d];
+                rem /= d;
+            }
+
+            ++d;
+        }
+
+        if (rem > 1) ++cnts[rem];
+    }
+
+    long res = 1;
+    foreach (val; cnts.byValue) {
+        mulMod(res, val+1);
+    }
+
+    res.writeln;
+}
