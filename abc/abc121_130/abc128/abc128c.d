9e6d62f1790b0ddd4cf46b169fa84d2714ed58b8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-09 22:40:15 +0900

    solve abc128c

---
+++ abc/abc121_130/abc128/abc128c.d
@@ -0,0 +1,36 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto s = new int[][](M);
+    foreach (i; 0 .. M) {
+        s[i] = readln.chomp.split.to!(int[]);
+        s[i].popFront;
+        s[i][] -= 1;
+    }
+
+    auto p = readln.chomp.split.to!(int[]);
+
+    int res;
+    foreach (i; 0 .. 1<<N) {
+        bool isOK = true;
+
+        foreach (j; 0 .. M) {
+            int cnt;
+
+            foreach (x; s[j]) {
+                if ((i >> x) & 1) ++cnt;
+            }
+
+            cnt %= 2;
+
+            isOK &= (cnt == p[j]);
+        }
+
+        if (isOK) ++res;
+    }
+
+    res.writeln;
+}
