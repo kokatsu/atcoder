716f75f959aafdfe1dd7e43404b2a578768f9788
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-10 21:15:05 +0900

    solve abc129b

---
+++ abc/abc121_130/abc129/abc129b.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto W = readln.chomp.split.to!(int[]);
+
+    int S1, S2 = W.sum;
+
+    int res = S2;
+    foreach (w; W) {
+        S1 += w, S2 -= w;
+
+        res = min(res, abs(S1-S2));
+    }
+
+    res.writeln;
+}
