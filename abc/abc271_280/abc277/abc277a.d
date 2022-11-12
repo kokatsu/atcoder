f6a528ad46c8b3f50da96aa3c1c394a782486a62
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-12 21:02:16 +0900

    solve abc277a

---
+++ abc/abc271_280/abc277/abc277a.d
@@ -0,0 +1,12 @@
+import std;
+
+void main() {
+    int N, X;
+    readf("%d %d\n", N, X);
+
+    auto P = readln.chomp.split.to!(int[]);
+
+    foreach (i, p; P) {
+        if (p == X) writeln(i+1);
+    }
+}
