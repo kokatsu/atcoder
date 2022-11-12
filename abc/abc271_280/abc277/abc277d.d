f3fd3750eb542e82c6daa72fd73f20dc49c5e3fb
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-12 21:56:48 +0900

    solve abc277d

---
+++ abc/abc271_280/abc277/abc277d.d
@@ -0,0 +1,59 @@
+import std;
+
+void main() {
+    long N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    BinaryHeap!(Array!long, "a < b")[long] heaps;
+    long[] list;
+    foreach (a; A) {
+        long m = a % M;
+
+        if (!(m in heaps)) {
+            auto tmp = Array!long([a]);
+            heaps[m] = heapify!"a < b"(tmp);
+            list ~= a;
+        }
+        else {
+            heaps[m].insert(a);
+        }
+    }
+
+    list.sort!"a > b";
+
+    long[long] nums;
+    bool[long] seen;
+
+    long func(long x) {
+        seen[x] = true;
+        long m = x % M, f = heaps[m].front, s = f;
+        heaps[m].popFront;
+
+        while (!heaps[m].empty && heaps[m].front == f) {
+            s += f;
+            heaps[m].popFront;
+        }
+
+        long nxt = (x + 1) % M;
+        if (nxt in heaps) {
+            if (nxt in nums) s += nums[nxt];
+            else {
+                if (!heaps[nxt].empty) s += func(nxt);
+            }
+        }
+
+        nums[x] = s;
+        return s;
+    }
+
+    long num;
+    foreach (l; list) {
+        if (l in seen) continue;
+        num = max(num, func(l));
+    }
+
+    long res = A.sum - num;
+    res.writeln;
+}
