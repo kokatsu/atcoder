f39386e178481f6e7074d08e3001d1d472a25393
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-13 22:10:05 +0900

    solve abc127c

---
+++ abc/abc121_130/abc127/abc127c.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    int A = 1, B = N;
+    foreach (_; 0 .. M) {
+        int L, R;
+        readf("%d %d\n", L, R);
+
+        A = max(A, L), B = min(B, R);
+    }
+
+    int res = max(0, B-A+1);
+    res.writeln;
+}
