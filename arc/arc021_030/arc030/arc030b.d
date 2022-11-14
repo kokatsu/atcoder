798e2a44ebacac7bad3aefcdafe6034280702472
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-14 22:58:59 +0900

    solve arc030b

---
+++ arc/arc021_030/arc030/arc030b.d
@@ -0,0 +1,35 @@
+import std;
+
+void main() {
+    int n, x;
+    readf("%d %d\n", n, x);
+
+    auto h = readln.chomp.split.to!(int[]);
+
+    auto tree = new int[][](n);
+    foreach (_; 0 .. n-1) {
+        int a, b;
+        readf("%d %d\n", a, b);
+
+        --a, --b;
+        tree[a] ~= b, tree[b] ~= a;
+    }
+
+    int f(int pos, int par = -1) {
+        int ret;
+
+        foreach (e; tree[pos]) {
+            if (e == par) continue;
+
+            int num = f(e, pos);
+            if (h[e] == 1 || num > 0) {
+                ret += num + 2;
+            }
+        }
+
+        return ret;
+    }
+
+    int res = f(x-1);
+    res.writeln;
+}
