9014958891882d89f4509094ab0f50a89917e08e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-15 22:59:16 +0900

    solve abc125b

---
+++ abc/abc121_130/abc125/abc125b.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto V = readln.chomp.split.to!(int[]);
+    auto C = readln.chomp.split.to!(int[]);
+
+    int res;
+    foreach (v, c; zip(V, C)) {
+        res += max(0, v-c);
+    }
+
+    res.writeln;
+}
