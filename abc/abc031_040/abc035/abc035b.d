47faf0d8639cd7bed7c6d3cc3dcd239157e28e1c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-15 22:53:13 +0900

    solve abc035b

---
+++ abc/abc031_040/abc035/abc035b.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    string S;
+    int T;
+    readf("%s\n%d\n", S, T);
+
+    int x, y, z;
+    foreach (s; S) {
+        if (s == 'L') --x;
+        if (s == 'R') ++x;
+        if (s == 'U') ++y;
+        if (s == 'D') --y;
+        if (s == '?') ++z;
+    }
+
+    int d = x.abs + y.abs;
+
+    int res;
+    if (T == 1) res = d + z;
+    else res = (d >= z ? d - z : (z - d) % 2);
+
+    res.writeln;
+}
