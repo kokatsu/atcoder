352d2e8a55c3deaaf3803da5306ddf59d3d2a624
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-16 21:21:23 +0900

    solve abc124c

---
+++ abc/abc121_130/abc124/abc124c.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    int x, y;
+    foreach (i, s; S) {
+        if ((i % 2 == 0) == (s == '0')) ++x;
+        else ++y;
+    }
+
+    int res = min(x, y);
+    res.writeln;
+}
