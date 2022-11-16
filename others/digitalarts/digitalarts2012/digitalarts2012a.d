321651fe1d2803a708e8886d6f721125e4d8c898
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-16 21:09:11 +0900

    solve digitalarts2012a

---
+++ others/digitalarts/digitalarts2012/digitalarts2012a.d
@@ -0,0 +1,34 @@
+import std;
+
+void main() {
+    auto s = readln.chomp.split;
+
+    int N;
+    readf("%d\n", N);
+
+    foreach (_; 0 .. N) {
+        string t;
+        readf("%s\n", t);
+
+        auto a = t.length;
+
+        foreach (ref x; s) {
+            auto b = x.length;
+            if (b != a) continue;
+
+            bool isOK = true;
+            foreach (u, v; zip(t, x)) {
+                if (u == '*' || u == v) continue;
+
+                isOK = false;
+            }
+
+            if (isOK) {
+                x = '*'.repeat(a).array.to!string;
+            }
+        }
+    }
+
+    auto res = s.join(" ");
+    res.writeln;
+}
