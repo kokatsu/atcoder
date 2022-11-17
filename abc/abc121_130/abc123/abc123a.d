e7a78de0f7d10cd48c2dc73866b8a73f624f6478
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-17 21:45:22 +0900

    solve abc123a

---
+++ abc/abc121_130/abc123/abc123a.d
@@ -0,0 +1,12 @@
+import std;
+
+void main() {
+    auto a = new int[](5);
+    foreach (i; 0 .. 5) readf("%d\n", a[i]);
+
+    int k;
+    readf("%d\n", k);
+
+    bool isOK = (a[4] - a[0] <= k);
+    writeln(isOK ? "Yay!" : ":(");
+}
