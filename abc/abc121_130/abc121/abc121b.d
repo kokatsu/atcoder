2acdeaf0359acedb2e6577586aa55ba9a3e80b36
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-20 21:12:16 +0900

    solve abc121b

---
+++ abc/abc121_130/abc121/abc121b.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N, M, C;
+    readf("%d %d %d\n", N, M, C);
+
+    auto B = readln.chomp.split.to!(int[]);
+
+    int res;
+    foreach (_; 0 .. N) {
+        auto A = readln.chomp.split.to!(int[]);
+
+        int num = C;
+        foreach (a, b; zip(A, B)) {
+            num += a * b;
+        }
+
+        if (num > 0) ++res;
+    }
+
+    res.writeln;
+}
