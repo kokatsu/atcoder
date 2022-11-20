7e8c80d19ebd0b54a4b1e4a6171a1b7cb66a525d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-20 21:05:04 +0900

    solve abc006c

---
+++ abc/abc001_010/abc006/abc006c.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    foreach (j; 0 .. N+1) {
+        int a = N - j, b = M - j * 3;
+        if (b < 0) break;
+        if (b % 2 == 1) continue;
+
+        b /= 2;
+
+        int i = a * 2 - b, k = b - a;
+        if (i >= 0 && k >= 0) {
+            writefln("%d %d %d", i, j, k);
+            return;
+        }
+    }
+
+    writefln("%d %d %d", -1, -1, -1);
+}
