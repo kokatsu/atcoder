82ff096da030c93332b024805a5c2d6e4b9a4181
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-21 21:00:28 +0900

    solve abc119b

---
+++ abc/abc111_120/abc119/abc119b.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    real res = 0.0, rate = 380000.0;
+    foreach (_; 0 .. N) {
+        real x;
+        string u;
+        readf("%f %s\n", x, u);
+
+        if (u == "JPY") res += x;
+        else res += x * rate;
+    }
+
+    writefln("%.10f", res);
+}
