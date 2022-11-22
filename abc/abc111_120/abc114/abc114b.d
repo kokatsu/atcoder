ab775ecb6a1e9023782be5417a1635dd0e3041c4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-22 22:38:51 +0900

    solve abc114b

---
+++ abc/abc111_120/abc114/abc114b.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    auto len = S.length;
+
+    int res = int.max, num = 753;
+    foreach (i; 2 .. len) {
+        int X = S[i-2..i+1].to!int;
+        res = min(res, abs(X-num));
+    }
+
+    res.writeln;
+}
