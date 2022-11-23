1c494a2cc5b6a32b566cb89e77a787733de69074
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-23 22:56:26 +0900

    solve abc117c

---
+++ abc/abc111_120/abc117/abc117c.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto X = readln.chomp.split.to!(int[]);
+
+    X.sort;
+
+    auto diff = new int[](M-1);
+    foreach (i; 0 .. M-1) {
+        diff[i] = X[i+1] - X[i];
+    }
+
+    diff.sort;
+
+    int res;
+    if (M > N) res = diff[0..M-N].sum;
+
+    res.writeln;
+}
