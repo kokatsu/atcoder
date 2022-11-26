0163c49c9fb6a9126737f82cf052fb55d50a8d20
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-26 21:11:19 +0900

    solve abc279c

---
+++ abc/abc271_280/abc279/abc279c.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int H, W;
+    readf("%d %d\n", H, W);
+
+    auto S = new string[](H), T = new string[](H);
+    foreach (i; 0 .. H) readf("%s\n", S[i]);
+    foreach (i; 0 .. H) readf("%s\n", T[i]);
+
+    auto U = new dchar[][](W, H), V = new dchar[][](W, H);
+    foreach (i; 0 .. H) {
+        foreach (j; 0 .. W) {
+            U[j][i] = S[i][j];
+            V[j][i] = T[i][j];
+        }
+    }
+
+    U.sort, V.sort;
+
+    writeln(U == V ? "Yes" : "No");
+}
