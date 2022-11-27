c1fa6e4638400948b1f87ec73ca9ba5cf3e611f4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-27 21:55:00 +0900

    solve abc109b

---
+++ abc/abc101_110/abc109/abc109b.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    bool[string] used;
+    string pre;
+    bool isOK = true;
+    foreach (i; 0 .. N) {
+        string W;
+        readf("%s\n", W);
+
+        if (i > 0) isOK &= (pre.back == W.front);
+        if (W in used) isOK = false;
+
+        used[W] = true;
+        pre = W;
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
