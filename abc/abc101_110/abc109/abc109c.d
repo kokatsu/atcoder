154033adb4a79470fc471f4296ee54134c3d3df0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-27 21:58:30 +0900

    solve abc109c

---
+++ abc/abc101_110/abc109/abc109c.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N, X;
+    readf("%d %d\n", N, X);
+
+    auto x = readln.chomp.split.to!(int[]);
+    x ~= X;
+
+    x.sort;
+
+    auto diff = new int[](N);
+    foreach (i; 0 .. N) {
+        diff[i] = x[i+1] - x[i];
+    }
+
+    int res = diff.fold!((a, b) => gcd(a, b));
+    res.writeln;
+}
