ecc88fa74e6e847d2f5c805ac2623e27686fd149
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-11-28 19:55:11 +0900

    solve abc106b

---
+++ abc/abc101_110/abc106/abc106b.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int res;
+    foreach (i; iota(1, N+1, 2)) {
+        int cnt, d = 1;
+        while (d * d <= i) {
+            if (i % d == 0) {
+                ++cnt;
+                if (d * d != i) ++cnt;
+            }
+
+            ++d;
+        }
+
+        if (cnt == 8) ++res;
+    }
+
+    res.writeln;
+}
