dea8faab8f5f2c51b6c218d106e347a050565ff2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-01 22:32:29 +0900

    solve abc100c

---
+++ abc/abc091_100/abc100/abc100c.d
@@ -0,0 +1,14 @@
+import std;
+import core.bitop;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto a = readln.chomp.split.to!(int[]);
+
+    int res;
+    foreach (x; a) res += x.bsf;
+
+    res.writeln;
+}
