15a409e4232355d29ee47827a14343795b1feea1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-02 21:02:17 +0900

    solve abc101c

---
+++ abc/abc101_110/abc101/abc101c.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int M = A.countUntil(1).to!int;
+
+    int L = (M + K - 2) / (K - 1);
+    M = max(M, (K-1)*L);
+    int R = (N - M + K - 3) / (K - 1);
+
+    int res = L + R;
+    res.writeln;
+}
