28f8eb6b062eaef3cb10637de0298ee764f67bae
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-07 23:07:07 +0900

    solve abc113b

---
+++ abc/abc111_120/abc113/abc113b.d
@@ -0,0 +1,24 @@
+import std;
+
+enum long M = 10 ^^ 3;
+
+void main() {
+    long N, T, A;
+    readf("%d\n%d %d\n", N, T, A);
+
+    auto H = readln.chomp.split.to!(long[]);
+
+    T *= M, A *= M;
+
+    long diff = long.max;
+    ulong res;
+    foreach (i, h; H) {
+        long d = abs(A-(T-h*6));
+        if (d < diff) {
+            diff = d;
+            res = i + 1;
+        }
+    }
+
+    res.writeln;
+}
