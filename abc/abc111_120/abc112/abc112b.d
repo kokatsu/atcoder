eaadfcbbb26134dde4cbfd62a44275e7384485ee
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-07 23:14:22 +0900

    solve abc112b

---
+++ abc/abc111_120/abc112/abc112b.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N, T;
+    readf("%d %d\n", N, T);
+
+    int cost = int.max;
+    foreach (_; 0 .. N) {
+        int c, t;
+        readf("%d %d\n", c, t);
+
+        if (t <= T) cost = min(cost, c);
+    }
+
+    string res = (cost == int.max ? "TLE" : cost.to!string);
+    res.writeln;
+}
