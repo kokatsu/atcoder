f3ec0e43d3abf44cb805512c900f795b7ecbb076
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-07 23:10:03 +0900

    solve abc112a

---
+++ abc/abc111_120/abc112/abc112a.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    string res = "Hello World";
+    if (N == 2) {
+        int A, B;
+        readf("%d\n%d\n", A, B);
+
+        res = to!string(A+B);
+    }
+
+    res.writeln;
+}
