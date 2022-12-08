e20e4c1385af6369e1ea967a17accfb6918b6a97
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-08 22:36:00 +0900

    solve abc094b

---
+++ abc/abc091_100/abc094/abc094b.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N, M, X;
+    readf("%d %d %d\n", N, M, X);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    A ~= X;
+    A.sort;
+
+    int P = A.countUntil(X).to!int;
+
+    int res = min(P, M-P);
+    res.writeln;
+}
