49e0c06a57847e03bcb307b3e058c8ac6eb40083
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-09 23:09:36 +0900

    solve abc092b

---
+++ abc/abc091_100/abc092/abc092b.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N, D, X;
+    readf("%d\n%d %d\n", N, D, X);
+
+    int res = X;
+    foreach (_; 0 .. N) {
+        int A;
+        readf("%d\n", A);
+
+        res += iota(1, D+1, A).length;
+    }
+
+    res.writeln;
+}
