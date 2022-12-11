91bdf9245be887cf41e21541485ce8e35896ca25
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-11 22:18:22 +0900

    solve abc090b

---
+++ abc/abc081_090/abc090/abc090b.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int A, B;
+    readf("%d %d\n", A, B);
+
+    int res;
+    foreach (i; A .. B+1) {
+        auto S = i.to!string.to!(dchar[]);
+        auto T = S.dup;
+
+        T.reverse;
+
+        if (S == T) ++res;
+    }
+
+    res.writeln;
+}
