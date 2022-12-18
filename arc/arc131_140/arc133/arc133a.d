838d3f83eff9a151816b0c9b24daf271fbe96fb6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-18 23:16:58 +0900

    solve arc133a

---
+++ arc/arc131_140/arc133/arc133a.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int x = A.back;
+    foreach (i; 0 .. N-1) {
+        if (A[i] > A[i+1]) {
+            x = A[i];
+            break;
+        }
+    }
+
+    auto res = A.filter!(a => a != x).array;
+    writefln("%(%s %)", res);
+}
