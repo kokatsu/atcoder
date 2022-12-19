4260c0176a6da3f5020de3e67057d666c17be525
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-19 22:25:03 +0900

    solve abc111b

---
+++ abc/abc111_120/abc111/abc111b.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    string N;
+    readf("%s\n", N);
+
+    bool f(string s) {
+        return s.all!(x => x == s.front);
+    }
+
+    while (!f(N)) N = N.succ;
+
+    N.writeln;
+}
