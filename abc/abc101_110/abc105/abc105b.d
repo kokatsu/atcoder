7e9afdb6c55559de07aba45da6ce952dec434dda
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-21 23:07:56 +0900

    solve abc105b

---
+++ abc/abc101_110/abc105/abc105b.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    bool isOK;
+    foreach (i; 0 .. N/4+1) {
+        int rem = N - i * 4;
+        isOK |= (rem % 7 == 0);
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
