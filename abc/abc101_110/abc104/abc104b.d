d03b141645ae445f60222561ae0401c579dcbade
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-22 21:00:06 +0900

    solve abc104b

---
+++ abc/abc101_110/abc104/abc104b.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    bool isOK = (S.front == 'A');
+    isOK &= (S[2..$-1].canFind('C'));
+
+    int cnt;
+    foreach (s; S) {
+        if (std.uni.isLower(s)) ++cnt;
+    }
+
+    isOK &= (S.length == cnt + 2);
+
+    writeln(isOK ? "AC" : "WA");
+}
