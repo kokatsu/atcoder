5d7c26535d239fba98e4aea4aceba83569356d98
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-22 20:53:59 +0900

    solve abc104a

---
+++ abc/abc101_110/abc104/abc104a.d
@@ -0,0 +1,15 @@
+import std;
+
+enum int ABC = 1200;
+enum int ARC = 2800;
+
+void main() {
+    int R;
+    readf("%d\n", R);
+
+    auto res = to!(dchar[])("AGC");
+    if (R < ABC) res[1] = 'B';
+    else if (R < ARC) res[1] = 'R';
+
+    res.writeln;
+}
