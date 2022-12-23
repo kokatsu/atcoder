47cb8752a9f66821ae9b21ed5b0cd5647da675ae
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-23 19:19:59 +0900

    solve abc103b

---
+++ abc/abc101_110/abc103/abc103b.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    auto S = readln.chomp.to!(dchar[]);
+    auto T = readln.chomp.to!(dchar[]);
+
+    auto len = S.length;
+    bool isOK;
+    foreach (_; 0 .. len) {
+        isOK |= (S == T);
+
+        auto b = S.back;
+        S = b ~ S[0..$-1];
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
