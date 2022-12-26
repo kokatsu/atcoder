09d59bb066c605754d7d3a4373a38c4e5c53be20
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-26 20:02:04 +0900

    solve abc098b

---
+++ abc/abc091_100/abc098/abc098b.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    int N;
+    string S;
+    readf("%d\n%s\n", N, S);
+
+    auto X = new int[](26), Y = new int[](26);
+    foreach (s; S) ++Y[s-'a'];
+
+    int res;
+    foreach (s; S) {
+        ++X[s-'a'], --Y[s-'a'];
+
+        int num;
+        foreach (x, y; zip(X, Y)) {
+            if (x > 0 && y > 0) ++num;
+        }
+
+        res = max(res, num);
+    }
+
+    res.writeln;
+}
