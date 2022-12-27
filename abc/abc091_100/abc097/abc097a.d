b98e88a3c250875d29156b768d5e2e098f7878ae
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-27 22:23:53 +0900

    solve abc097a

---
+++ abc/abc091_100/abc097/abc097a.d
@@ -0,0 +1,9 @@
+import std;
+
+void main() {
+    int a, b, c, d;
+    readf("%d %d %d %d\n", a, b, c, d);
+
+    int x = min(abs(a-c), max(abs(a-b), abs(b-c)));
+    writeln(x <= d ? "Yes" : "No");
+}
