033b311303e5c890bfcb8b0110e7e68cd3e3c7f7
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-29 23:49:34 +0900

    solve abc084b

---
+++ abc/abc081_090/abc084/abc084b.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    int A, B;
+    string S;
+    readf("%s %d\n%s\n", A, B, S);
+
+    bool isOK = true;
+    foreach (i, s; S) {
+        if (i == A) isOK &= (s == '-');
+        else isOK &= std.uni.isNumber(s);
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
