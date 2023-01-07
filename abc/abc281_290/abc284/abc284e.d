9783b596eb36557747e1bc546a6d099f1f14c651
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-01-07 22:03:26 +0900

    solve abc284e

---
+++ abc/abc281_290/abc284/abc284e.d
@@ -0,0 +1,34 @@
+import std;
+
+enum long L = 10 ^^ 6;
+
+void main() {
+    long N, M;
+    readf("%d %d\n", N, M);
+
+    auto edges = new long[][](N);
+    foreach (_; 0 .. M) {
+        long u, v;
+        readf("%d %d\n", u, v);
+
+        --u, --v;
+        edges[u] ~= v, edges[v] ~= u;
+    }
+
+    auto seen = new bool[](N);
+
+    long f(long pos) {
+        long ret = 1;
+        seen[pos] = true;
+        foreach (edge; edges[pos]) {
+            if (seen[edge]) continue;
+            ret += f(edge);
+            if (ret >= L) break;
+        }
+        seen[pos] = false;
+        return ret;
+    }
+
+    long res = min(f(0), L);
+    res.writeln;
+}
