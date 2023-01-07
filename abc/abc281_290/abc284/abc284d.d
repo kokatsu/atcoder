f4a297f12a988de326c31050348a0daa9873fc20
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-01-07 21:49:38 +0900

    solve abc284d

---
+++ abc/abc281_290/abc284/abc284d.d
@@ -0,0 +1,44 @@
+import std;
+
+void main() {
+    long T;
+    readf("%d\n", T);
+
+    long L = 3 * 10 ^^ 6;
+    auto sieve = new bool[](L+1);
+    sieve[2..L+1] = true;
+    long d = 2;
+    while (d * d <= L) {
+        if (sieve[d]) {
+            foreach (i; iota(d*d, L+1, d)) {
+                sieve[i] = false;
+            }
+        }
+        ++d;
+    }
+
+    auto primes = iota(L+1).filter!(i => sieve[i]).array;
+
+    foreach (_; 0 .. T) {
+        long N;
+        readf("%d\n", N);
+
+        foreach (q; primes) {
+            if (N % q == 0) {
+                long M = N / q;
+                long S = M.to!real.sqrt.floor.to!long;
+                if (S * S == M) {
+                    writeln(S, " ", q);
+                    break;
+                }
+            }
+
+            long q2 = q * q;
+            if (N % q2 == 0) {
+                long M = N / q2;
+                writeln(q, " ", M);
+                break;
+            }
+        }
+    }
+}
