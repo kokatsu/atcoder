48dbdc28b0c69402849b6670bdb3e201e840c6b3
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-01-07 21:02:02 +0900

    solve abc284a

---
+++ abc/abc281_290/abc284/abc284a.d
@@ -0,0 +1,11 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto S = new string[](N);
+    foreach (i; 0 .. N) readf("%s\n", S[i]);
+
+    foreach_reverse (s; S) s.writeln;
+}
