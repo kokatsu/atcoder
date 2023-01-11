c159094b636394ca94647ea6d6de05dfde69ed55
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-01-11 23:30:53 +0900

    solve abc059b

---
+++ abc/abc051_060/abc059b.d
@@ -0,0 +1,28 @@
+import std;
+
+void main() {
+    string A, B;
+    readf("%s\n%s\n", A, B);
+
+    int c = compareArray(A, B);
+
+    string res = "EQUAL";
+    if (c > 0) res = "GREATER";
+    if (c < 0) res = "LESS";
+
+    res.writeln;
+}
+
+int compareArray(T)(T x, T y)
+if (isArray!T) {
+    if (x.length != y.length) {
+        return x.length > y.length ? 1 : -1;
+    }
+    else if (x != y) {
+        foreach (u, v; zip(x, y)) {
+            if (u == v) continue;
+            return u > v ? 1 : -1;
+        }
+    }
+    return 0;
+}
