90060500b5d4f54b549f83c5aab32c5cc545db54
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-01-15 21:08:18 +0900

    solve abc285b

---
+++ abc/abc281_290/abc285/abc285b.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N;
+    string S;
+    readf("%d\n%s\n", N, S);
+
+    foreach (i; 1 .. N) {
+        int res;
+        foreach (j; 0 .. N-i) {
+            if (S[j] == S[i+j]) break;
+            ++res;
+        }
+
+        res.writeln;
+    }
+}
