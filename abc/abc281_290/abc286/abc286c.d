3660abeba97b45d8911e4811d8f17463334603d6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-01-21 21:12:39 +0900

    solve abc286c

---
+++ abc/abc281_290/abc286/abc286c.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    long N, A, B;
+    readf("%d %d %d\n", N, A, B);
+
+    auto S = readln.chomp.to!(dchar[]);
+
+    long res = long.max, h = N / 2;
+    foreach (i; 0 .. N) {
+        long num = i * A;
+        foreach (j; 0 .. h) {
+            if (S[j] != S[N-j-1]) num += B;
+        }
+
+        res = min(res, num);
+
+        auto f = S.front;
+        S.popFront;
+        S ~= f;
+    }
+
+    res.writeln;
+}
