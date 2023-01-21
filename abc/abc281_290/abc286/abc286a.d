09a76143c66ff936c34e3b6f03ded4f40b5c27b4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-01-21 21:03:50 +0900

    solve abc286a

---
+++ abc/abc281_290/abc286/abc286a.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int N, P, Q, R, S;
+    readf("%d %d %d %d %d\n", N, P, Q, R, S);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    foreach (i; 0 .. Q-P+1) {
+        A.swapAt(P+i-1, R+i-1);
+    }
+
+    writefln("%(%s %)", A);
+}
