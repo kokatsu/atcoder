4a4eff6f04c5ea9d87a4f27ec1c9aa13e8b90850
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-01-28 12:48:27 +0900

    solve abc037b

---
+++ abc/abc031_040/abc037/abc037b.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N, Q;
+    readf("%d %d\n", N, Q);
+
+    auto a = new int[](N);
+    foreach (_; 0 .. Q) {
+        int L, R, T;
+        readf("%d %d %d\n", L, R, T);
+
+        foreach (i; L-1 .. R) a[i] = T;
+    }
+
+    writefln("%(%s\n%)", a);
+}
