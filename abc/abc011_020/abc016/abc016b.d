9bcd019543131b0275aa28c5b9e8dd065f2f5bed
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-01-31 00:43:55 +0900

    solve abc016b

---
+++ abc/abc011_020/abc016/abc016b.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    int A, B, C;
+    readf("%d %d %d\n", A, B, C);
+
+    int X = A + B, Y = A - B;
+
+    if (X == C && Y == C) writeln("?");
+    else if (X == C) writeln("+");
+    else if (Y == C) writeln("-");
+    else writeln("!");
+}
