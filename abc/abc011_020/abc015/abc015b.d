1aff0254eb36ed9d99bca3347c877ab046bdc457
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-01-30 19:06:13 +0900

    solve abc015b

---
+++ abc/abc011_020/abc015/abc015b.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int M = N - A.count!"a == 0".to!int;
+
+    int res = (A.sum + M - 1) / M;
+    res.writeln;
+}
