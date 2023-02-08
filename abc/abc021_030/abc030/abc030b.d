438c0c6341e5059d9b1d64c1c5d69150dd93f46a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-02-08 22:57:42 +0900

    solve abc030b

---
+++ abc/abc021_030/abc030/abc030b.d
@@ -0,0 +1,12 @@
+import std;
+
+void main() {
+    int n, m;
+    readf("%d %d\n", n, m);
+
+    real x = ((n % 12) * 60.0 + m) * 0.5, y = m * 6.0;
+    real d = abs(x-y);
+
+    real res = min(d, 360.0-d);
+    writefln("%.10f", res);
+}
