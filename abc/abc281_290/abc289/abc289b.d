43566d9038a9fb4065d7c72065c5642c6fd48f02
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-02-11 21:18:47 +0900

    solve abc289b

---
+++ abc/abc281_290/abc289/abc289b.d
@@ -0,0 +1,29 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto a = readln.chomp.split.to!(int[]);
+
+    auto used = new bool[](N+1);
+    foreach (x; a) used[x] = true;
+
+    int i, pos = 1;
+    int[] res;
+    while (i < N) {
+        int next = pos;
+        while (next <= N && used[next]) {
+            ++next;
+        }
+
+        auto arr = iota(pos, next+1).array;
+        arr.reverse;
+
+        res ~= arr;
+        i = res.length.to!int;
+        pos = next + 1;
+    }
+
+    writefln("%(%s %)", res);
+}
