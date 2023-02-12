c81e92b7882464e788fe68e1ab8bfd975d113f44
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-02-12 22:24:06 +0900

    solve abc031b

---
+++ abc/abc031_040/abc031/abc031b.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int L, H;
+    readf("%d %d\n", L, H);
+
+    int N;
+    readf("%d\n", N);
+
+    foreach (_; 0 .. N) {
+        int A;
+        readf("%d\n", A);
+
+        int res;
+        if (A < L) res = L - A;
+        else if (A > H) res = -1;
+
+        res.writeln;
+    }
+}
