e5ffb2b06fb49d0750f9739916e91518b03edd13
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-02-13 22:12:19 +0900

    solve abc032b

---
+++ abc/abc031_040/abc032/abc032b.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    string s;
+    int k;
+    readf("%s\n%d\n", s, k);
+
+    auto l = s.length.to!int;
+
+    int[string] password;
+    foreach (i; 0 .. l-k+1) ++password[s[i..i+k]];
+
+    auto res = password.length;
+    res.writeln;
+}
