378103349a097f90777bda4066ca9a1f6b57dbfa
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-02-16 23:04:18 +0900

    solve abc021b

---
+++ abc/abc021_030/abc021/abc021b.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N, a, b;
+    readf("%d\n%d %d\n", N, a, b);
+
+    int K;
+    readf("%d\n", K);
+
+    auto P = readln.chomp.split.to!(int[]);
+
+    bool[int] seen;
+    seen[a] = true, seen[b] = true;
+
+    bool isOK = true;
+    foreach (p; P) {
+        if (p in seen) isOK = false;
+        seen[p] = true;
+    }
+
+    writeln(isOK ? "YES" : "NO");
+}
