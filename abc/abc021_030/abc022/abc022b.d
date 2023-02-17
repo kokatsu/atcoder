cfee2841a18fef4c56a204228a2f3497b63236a0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-02-17 21:32:52 +0900

    solve abc022b

---
+++ abc/abc021_030/abc022/abc022b.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int res;
+    bool[int] exists;
+    foreach (_; 0 .. N) {
+        int A;
+        readf("%d\n", A);
+
+        if (A in exists) ++res;
+
+        exists[A] = true;
+    }
+
+    res.writeln;
+}
