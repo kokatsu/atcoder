6f48a62f2850fab110217a344058222363451063
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-02-17 21:30:08 +0900

    solve abc022a

---
+++ abc/abc021_030/abc022/abc022a.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int N, S, T;
+    readf("%d %d %d\n",N, S, T);
+
+    int res, W;
+    foreach (i; 0 .. N) {
+        int A;
+        readf("%d\n", A);
+
+        W += A;
+
+        if (S <= W && W <= T) ++res;
+    }
+
+    res.writeln;
+}
