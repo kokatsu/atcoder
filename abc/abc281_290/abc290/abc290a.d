68921552e4c04de7ac3e9a158ff67d1d437fc8db
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-02-19 21:02:07 +0900

    solve abc290a

---
+++ abc/abc281_290/abc290/abc290a.d
@@ -0,0 +1,12 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = readln.chomp.split.to!(int[]);
+    auto B = readln.chomp.split.to!(int[]);
+
+    auto res = B.map!(b => A[b-1]).array.sum;
+    res.writeln;
+}
