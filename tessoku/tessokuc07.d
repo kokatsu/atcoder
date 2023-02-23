142b9eabd36205f821382fc92d3f216a3b78154c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-02-23 23:03:16 +0900

    solve tessokuc07

---
+++ tessoku/tessokuc07.d
@@ -0,0 +1,25 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto C = readln.chomp.split.to!(long[]);
+
+    C.sort;
+
+    auto S = C.cumulativeFold!"a + b".array.assumeSorted;
+
+    long Q;
+    readf("%d\n", Q);
+
+    foreach (_; 0 .. Q) {
+        long X;
+        readf("%d\n", X);
+
+        auto lb = S.lowerBound(X+1);
+
+        auto res = lb.length;
+        res.writeln;
+    }
+}
