f2f78c7d7b6b59bb524e40c3451f586e85a01caf
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-02-26 21:10:35 +0900

    solve abc291c

---
+++ abc/abc291_300/abc291/abc291c.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    long N;
+    string S;
+    readf("%d\n%s\n", N, S);
+
+    auto L = S.length + 1;
+
+    long pos;
+    bool[long] seen;
+    seen[pos] = true;
+    bool isOK;
+    foreach (s; S) {
+        if (s == 'R') pos += L;
+        if (s == 'L') pos -= L;
+        if (s == 'U') ++pos;
+        if (s == 'D') --pos;
+
+        if (pos in seen) isOK = true;
+
+        seen[pos] = true;
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
