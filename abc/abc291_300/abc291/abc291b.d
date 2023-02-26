7a9ec429ec37b0dc89b2b8421eb5fbbb8bfc29e8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-02-26 21:05:05 +0900

    solve abc291b

---
+++ abc/abc291_300/abc291/abc291b.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto X = readln.chomp.split.to!(int[]);
+
+    X.sort;
+
+    real res = 0.0;
+    foreach (i; N .. N*4) {
+        res += X[i].to!real;
+    }
+
+    res /= N * 3.0;
+
+    writefln("%.10f", res);
+}
