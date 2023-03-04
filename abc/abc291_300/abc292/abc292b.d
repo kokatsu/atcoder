cf9eb3f09569c369338748c1a8de52ed44f9928c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-03-04 21:20:04 +0900

    solve abc292b

---
+++ abc/abc291_300/abc292/abc292b.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N, Q;
+    readf("%d %d\n", N, Q);
+
+    auto cnts = new int[](N+1);
+    foreach (_; 0 .. Q) {
+        int i, x;
+        readf("%d %d\n", i, x);
+
+        if (i == 1) ++cnts[x];
+        else if (i == 2) cnts[x] = 2;
+        else writeln(cnts[x] >= 2 ? "Yes" : "No");
+    }
+}
--- abc/abc291_300/abc292a.d
+++
@@ -1,9 +0,0 @@
-import std;
-
-void main() {
-    string S;
-    readf("%s\n", S);
-
-    auto res = std.uni.toUpper(S);
-    res.writeln;
-}
