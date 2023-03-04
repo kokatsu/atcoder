220aded714ea2bedc454f5bee77ca3edc8c572de
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-03-04 21:42:40 +0900

    solve abc292c

---
+++ abc/abc291_300/abc292/abc292c.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto cnts = new long[](N);
+    foreach (i; 1 .. N) {
+        foreach (j; iota(i, N, i)) {
+            ++cnts[j];
+        }
+    }
+
+    long res;
+    foreach (i; 1 .. N) {
+        res += cnts[i] * cnts[N-i];
+    }
+
+    res.writeln;
+}
