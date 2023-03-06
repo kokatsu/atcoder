c045ae5a4e3e14fe8347f7a9d9b1eeeed17354aa
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-03-06 21:15:59 +0900

    solve tessokub03

---
+++ tessoku/tessokub03.d
@@ -0,0 +1,25 @@
+import std;
+
+enum int L = 1000;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    auto dp = new bool[][](4, L+1);
+    dp[0][0] = true;
+    foreach (a; A) {
+        foreach_reverse (i; 0 .. 3) {
+            foreach (j; 0 .. L) {
+                if (j + a > L) break;
+                if (!dp[i][j]) continue;
+
+                dp[i+1][j+a] = true;
+            }
+        }
+    }
+
+    writeln(dp[3][L] ? "Yes" : "No");
+}
