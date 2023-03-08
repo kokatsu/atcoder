5d2f66510f3db7619bfe38e211673b9c70eb08da
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-03-09 00:19:11 +0900

    solve tessokua05

---
+++ tessoku/tessokua05.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    int res;
+    foreach (i; 1 .. N+1) {
+        int rem = K - i;
+        if (rem < 2) break;
+        if (rem > N * 2) continue;
+
+        int t = clamp(rem-1, 1, N);
+        res += (t == N ? N * 2 - rem + 1 : t);
+    }
+
+    res.writeln;
+}
