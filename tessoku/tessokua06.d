f554ba5e86ea70990ccbf5d0242420b91faa83ae
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-03-10 18:06:52 +0900

    solve tessokua06

---
+++ tessoku/tessokua06.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    long N, Q;
+    readf("%d %d\n", N, Q);
+
+    auto A = new long[](N+1);
+    A[1..$] = readln.chomp.split.to!(long[]);
+
+    auto C = A.cumulativeFold!"a + b".array;
+    foreach (_; 0 .. Q) {
+        long L, R;
+        readf("%d %d\n", L, R);
+
+        long res = C[R] - C[L-1];
+        res.writeln;
+    }
+}
