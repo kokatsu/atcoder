3f4375c823d344af19df0aa5561eea8ff5f014a0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-03-11 21:07:59 +0900

    solve abc293b

---
+++ abc/abc291_300/abc293/abc293b.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    auto call = new bool[](N+1);
+    call[0] = true;
+    foreach (i, a; A) {
+        if (call[i+1]) continue;
+
+        call[a] = true;
+    }
+
+    auto res = iota(1, N+1).filter!(a => !call[a]).array;
+    res.length.writeln;
+    writefln("%(%s %)", res);
+}
