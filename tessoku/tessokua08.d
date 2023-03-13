a2c9441067bf513575402175492642d5a29ed85c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-03-13 21:24:58 +0900

    solve tessokua08

---
+++ tessoku/tessokua08.d
@@ -0,0 +1,31 @@
+import std;
+
+void main() {
+    int H, W;
+    readf("%d %d\n", H, W);
+
+    auto X = new int[][](H+1, W+1);
+    foreach (i; 1 .. H+1) {
+        X[i][1..$] = readln.chomp.split.to!(int[]);
+        X[i][] += X[i-1][];
+    }
+
+    foreach (i; 1 .. H+1) {
+        foreach (j; 0 .. W) {
+            X[i][j+1] += X[i][j];
+        }
+    }
+
+    int Q;
+    readf("%d\n", Q);
+
+    foreach (_; 0 .. Q) {
+        int A, B, C, D;
+        readf("%d %d %d %d\n", A, B, C, D);
+
+        --A, --B;
+
+        int res = X[C][D] - X[A][D] - X[C][B] + X[A][B];
+        res.writeln;
+    }
+}
