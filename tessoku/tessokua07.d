fc58d05990d92520570f3b6296db7c8998540e4b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-03-14 22:44:39 +0900

    solve tessokua07

---
+++ tessoku/tessokua07.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int D, N;
+    readf("%d\n%d\n", D, N);
+
+    auto dp = new int[](D+1);
+    foreach (_; 0 .. N) {
+        int L, R;
+        readf("%d %d\n", L, R);
+
+        ++dp[L-1], --dp[R];
+    }
+
+    foreach (i; 0 .. D) {
+        dp[i+1] += dp[i];
+    }
+
+    writefln("%(%s\n%)", dp[0..D]);
+}
