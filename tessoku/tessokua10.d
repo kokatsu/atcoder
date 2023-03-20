86b3a68562297f9bc80753222eb0e4ec8bca8bb3
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-03-20 22:07:50 +0900

    solve tessokua10

---
+++ tessoku/tessokua10.d
@@ -0,0 +1,25 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    auto X = A.cumulativeFold!max.array;
+
+    A.reverse;
+    auto Y = A.cumulativeFold!max.array;
+    Y.reverse;
+
+    int D;
+    readf("%d\n", D);
+
+    foreach (_; 0 .. D) {
+        int L, R;
+        readf("%d %d\n", L, R);
+
+        int res = max(X[L-2], Y[R]);
+        res.writeln;
+    }
+}
