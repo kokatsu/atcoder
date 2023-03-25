52e2a9e57c6b038acb4addd652becf5198466e64
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-03-25 20:47:04 +0900

    solve tessokua13

---
+++ tessoku/tessokua13.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    long N, K;
+    readf("%d %d\n", N, K);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    auto S = A.assumeSorted;
+
+    long res;
+    foreach (i, a; A) {
+        res += S.lowerBound(a+K+1).length.to!long - i - 1;
+    }
+
+    res.writeln;
+}
