110141379d2eaf808fd00589168a4149670fa222
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-03-28 13:23:23 +0900

    solve tessokub14

---
+++ tessoku/tessokub14.d
@@ -0,0 +1,39 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    bool[int] L, R;
+    L[0] = R[0] = true;
+    foreach (i; 0 .. N/2) {
+        bool[int] T = L.dup;
+        foreach (l; L.byKey) {
+            if (l + A[i] <= K) {
+                T[l+A[i]] = true;
+            }
+        }
+        L = T;
+    }
+
+    foreach (i; N/2 .. N) {
+        bool[int] T = R.dup;
+        foreach (r; R.byKey) {
+            if (r + A[i] <= K) {
+                T[r+A[i]] = true;
+            }
+        }
+        R = T;
+    }
+
+    bool isOK;
+    foreach (r; R.byKey) {
+        if (K - r in L) {
+            isOK = true;
+        }
+    }
+
+    writeln(isOK ? "Yes" : "No");
+}
