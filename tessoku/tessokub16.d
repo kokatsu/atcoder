97b0aa079b5f01ff58a4d374034f5f0506ceee87
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-03-31 19:33:17 +0900

    solve tessokub16

---
+++ tessoku/tessokub16.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto h = readln.chomp.split.to!(int[]);
+
+    auto dp = new int[](N);
+    dp[1..$] = int.max;
+    foreach (i; 1 .. N) {
+        dp[i] = min(dp[i], dp[i-1]+abs(h[i]-h[i-1]));
+        if (i > 1) dp[i] = min(dp[i], dp[i-2]+abs(h[i]-h[i-2]));
+    }
+
+    int res = dp[N-1];
+    res.writeln;
+}
