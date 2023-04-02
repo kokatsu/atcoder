2e87d385c2a8f14abd1dd01090ffb1073f180f9f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-04-02 21:11:23 +0900

    solve abc296e with commentary

---
+++ abc/abc291_300/abc296/abc296e.d
@@ -0,0 +1,36 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    A[] -= 1;
+
+    auto cnts = new int[](N);
+    foreach (a; A) ++cnts[a];
+
+    auto list = new int[][](N);
+    int res = N;
+    foreach (i, c; cnts) {
+        if (c == 0) {
+            --res;
+            list[0] ~= i.to!int;
+        }
+    }
+
+    foreach (i; 0 .. N-1) {
+        if (list[i].empty) break;
+
+        foreach (l; list[i]) {
+            --cnts[A[l]];
+            if (cnts[A[l]] == 0) {
+                --res;
+                list[i+1] ~= A[l];
+            }
+        }
+    }
+
+    res.writeln;
+}
