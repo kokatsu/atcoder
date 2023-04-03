639445285ca798ce7fbc355404afb28467fb0c46
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-04-03 12:28:40 +0900

    solve tessokua17

---
+++ tessoku/tessokua17.d
@@ -0,0 +1,30 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = new int[][](2);
+    foreach (i; 0 .. 2) {
+        A[i] = readln.chomp.split.to!(int[]);
+    }
+
+    int[] dp = new int[](N);
+    dp[1..$] = int.max;
+    foreach (i; 1 .. N) {
+        dp[i] = min(dp[i], dp[i-1]+A[0][i-1]);
+        if (i > 1) dp[i] = min(dp[i], dp[i-2]+A[1][i-2]);
+    }
+
+    int pos = N - 1;
+    int[] res = [N];
+    while (pos > 0) {
+        pos -= (dp[pos] == dp[pos-1] + A[0][pos-1] ? 1 : 2);
+        res ~= pos + 1;
+    }
+
+    res.reverse;
+
+    res.length.writeln;
+    writefln("%(%s %)", res);
+}
