6b116422a316565bc0151510fe2eea51b49bc92d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-04-16 19:56:11 +0900

    solve tessokua24

---
+++ tessoku/tessokua24.d
@@ -0,0 +1,31 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int[] arr = [];
+    foreach (i, a; A) {
+        if (arr.empty || a > arr[$-1]) {
+            arr ~= a;
+        }
+        else {
+            int ok = arr.length.to!int, ng;
+            while (ng < ok) {
+                int mid = (ok + ng) / 2;
+                if (arr[mid] >= a) {
+                    ok = mid;
+                }
+                else {
+                    ng = mid + 1;
+                }
+            }
+            arr[ok] = a;
+        }
+    }
+
+    auto res = arr.length;
+    res.writeln;
+}
