8f806b35abfffde99d6e17dd83cba9ed0f7ad10b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-04-19 00:59:28 +0900

    solve tessokua26

---
+++ tessoku/tessokua26.d
@@ -0,0 +1,27 @@
+import std;
+
+void main() {
+    int Q;
+    readf("%d\n", Q);
+
+    long L = 3 * 10 ^^ 6;
+
+    auto sieve = new bool[](L+1);
+    sieve[2..L+1] = true;
+    long d = 2;
+    while (d * d <= L) {
+        if (sieve[d]) {
+            foreach (i; iota(d*d, L+1, d)) {
+                sieve[i] = false;
+            }
+        }
+        ++d;
+    }
+
+    foreach (_; 0 .. Q) {
+        int X;
+        readf("%d\n", X);
+
+        writeln(sieve[X] ? "Yes" : "No");
+    }
+}
