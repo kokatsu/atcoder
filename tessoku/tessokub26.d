10556496ed88f8f8d1a7f4ceb31797ca8b0126c9
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-04-20 18:45:05 +0900

    solve tessokub26

---
+++ tessoku/tessokub26.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto sieve = new bool[](N+1);
+    sieve[2..N+1] = true;
+    long d = 2;
+    while (d * d <= N) {
+        if (sieve[d]) {
+            foreach (i; iota(d*d, N+1, d)) {
+                sieve[i] = false;
+            }
+        }
+        ++d;
+    }
+
+    foreach (i, s; sieve) {
+        if (s) i.writeln;
+    }
+}
