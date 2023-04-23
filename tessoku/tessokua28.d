e8faa2faabf181230558f93d5c8c6b0458ae9129
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-04-23 18:29:18 +0900

    solve tessokua28

---
+++ tessoku/tessokua28.d
@@ -0,0 +1,23 @@
+import std;
+
+enum int MOD = 10 ^^ 4;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int res;
+    foreach (_; 0 .. N) {
+        dchar T;
+        int A;
+        readf("%c %d\n", T, A);
+
+        if (T == '+') res += A;
+        if (T == '-') res -= A;
+        if (T == '*') res *= A;
+
+        res = (res + MOD) % MOD;
+
+        res.writeln;
+    }
+}
