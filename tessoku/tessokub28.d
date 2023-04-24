1ead6015d0c60ce10d1ab8fc3d52b3dcb11cd71a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-04-24 12:43:22 +0900

    solve tessokub28

---
+++ tessoku/tessokub28.d
@@ -0,0 +1,17 @@
+import std;
+
+enum long MOD = 10 ^^ 9 + 7;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto dp = new long[](N);
+    dp[0..2] = 1;
+    foreach (i; 2 .. N) {
+        dp[i] = (dp[i-1] + dp[i-2]) % MOD;
+    }
+
+    long res = dp[N-1];
+    res.writeln;
+}
