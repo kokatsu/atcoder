ddc017a51f14b20e28de445333984c58d42ad817
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-13 22:28:23 +0900

    solve tessokua38

---
+++ tessoku/tessokua38.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int D, N;
+    readf("%d %d\n", D, N);
+
+    auto W = new int[](D);
+    W[] = 24;
+
+    foreach (_; 0 .. N) {
+        int L, R, H;
+        readf("%d %d %d\n", L, R, H);
+
+        --L, --R;
+        foreach (i; L .. R+1) {
+            W[i] = min(W[i], H);
+        }
+    }
+
+    int res = W.sum;
+    res.writeln;
+}
