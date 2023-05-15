890cd4ba925dce83cfe342758d3cc312981bb312
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-15 18:34:54 +0900

    solve tessokub37

---
+++ tessoku/tessokub37.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    long N = readln.chomp.to!long;
+
+    long res, P = 1;
+    while (P <= N) {
+        foreach (i; 0 .. 10) {
+            long d = N / (P * 10);
+            long r = N % (P * 10) + 1;
+
+            res += i * d * P;
+
+            if (r > i * P) {
+                res += i * min(P, r-i*P);
+            }
+        }
+
+        P *= 10;
+    }
+
+    res.writeln;
+}
