909d9430edb0a2162b5dc22e3d5aa9d15d378236
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-16 22:57:03 +0900

    solve tessokua39

---
+++ tessoku/tessokua39.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto L = new int[](N), R = new int[](N);
+    foreach (i; 0 .. N) readf("%d %d\n", L[i], R[i]);
+
+    zip(L, R).sort!"a[1] < b[1]";
+
+    int res, now;
+    foreach (l, r; zip(L, R)) {
+        if (now <= l) {
+            ++res, now = r;
+        }
+    }
+
+    res.writeln;
+}
