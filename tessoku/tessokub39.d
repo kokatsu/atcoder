7d60b0ae7c52ce25870e0eafddcebc19072d23b8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-17 22:25:55 +0900

    solve tessokub39

---
+++ tessoku/tessokub39.d
@@ -0,0 +1,27 @@
+import std;
+
+void main() {
+    int N, D;
+    readf("%d %d\n", N, D);
+
+    auto M = new int[][](D);
+    foreach (_; 0 .. N) {
+        int X, Y;
+        readf("%d %d\n", X, Y);
+
+        M[X-1] ~= Y;
+    }
+
+    auto heap = new BinaryHeap!(Array!int, "a < b")();
+    int res;
+    foreach (i; 0 .. D) {
+        foreach (m; M[i]) heap.insert(m);
+
+        if (!heap.empty) {
+            res += heap.front;
+            heap.popFront;
+        }
+    }
+
+    res.writeln;
+}
