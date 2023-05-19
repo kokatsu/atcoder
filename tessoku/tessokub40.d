47ae54fe1c8eebca2c325e5a799167442db6b637
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-19 21:59:15 +0900

    solve tessokub40

---
+++ tessoku/tessokub40.d
@@ -0,0 +1,19 @@
+import std;
+
+enum long M = 100;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    auto cnts = new long[](M);
+    long res;
+    foreach (a; A) {
+        res += cnts[(M-a%M)%M];
+        ++cnts[a%M];
+    }
+
+    res.writeln;
+}
