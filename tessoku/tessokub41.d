11e44a234453f70107f401e63cb937241f961a81
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-21 20:01:34 +0900

    solve tessokub41

---
+++ tessoku/tessokub41.d
@@ -0,0 +1,21 @@
+import std;
+
+void main() {
+    int X, Y;
+    readf("%d %d\n", X, Y);
+
+    int[] x, y;
+    while (X > 1 || Y > 1) {
+        x ~= X, y ~= Y;
+
+        if (X > Y) {
+            X -= Y;
+        }
+        else {
+            Y -= X;
+        }
+    }
+
+    x.length.writeln;
+    foreach_reverse (u, v; zip(x, y)) writeln(u, " ", v);
+}
