f90ee596a08aacde14f07089707e2cd644b65d81
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-25 20:59:08 +0900

    solve tessokub43

---
+++ tessoku/tessokub43.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    A[] -= 1;
+
+    auto res = new int[](N);
+    res[] = M;
+    foreach (a; A) --res[a];
+
+    foreach (r; res) r.writeln;
+}
