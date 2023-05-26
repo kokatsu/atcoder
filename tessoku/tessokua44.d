17261f9776609435336bad865bfa45469cf71ed4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-26 18:18:09 +0900

    solve tessokua44

---
+++ tessoku/tessokua44.d
@@ -0,0 +1,25 @@
+import std;
+
+void main() {
+    int N, Q;
+    readf("%d %d\n", N, Q);
+
+    auto A = iota(1, N+1).array;
+
+    bool isRev;
+    foreach (_; 0 .. Q) {
+        auto query = readln.chomp.split.to!(int[]);
+
+        if (query[0] == 1) {
+            int i = (isRev ? N-query[1] : query[1]-1);
+            A[i] = query[2];
+        }
+        else if (query[0] == 2) {
+            isRev = !isRev;
+        }
+        else {
+            int i = (isRev ? N-query[1] : query[1]-1);
+            A[i].writeln;
+        }
+    }
+}
