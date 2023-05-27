ffc8a2592bf4f91761316a11096743de5c5ad149
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-27 20:41:31 +0900

    solve tessokub44

---
+++ tessoku/tessokub44.d
@@ -0,0 +1,28 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = new int[][](N);
+    foreach (i; 0 .. N) A[i] = readln.chomp.split.to!(int[]);
+
+    auto P = iota(N).array;
+
+    int Q;
+    readf("%d\n", Q);
+
+    foreach (_; 0 .. Q) {
+        int op, x, y;
+        readf("%d %d %d\n", op, x, y);
+
+        --x, --y;
+
+        if (op == 1) {
+            P.swapAt(x, y);
+        }
+        else {
+            A[P[x]][y].writeln;
+        }
+    }
+}
