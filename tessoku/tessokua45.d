90fe05218e3320366aac3fb0d00576ecdc819a37
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-28 20:50:50 +0900

    solve tessokua45

---
+++ tessoku/tessokua45.d
@@ -0,0 +1,19 @@
+import std;
+
+const string COLOR = "WBR";
+
+void main() {
+    int N;
+    dchar C;
+    string A;
+    readf("%d %c\n%s\n", N, C, A);
+
+    int num;
+    foreach (a; A) {
+        foreach (i, c; COLOR) {
+            if (a == c) num += i;
+        }
+    }
+
+    writeln(COLOR[num%3] == C ? "Yes" : "No");
+}
