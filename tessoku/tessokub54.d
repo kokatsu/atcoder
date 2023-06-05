dff2d68a4c4f555c39b7881c09774a3a30940080
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-06-05 18:34:28 +0900

    solve tessokub54

---
+++ tessoku/tessokub54.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    long[long] cnts;
+    long res;
+
+    long N;
+    readf("%d\n", N);
+
+    foreach (_; 0 .. N) {
+        long A;
+        readf("%d\n", A);
+
+        if (A in cnts) res += cnts[A];
+
+        ++cnts[A];
+    }
+
+    res.writeln;
+}
