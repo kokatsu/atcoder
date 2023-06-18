a14714358f1404b81799b9d011e0b30815fd606e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-06-18 22:43:15 +0900

    solve tessokub61

---
+++ tessoku/tessokub61.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto cnts = new int[](N+1);
+    foreach (_; 0 .. M) {
+        int A, B;
+        readf("%d %d\n", A, B);
+
+        ++cnts[A], ++cnts[B];
+    }
+
+    auto res = cnts.maxIndex;
+    res.writeln;
+}
