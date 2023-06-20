fe8e9ac4ac900c30366fc8d545e0bb941ab27a70
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-06-20 22:11:04 +0900

    solve tessokub62

---
+++ tessoku/tessokub62.d
@@ -0,0 +1,41 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto edge = new int[][](N+1);
+    foreach (_; 0 .. M) {
+        int A, B;
+        readf("%d %d\n", A, B);
+
+        edge[A] ~= B, edge[B] ~= A;
+    }
+
+    auto prev = new int[](N+1);
+
+    void f(int x, int y) {
+        prev[x] = y;
+        foreach (e; edge[x]) {
+            if (prev[e] > 0) continue;
+            f(e, x);
+        }
+    }
+
+    foreach (i; 1 .. N+1) {
+        if (prev[i] == 0) f(i, i);
+    }
+
+    int[] res;
+
+    void g(int x) {
+        res ~= x;
+        if (x != prev[x]) g(prev[x]);
+    }
+
+    g(N);
+
+    res.reverse;
+
+    writefln("%(%s %)", res);
+}
