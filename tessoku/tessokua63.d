fd575d10e3b9d4f486493ddd7bee9500708c730f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-06-21 21:30:42 +0900

    solve tessokua63

---
+++ tessoku/tessokua63.d
@@ -0,0 +1,33 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto edge = new int[][](N);
+    foreach (_; 0 .. M) {
+        int A, B;
+        readf("%d %d\n", A, B);
+
+        --A, --B;
+        edge[A] ~= B, edge[B] ~= A;
+    }
+
+    auto res = new int[](N);
+    res[1..N] = - 1;
+
+    int[] queue;
+    queue ~= 0;
+    while (!queue.empty) {
+        auto f = queue.front;
+        queue.popFront;
+        foreach (e; edge[f]) {
+            if (res[e] >= 0) continue;
+
+            res[e] = res[f] + 1;
+            queue ~= e;
+        }
+    }
+
+    writefln("%(%s\n%)", res);
+}
