70fc7fb4b98fc0bd61b5bd866e8a1c183d8717a7
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-07-16 21:40:21 +0900

    solve edpcc

---
+++ edpc/edpcc.d
@@ -0,0 +1,21 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto dp = new int[][](N+1, 3);
+    foreach (i; 0 .. N) {
+        auto a = readln.chomp.split.to!(int[]);
+        foreach (j; 0 .. 3) {
+            foreach (k, x; a) {
+                if (j == k) continue;
+
+                dp[i+1][j] = max(dp[i+1][j], dp[i][k]+x);
+            }
+        }
+    }
+
+    int res = dp[N].maxElement;
+    res.writeln;
+}
