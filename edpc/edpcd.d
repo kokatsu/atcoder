8bff1b3b5e61704f8839e1e4862a561e8936b0c0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-07-17 18:35:29 +0900

    solve edpcd

---
+++ edpc/edpcd.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    long N, W;
+    readf("%d %d\n", N, W);
+
+    auto dp = new long[](W+1);
+    foreach (_; 0 .. N) {
+        long w, v;
+        readf("%d %d\n", w, v);
+
+        foreach_reverse (i; w .. W+1) {
+            dp[i] = max(dp[i], dp[i-w]+v);
+        }
+    }
+
+    long res = dp[W];
+    res.writeln;
+}
