251ef193ad3475f318849f94ac27c25461ab98f9
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-07-19 19:57:03 +0900

    solve edpch

---
+++ edpc/edpch.d
@@ -0,0 +1,25 @@
+import std;
+
+enum long MOD = 10 ^^ 9 + 7;
+
+void main() {
+    long H, W;
+    readf("%d %d\n", H, W);
+
+    auto dp = new long[][](H, W);
+    dp[0][0] = 1;
+    foreach (i; 0 .. H) {
+        string a;
+        readf("%s\n", a);
+
+        foreach (j, x; a) {
+            if (x == '#') continue;
+
+            if (i > 0) dp[i][j] = (dp[i][j] + dp[i-1][j]) % MOD;
+            if (j > 0) dp[i][j] = (dp[i][j] + dp[i][j-1]) % MOD;
+        }
+    }
+
+    long res = dp[H-1][W-1];
+    res.writeln;
+}
