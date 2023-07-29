b3731c10e7c8843f68261a59b0f7853c74b3e579
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-07-29 21:56:24 +0900

    solve abc312c

---
+++ abc/abc311_320/abc312/abc312c.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    long N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = readln.chomp.split.to!(long[]);
+    auto B = readln.chomp.split.to!(long[]);
+
+    auto C = A.sort, D = B.sort;
+
+    long ok = B[M-1] + 1, ng;
+    while (ok - ng > 1) {
+        long mid = (ok + ng) / 2;
+
+        auto x = C.lowerBound(mid+1).length;
+        auto y = D.upperBound(mid-1).length;
+
+        (x >= y ? ok : ng) = mid;
+    }
+
+    ok.writeln;
+}
