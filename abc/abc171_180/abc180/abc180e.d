47366a10f77a67946a3407a9ee86a04e2cced2d4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-07-31 21:20:39 +0900

    solve abc180e

---
+++ abc/abc171_180/abc180/abc180e.d
@@ -0,0 +1,35 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto X = new long[](N), Y = new long[](N), Z = new long[](N);
+    foreach (i; 0 .. N) readf("%d %d %d\n", X[i], Y[i], Z[i]);
+
+    auto dists = new long[][](N, N);
+    foreach (i; 0 .. N) {
+        foreach (j; 0 .. N) {
+            dists[i][j] = abs(X[i]-X[j]) + abs(Y[i]-Y[j]) + max(0, Z[i]-Z[j]);
+        }
+    }
+
+    long l = 1 << N;
+    auto dp = new long[][](N, l);
+    foreach (i; 0 .. N) dp[i][] = long.max;
+    dp[0][0] = 0;
+    foreach (i; 0 .. l) {
+        foreach (j; 0 .. N) {
+            if (dp[j][i] == long.max) continue;
+
+            foreach (k; 0 .. N) {
+                if (j == k) continue;
+
+                dp[k][i|(1<<k)] = min(dp[k][i|(1<<k)], dp[j][i]+dists[j][k]);
+            }
+        }
+    }
+
+    long res = dp[0][l-1];
+    res.writeln;
+}
