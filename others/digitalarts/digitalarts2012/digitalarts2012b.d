5f345be40af2e4f452ade22792dbfae901818e60
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-04 23:18:46 +0900

    solve digitalarts2012b

---
+++ others/digitalarts/digitalarts2012/digitalarts2012b.d
@@ -0,0 +1,36 @@
+import std;
+
+void main() {
+    string s;
+    readf("%s\n", s);
+
+    if (s == "a" || s == "zzzzzzzzzzzzzzzzzzzz") {
+        "NO".writeln;
+    }
+    else {
+        auto res = s.to!(dchar[]);
+        res.sort;
+
+        if (res[0] == res[$-1]) {
+            if (res[0] == 'a') {
+                res.popBack;
+                ++res[$-1];
+            }
+            else if (res[0] == 'z' || res.length == 1) {
+                --res[$-1];
+                res ~= 'a';
+            }
+            else {
+                ++res[0];
+                --res[$-1];
+            }
+        }
+        else {
+            if (res.to!string == s) {
+                res.reverse;
+            }
+        }
+
+        res.writeln;
+    }
+}
