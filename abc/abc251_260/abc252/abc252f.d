dab58c83a63d6ffc1c4a5d25e1277051d138a7e8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-08 23:29:09 +0900

    solve abc252f

---
+++ abc/abc251_260/abc252/abc252f.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    long N, L;
+    readf("%d %d\n", N, L);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    auto heap = A.heapify!"a > b";
+    if (A.sum < L) heap.insert(L-A.sum);
+
+    long res;
+    while (heap.length > 1) {
+        long s = heap.take(2).sum;
+        res += s;
+        heap.insert(s);
+    }
+
+    res.writeln;
+}
