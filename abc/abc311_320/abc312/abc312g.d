24c26bdab9c117bdf585d85042bc019efb803d80
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-11 22:49:07 +0900

    solve abc312g with commentary

---
+++ abc/abc311_320/abc312/abc312g.d
@@ -0,0 +1,39 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto edges = new long[][](N);
+    foreach (_; 0 .. N-1) {
+        long A, B;
+        readf("%d %d\n", A, B);
+
+        --A, --B;
+        edges[A] ~= B, edges[B] ~= A;
+    }
+
+    auto cnts = new long[](N);
+    cnts[] = 1;
+
+    long res = N * (N - 1) * (N - 2) / 6;
+
+    void f(long x, long y = -1) {
+        long num = (N - 1) ^^ 2;
+        foreach (e; edges[x]) {
+            if (e == y) continue;
+
+            f(e, x);
+            num -= cnts[e] ^^ 2;
+            cnts[x] += cnts[e];
+        }
+
+        if (y != - 1) num -= (N - cnts[x]) ^^ 2;
+
+        res -= num / 2;
+    }
+
+    f(0);
+
+    res.writeln;
+}
