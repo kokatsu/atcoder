e5e51758ff9ced23e0368cb202b65586be3777ee
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-16 22:45:17 +0900

    solve tenka1_2012_final_a

---
+++ others/tenka1/tenka1_2012/tenka1_2012_final/tenka1_2012_final_a.d
@@ -0,0 +1,21 @@
+import std;
+
+void main() {
+    long n;
+    readf("%d\n", n);
+
+    long[] fib = [0, 1];
+    while (fib.back < n) {
+        fib ~= fib[$-2] + fib[$-1];
+    }
+
+    long res;
+    foreach_reverse (f; fib) {
+        if (f > 0 && f <= n) {
+            n -= f;
+            ++res;
+        }
+    }
+
+    res.writeln;
+}
