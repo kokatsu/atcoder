dc5aef04c87802c5d09ae4689aebff117716e1b6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-21 23:24:30 +0900

    solve tenka1_2014_qualb_b

---
+++ others/tenka1/tenka1_2014/tenka1_2014_qualb/tenka1_2014_qualb_b.d
@@ -0,0 +1,30 @@
+import std;
+
+enum long MOD = 10 ^^ 9 + 7;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    string S;
+    readf("%s\n", S);
+
+    auto T = new string[](N);
+    foreach (i; 0 .. N) readf("%s\n", T[i]);
+
+    auto L = S.length;
+
+    auto dp = new long[](L+1);
+    dp[0] = 1;
+    foreach (i; 0 .. L) {
+        foreach (t; T) {
+            auto l = t.length;
+            if (l + i > L) continue;
+
+            if (S[i..i+l] == t) dp[i+l] = (dp[i+l] + dp[i]) % MOD;
+        }
+    }
+
+    long res = dp[L];
+    res.writeln;
+}
