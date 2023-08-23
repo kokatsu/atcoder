328e34441a8f92430e3dea12aa8c2c7e62141657
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-23 22:27:39 +0900

    solve tenka1_2015_qualb_a

---
+++ others/tenka1/tenka1_2015/tenka1_2015_qualb/tenka1_2015_qualb_a.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int n = 20;
+
+    auto a = new int[](n);
+    a[0] = a[1] = 100, a[2] = 200;
+    foreach (i; 3 .. n) {
+        a[i] = a[i-1] + a[i-2] + a[i-3];
+    }
+
+    int res = a[n-1];
+    res.writeln;
+}
