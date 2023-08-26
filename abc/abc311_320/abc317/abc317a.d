606dee2403b9c2090b844380ddac8c0683689f32
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-26 21:04:28 +0900

    solve abc317a

---
+++ abc/abc311_320/abc317/abc317a.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    int N, H, X;
+    readf("%d %d %d\n", N, H, X);
+
+    auto P = readln.chomp.split.to!(int[]).assumeSorted;
+
+    auto lb = P.lowerBound(X-H);
+
+    auto res = lb.length + 1;
+    res.writeln;
+}
