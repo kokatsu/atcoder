304c457dea9007f0bd6d07a1126c631c706b78d1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-26 21:47:09 +0900

    solve abc317d

---
+++ abc/abc311_320/abc317/abc317d.d
@@ -0,0 +1,44 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto X = new long[](N), Y = new long[](N), Z = new long[](N);
+    foreach (i; 0 .. N) readf("%d %d %d\n", X[i], Y[i], Z[i]);
+
+    long S = Z.sum, L = S / 2 + 1;
+    foreach (i; 0 .. N) {
+        if (X[i] > Y[i]) L -= Z[i];
+    }
+
+    if (L <= 0) {
+        0.writeln;
+        return;
+    }
+
+    long[long] list;
+    list[0] = 0;
+    long res = long.max;
+    foreach (i; 0 .. N) {
+        if (X[i] > Y[i]) continue;
+
+        long[long] next = list.dup;
+        foreach (l; list.byKeyValue) {
+            long num = l.key + Z[i];
+            if (num >= L) {
+                res = min(res, l.value+((X[i]+Y[i])/2+1)-X[i]);
+            }
+            else if (num in next) {
+                next[num] = min(next[num], l.value+((X[i]+Y[i])/2+1)-X[i]);
+            }
+            else {
+                next[num] = l.value + ((X[i] + Y[i]) / 2 + 1) - X[i];
+            }
+        }
+
+        list = next;
+    }
+
+    res.writeln;
+}
