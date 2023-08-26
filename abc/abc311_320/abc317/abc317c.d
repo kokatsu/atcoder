bf52451c106898bb1ec1358261a27ca42ccd7293
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-26 21:16:15 +0900

    solve abc317c

---
+++ abc/abc311_320/abc317/abc317c.d
@@ -0,0 +1,34 @@
+import std;
+
+
+
+void main() {
+    long N, M;
+    readf("%d %d\n", N, M);
+
+    auto roads = new long[][](N, N);
+    foreach (_; 0 .. M) {
+        long A, B, C;
+        readf("%d %d %d\n", A, B, C);
+
+        --A, --B;
+        roads[A][B] = roads[B][A] = C;
+    }
+
+    long res;
+    auto seen = new bool[](N);
+
+    void f(long to, long num, ref bool[] arr) {
+        arr[to] = true;
+        res = max(res, num);
+        foreach (i; 0 .. N) {
+            if (arr[i] || roads[to][i] == 0) continue;
+            f(i, num+roads[to][i], arr);
+        }
+        arr[to] = false;
+    }
+
+    foreach (i; 0 .. N) f(i, 0, seen);
+
+    res.writeln;
+}
