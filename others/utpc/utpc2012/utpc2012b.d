deaadff85ce165352218e0ce9e0f77fa426eef29
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-30 01:07:50 +0900

    solve utpc2012b

---
+++ others/utpc/utpc2012/utpc2012b.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    auto used = new bool[](8);
+    dchar[] res;
+    foreach_reverse (s; S) {
+        if (!used[s-'A']) {
+            res ~= s;
+            used[s-'A'] = true;
+        }
+    }
+
+    foreach (i; 0 .. 8) {
+        if (!used[i]) res ~= 'A' + i;
+    }
+
+    res.reverse;
+
+    res.writeln;
+}
