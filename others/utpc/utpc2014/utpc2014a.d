cd793115fe66f9b786b9db9b1d96876b78920297
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-03 23:25:37 +0900

    solve utpc2014a

---
+++ others/utpc/utpc2014/utpc2014a.d
@@ -0,0 +1,30 @@
+import std;
+
+void main() {
+    auto S = readln.chomp.split;
+
+    bool existNot, existOther;
+    string[] T;
+    foreach_reverse (s; S) {
+        if (s == "not") {
+            if (existNot) {
+                if (!existOther) T ~= "not", T ~= "not";
+            }
+            existNot ^= true;
+        }
+        else {
+            if (existNot) {
+                T ~= "not";
+                existNot = false;
+            }
+            T ~= s;
+            existOther = true;
+        }
+    }
+
+    if (existNot) T ~= "not";
+
+    T.reverse;
+
+    writefln("%-(%s %)", T);
+}
