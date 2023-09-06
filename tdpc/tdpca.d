3de73795ec3745b03cbcd2cccce6d10d5ad6fb89
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-07 00:38:52 +0900

    solve tdpca

---
+++ tdpc/tdpca.d
@@ -0,0 +1,21 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto p = readln.chomp.split.to!(int[]);
+
+    auto l = p.sum + 1;
+
+    auto points = new bool[](l);
+    points[0] = true;
+    foreach (x; p) {
+        foreach_reverse (i; 0 .. l-x) {
+            points[i+x] |= points[i];
+        }
+    }
+
+    auto res = points.count(true);
+    res.writeln;
+}
