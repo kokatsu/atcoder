39059c80588f30032a9f89dd431feba7cddd11f2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-08 23:42:00 +0900

    solve kupc2015a

---
+++ others/kupc/kupc2015/kupc2015a.d
@@ -0,0 +1,25 @@
+import std;
+
+void main() {
+    int T;
+    readf("%d\n", T);
+
+    foreach (_; 0 .. T) {
+        string S;
+        readf("%s\n", S);
+
+        auto L = S.length;
+        ulong res, i;
+        while (i + 5 <= L) {
+            if (S[i..i+5] == "tokyo" || S[i..i+5] == "kyoto") {
+                ++res;
+                i += 5;
+            }
+            else {
+                ++i;
+            }
+        }
+
+        res.writeln;
+    }
+}
