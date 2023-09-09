f313136b4a719254efcc5ca657d2ab279fa1a119
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-09 23:39:04 +0900

    solve kupc2016a

---
+++ others/kupc/kupc2016/kupc2016a.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N, A, B;
+    readf("%d %d %d\n", N, A, B);
+
+    int res = N;
+    foreach (_; 0 .. N) {
+        int t;
+        readf("%d\n", t);
+
+        if (A <= t && t < B) --res;
+    }
+
+    res.writeln;
+}
