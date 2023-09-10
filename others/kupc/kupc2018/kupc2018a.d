6b888635e479ed85f8a59f65b9d3274f5660bf6c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-11 00:49:35 +0900

    solve kupc2018a

---
+++ others/kupc/kupc2018/kupc2018a.d
@@ -0,0 +1,12 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto s = readln.chomp.split.to!(int[]);
+    auto a = readln.chomp.split.to!(int[]);
+
+    int res = zip(s, a).map!(x => x[0] * x[1]).fold!max;
+    res.writeln;
+}
