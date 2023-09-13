60b1964cf9592219fbc3c42483149ab54d0962c0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-13 09:02:02 +0900

    solve kupc2019a

---
+++ others/kupc/kupc2019/kupc2019a.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N, X;
+    readf("%d %d\n", N, X);
+
+    auto a = readln.chomp.split.to!(int[]);
+
+    int mx = a.maxElement;
+
+    int res;
+    foreach (u; a) {
+        if (u + X >= mx) ++res;
+    }
+
+    res.writeln;
+}
