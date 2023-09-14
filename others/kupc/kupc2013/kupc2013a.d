364abfb4583c523d96eb8cd0a1a7d8490d290b66
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-15 01:11:30 +0900

    solve kupc2013a

---
+++ others/kupc/kupc2013/kupc2013a.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N, Q;
+    readf("%d %d\n", N, Q);
+
+    string res = "kogakubu10gokan";
+    foreach (_; 0 .. N) {
+        int year;
+        string name;
+        readf("%d %s\n", year, name);
+
+        if (year <= Q) res = name;
+    }
+
+    res.writeln;
+}
