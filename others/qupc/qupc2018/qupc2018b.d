11b9c641b3faa032c05dd52126293f560afce1a5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-19 00:35:41 +0900

    solve qupc2018b

---
+++ others/qupc/qupc2018/qupc2018b.d
@@ -0,0 +1,31 @@
+import std;
+
+void main() {
+    int Q;
+    readf("%d\n", Q);
+
+    foreach (_; 0 .. Q) {
+        long A, B, C;
+        readf("%d %d %d\n", A, B, C);
+
+        long S = A * 100 + B * 10 + C;
+
+        string res = "No";
+        if (S % 2 == 0) {
+            long rem = S / 2;
+
+            long a = min(A, rem/100);
+            rem -= a * 100;
+
+            long b = min(B, rem/10);
+            rem -= b * 10;
+
+            long c = min(C, rem);
+            rem -= c;
+
+            if (rem == 0) res = "Yes";
+        }
+
+        res.writeln;
+    }
+}
