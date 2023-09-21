d31570d6c0a20660cab09f7c197f67fd2543057a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-22 01:36:54 +0900

    solve qupc2014b

---
+++ others/qupc/qupc2014/qupc2014b.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    string[] list = ["nil", "un", "bi", "tri", "quad", "pent", "hex", "sept", "oct", "enn"];
+
+    auto N = readln.chomp.to!(dchar[]);
+
+    string str = N.map!(c => list[c-'0']).join ~ "ium";
+    str = str.replace("ii", "i");
+    str = str.replace("ennn", "enn");
+
+    string res = str.capitalize;
+    res.writeln;
+}
