fb961f3832ac1276db17295e577d9813329615b5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-23 23:55:55 +0900

    solve qupc2014c

---
+++ others/qupc/qupc2014/qupc2014c.d
@@ -0,0 +1,30 @@
+import std;
+
+void main() {
+    ulong N, M, Q;
+    readf("%d %d %d\n", N, M, Q);
+
+    Tuple!(ulong, ulong)[char] pos;
+    foreach (i; 0 .. N) {
+        string s;
+        readf("%s\n", s);
+
+        foreach (j, x; s) {
+            if (x == '*') continue;
+
+            pos[x] = tuple(i+1, j+1);
+        }
+    }
+
+    foreach (_; 0 .. Q) {
+        char q;
+        readf("%c\n", q);
+
+        if (q in pos) {
+            writeln(pos[q][0], " ", pos[q][1]);
+        }
+        else {
+            writeln("NA");
+        }
+    }
+}
