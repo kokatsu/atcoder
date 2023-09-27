153049f595302951bace0cf0dc988b31535751bc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-28 01:08:06 +0900

    solve tkppc2b

---
+++ others/tkppc/tkppc2/tkppc2b.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto dp = new int[](M+1);
+    foreach (_; 0 .. N) {
+        int V, T;
+        readf("%d %d\n", V, T);
+
+        foreach_reverse (i; 0 .. M-T+1) {
+            dp[i+T] = max(dp[i+T], dp[i]+V);
+        }
+    }
+
+    int res = dp[M];
+    res.writeln;
+}
