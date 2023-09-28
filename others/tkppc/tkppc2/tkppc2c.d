cfcee3dddb577fd93e7d5e4e329abeabd7d7d16b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-29 01:04:06 +0900

    solve tkppc2c

---
+++ others/tkppc/tkppc2/tkppc2c.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto H = readln.chomp.split.to!(int[]);
+
+    int res, cnt, r;
+    foreach (l; 0 .. N) {
+        while (r < N && cnt + 1 - H[r] <= K) {
+            cnt += 1 - H[r++];
+        }
+
+        res = max(res, r-l);
+
+        if (l < r) {
+            cnt -= 1 - H[l];
+        }
+        else {
+            ++r;
+        }
+    }
+
+    res.writeln;
+}
