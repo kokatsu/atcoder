58f7a416ba61ee186f7bb68440f5a6faf2a012c9
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-01 23:38:07 +0900

    solve tkppc3b

---
+++ others/tkppc/tkppc3/tkppc3b.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    string N;
+    readf("%s\n", N);
+
+    int total, odd, even;
+    foreach (i, n; N) {
+        int d = n - '0';
+
+        total += d;
+        (i % 2 == 1 ? odd : even) += d;
+    }
+
+    string res1 = (total % 3 == 0) && ((N[$-1] - '0') % 2 == 0) ? "yES" : "nO";
+    string res2 = abs(odd-even) % 11 == 0 ? "yES" : "nO";
+    res1.writeln;
+    res2.writeln;
+}
