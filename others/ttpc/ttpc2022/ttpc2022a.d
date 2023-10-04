86531aa445e434b85672418a91be56920259bb82
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-05 00:40:56 +0900

    solve ttpc2022a

---
+++ others/ttpc/ttpc2022/ttpc2022a.d
@@ -0,0 +1,34 @@
+import std;
+
+enum long B = 2015;
+
+void main() {
+    long X, Y;
+    readf("%d %d\n", X, Y);
+
+    X -= B, Y -= B;
+
+    long d = 1;
+    bool[long] list1;
+    while (d * d <= X) {
+        if (X % d == 0) list1[d] = list1[X/d] = true;
+        ++d;
+    }
+
+    d = 1;
+    long Z = Y - X;
+    bool[long] list2;
+    while (d * d <= Z) {
+        if (Z % d == 0) {
+            if (d in list1) list2[d] = true;
+            if (Z / d in list1) list2[Z/d] = true;
+        }
+        ++d;
+    }
+
+    auto res = list2.keys;
+
+    res.sort;
+
+    writefln("%(%s\n%)", res);
+}
