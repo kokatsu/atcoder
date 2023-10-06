ce7c890f3049bbde2f5e20935356b44f99d60d0b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-06 23:26:23 +0900

    solve ttpc2015a

---
+++ others/ttpc/ttpc2015/ttpc2015a.d
@@ -0,0 +1,10 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    string[char] courses = ['B': "Bachelor", 'M': "Master", 'D': "Doctor"];
+
+    writeln(courses[S[2]], " ", S[0..2]);
+}
