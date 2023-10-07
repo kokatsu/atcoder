486b63c1cc6b68344227bc711ed225c26f8b35fc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-07 21:12:03 +0900

    solve abc323b

---
+++ abc/abc321_330/abc323/abc323b.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto C = new ulong[](N);
+    foreach (i; 0 .. N) {
+        string S;
+        readf("%s\n", S);
+
+        C[i] = S.count('o');
+    }
+
+    auto res = iota(1, N+1).array;
+
+    zip(C, res).sort!"a[0] == b[0] ? a[1] < b[1] : a[0] > b[0]";
+
+    writefln("%(%s %)", res);
+}
