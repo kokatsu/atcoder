0d853c89e4fb259bfd69ee85995802a0a093886e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-10 01:03:02 +0900

    solve s8pc2a

---
+++ others/s8pc/s8pc2/s8pc2a.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    ulong L = S.length;
+
+    auto dp = new int[][](L+1, 2);
+    foreach (i, s; S) {
+        if (s == 'I') {
+            dp[i+1][0] = max(1, dp[i][1]+1);
+            dp[i+1][1] = dp[i][1];
+        }
+        else {
+            dp[i+1][0] = dp[i][0];
+            dp[i+1][1] = dp[i][0] > 0 ? dp[i][0] + 1 : 0;
+        }
+    }
+
+    int res = dp[L][0];
+    res.writeln;
+}
