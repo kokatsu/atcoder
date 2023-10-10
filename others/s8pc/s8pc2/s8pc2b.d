7f426025fdc82bb34a7c9a208e6f6a9494d9b9ce
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-11 00:21:22 +0900

    solve s8pc2b

---
+++ others/s8pc/s8pc2/s8pc2b.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    long n, q;
+    readf("%d %d\n", n, q);
+
+    auto a = new long[](q);
+    foreach (i; 0 .. q) readf("%d\n", a[i]);
+
+    long f(long x, long y = 1) {
+        if (y > n) return 0;
+        if (x < 0) return 1;
+        if (y % a[x] == 0) return f(x-1, y*a[x]);
+        return f(x-1, y) + f(x-1, y*a[x]);
+    }
+
+    long res = f(q-1);
+    res.writeln;
+}
