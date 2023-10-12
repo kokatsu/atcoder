1eeb1dc887cc307094be1b2ee71e6acd9590560a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-12 22:50:00 +0900

    solve s8pc3a

---
+++ others/s8pc/s8pc3/s8pc3a.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    long n, k;
+    readf("%d %d\n", n, k);
+
+    long res;
+    foreach (i; 0 .. 11) {
+        long cnt;
+
+        foreach (j; 1 .. 6) {
+            long rem = ((i + 1) * 63 + j * 9 + 9) % 11;
+            if (rem == k) ++cnt;
+        }
+
+        res += cnt * (n - i + 8) / 11;
+    }
+
+    res.writeln;
+}
