106f9fbebf70d269c94660271b73afa8fe41cf88
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-14 16:18:44 +0900

    solve ttpc2023c

---
+++ others/ttpc/ttpc2023/ttpc2023c.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int T;
+    readf("%d\n", T);
+
+    foreach (_; 0 .. T) {
+        long N;
+        readf("%d\n", N);
+
+        long S = to!real(N-1).sqrt.floor.to!long;
+
+        long M = N - S * S;
+
+        long res = min(S^^2, (S-1)^^2+M*2);
+        res.writeln;
+    }
+}
