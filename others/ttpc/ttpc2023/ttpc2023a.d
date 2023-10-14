378359019918ed9e0b1cad55b501ed81d9419ac4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-14 16:17:58 +0900

    solve ttpc2023a

---
+++ others/ttpc/ttpc2023/ttpc2023a.d
@@ -0,0 +1,25 @@
+import std;
+
+enum long MOD = 998244353;
+
+long powMod(long x, long y) {
+    long res = 1;
+    while (y > 0) {
+        if (y & 1) {
+            res *= x;
+            if (res > MOD) res %= MOD;
+        }
+        x *= x;
+        if (x > MOD) x %= MOD;
+        y >>= 1;
+    }
+    return res;
+}
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    long res = (powMod(2, N) + MOD - N - 1) % MOD;
+    res.writeln;
+}
