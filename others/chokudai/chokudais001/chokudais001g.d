681d6fae6731c4540d8888dfa3eface6409e60fc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-20 23:42:25 +0900

    solve chokudais001g

---
+++ others/chokudai/chokudais001/chokudais001g.d
@@ -0,0 +1,18 @@
+import std;
+
+enum long MOD = 10 ^^ 9 + 7;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto a = readln.chomp.split.to!(long[]);
+
+    long res, base = 1;
+    foreach_reverse (x; a) {
+        res = (res + x * base) % MOD;
+        base = (base * (10 ^^ x.to!string.length.to!long)) % MOD;
+    }
+
+    res.writeln;
+}
