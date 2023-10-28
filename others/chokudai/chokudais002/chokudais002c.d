6d4c9077f473ecaead1484726d1deb6736a66fc2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-28 23:41:23 +0900

    solve chokudais002c

---
+++ others/chokudai/chokudais002/chokudais002c.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int res;
+    foreach (_; 0 .. N) {
+        int A, B;
+        readf("%d %d\n", A, B);
+
+        res = max(res, A+B);
+    }
+
+    res.writeln;
+}
