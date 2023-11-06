af47e72bc0fb1b0b3f0b16dad1bd44a1ca38f790
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-11-07 00:20:33 +0900

    solve tkppc4b

---
+++ others/tkppc/tkppc4/tkppc4b.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N, K;
+    readf("%d %d\n", N, K);
+
+    auto a = readln.chomp.split.to!(int[]);
+
+    auto b = iota(1, N+1).array;
+
+    zip(a, b).sort;
+
+    auto lb = a.assumeSorted.lowerBound(K);
+
+    int res = lb.empty ? -1 : b[lb.length.to!int-1];
+    res.writeln;
+}
