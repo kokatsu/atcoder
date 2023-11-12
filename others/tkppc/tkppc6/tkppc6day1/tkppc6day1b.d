d9d9f1e2f61ca215886f348a9361afa1dd39aea0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-11-13 01:23:48 +0900

    solve tkppc6day1b

---
+++ others/tkppc/tkppc6/tkppc6day1/tkppc6day1b.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int L = A.sort.uniq.array.length.to!int;
+
+    int res = min(N, L+M);
+    res.writeln;
+}
