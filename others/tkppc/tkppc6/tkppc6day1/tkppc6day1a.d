8b7d42eff2c2f01d728492b78b1cebd69dc02331
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-11-12 23:33:17 +0900

    solve tkppc6day1a

---
+++ others/tkppc/tkppc6/tkppc6day1/tkppc6day1a.d
@@ -0,0 +1,12 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int res = N - 2015;
+    if (N <= 2014 || N == 2017) res = -1;
+    else if (N <= 2016) res = N - 2014;
+
+    res.writeln;
+}
