a42161ab61a02baf8daf086873b4026bc6bef7ab
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-11-18 23:54:40 +0900

    solve iroha2019day1d

---
+++ others/iroha2019/iroha2019day1/iroha2019day1d.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N, X, Y;
+    readf("%d %d %d\n", N, X, Y);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    A.sort!"a > b";
+
+    int T = A.stride(2).sum;
+
+    int U = X + T, V = Y + A.sum - T;
+
+    string res = "Draw";
+    if (U > V) res = "Takahashi";
+    if (U < V) res = "Aoki";
+
+    res.writeln;
+}
