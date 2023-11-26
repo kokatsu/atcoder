96f83b05c147343fc73f201ad70a085ddb622160
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-11-27 01:39:06 +0900

    solve tkppc4day1i

---
+++ others/tkppc/tkppc4/tkppc4day1/tkppc4day1i.d
@@ -0,0 +1,28 @@
+import std;
+
+enum long MOD = 10 ^^ 9 + 7;
+
+void main() {
+    long N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = readln.chomp.split.to!(long[]);
+    auto B = readln.chomp.split.to!(long[]);
+
+    A.sort;
+    auto C = B.sort;
+
+    auto L = new long[](N), R = new long[](N);
+    foreach_reverse (i, a; A) {
+        L[i] = C.lowerBound(a).length;
+        R[i] = (R[i] + C.upperBound(a).length) % MOD;
+        if (i > 0) R[i-1] += R[i];
+    }
+
+    long res;
+    foreach (i; 0 .. N-1) {
+        res = (res + L[i] * R[i+1]) % MOD;
+    }
+
+    res.writeln;
+}
