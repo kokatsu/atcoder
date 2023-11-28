8bd4842f53054e3141fcbbe02bdc4d4295889d6b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-11-29 01:16:21 +0900

    solve cpsco2019session1b

---
+++ others/cpsco/cpsco2019/cpsco2019session1/cpsco2019session1b.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    int[char] dict;
+    foreach (s; S) ++dict[s];
+
+    int[] vals = dict.values;
+
+    string res = vals.all!(a => a == vals[0]) ? "Yes" : "No";
+    res.writeln;
+}
