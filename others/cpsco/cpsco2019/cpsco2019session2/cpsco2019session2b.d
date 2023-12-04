627f1159fd3fd762958f215068b8011509ddf530
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-12-04 23:46:36 +0900

    solve cpsco2019session2b

---
+++ others/cpsco/cpsco2019/cpsco2019session2/cpsco2019session2b.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int add, mul = 1;
+    foreach (i_; 0 .. N) {
+        char c;
+        int a;
+        readf("%c %d\n", c, a);
+
+        if (c == '+') add += a;
+        if (c == '*' && a > 0) mul *= a;
+    }
+
+    int res = add * mul;
+    res.writeln;
+}
