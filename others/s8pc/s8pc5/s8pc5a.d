e76d34a8fc5c45cdb9b0a329134d0d856deb67bc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-12-12 23:59:45 +0900

    solve s8pc5a

---
+++ others/s8pc/s8pc5/s8pc5a.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    int N, T;
+    readf("%d %d\n", N, T);
+
+    auto a = readln.chomp.split.to!(int[]);
+
+    int res = a[N-1];
+    foreach (i; 0 .. N-1) {
+        if (a[i] > a[i+1]) res += T;
+    }
+
+    res.writeln;
+}
