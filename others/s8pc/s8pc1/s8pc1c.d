d5f452af200e62d37241ec9c0c30a8678bc72fac
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-12-13 23:11:09 +0900

    solve s8pc1c

---
+++ others/s8pc/s8pc1/s8pc1c.d
@@ -0,0 +1,45 @@
+import std;
+
+void main() {
+    long N, K;
+    readf("%d %d\n", N, K);
+
+    auto D = new long[](N);
+    foreach (i; 0 .. N) readf("%d\n", D[i]);
+
+    auto roads = new long[][](N);
+    foreach (_; 0 .. K) {
+        long X, Y;
+        readf("%d %d\n", X, Y);
+
+        --X, --Y;
+
+        roads[X] ~= Y, roads[Y] ~= X;
+    }
+
+    long res;
+
+    void f(long x) {
+        auto seen = new bool[](N);
+
+        void g(long pos, long num = 0) {
+            seen[pos] = true;
+            num += D[pos];
+            res = max(res, num);
+
+            foreach (r; roads[pos]) {
+                if (seen[r]) continue;
+
+                g(r, num);
+            }
+
+            seen[pos] = false;
+        }
+
+        g(x);
+    }
+
+    foreach (i; 0 .. N) f(i);
+
+    res.writeln;
+}
