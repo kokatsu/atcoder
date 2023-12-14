5b667d6dbcd457296cfcd10405d3e5920e5d55fe
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-12-15 00:53:51 +0900

    solve joi2024yo2a

---
+++ joi/joi2024/joi2024yo2/joi2024yo2a.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    bool[int] dict;
+    foreach (a; A) dict[a] = true;
+
+    bool isOk;
+    foreach (a; A) isOk |= (a - 3 in dict) && (a + 3 in dict);
+
+    string res = isOk ? "Yes" : "No";
+    res.writeln;
+}
