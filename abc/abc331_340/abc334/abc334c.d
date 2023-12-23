a862fdd38931edbf7b04a68291e3b88f0c4a7ad7
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-12-23 21:51:27 +0900

    solve abc334c

---
+++ abc/abc331_340/abc334/abc334c.d
@@ -0,0 +1,29 @@
+import std;
+
+void main() {
+    long N, K;
+    readf("%d %d\n", N, K);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    long res, M = K / 2;
+    if (K % 2 == 0) {
+        foreach (i; 0 .. M) {
+            res += A[i*2+1] - A[i*2];
+        }
+    }
+    else {
+        auto L = new long[](M+1);
+        foreach (i; 0 .. M) L[i+1] = A[i*2+1] - A[i*2] + L[i];
+
+        auto R = new long[](M+1);
+        foreach_reverse (i; 0 .. M) R[i] = A[(i+1)*2] - A[i*2+1] + R[i+1];
+
+        res = long.max;
+        foreach (i; 0 .. M+1) {
+            res = min(res, L[i]+R[i]);
+        }
+    }
+
+    res.writeln;
+}
