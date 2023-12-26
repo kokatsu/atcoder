36e864b6f4f06226445e1e566347c480abee9a57
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-12-27 01:04:00 +0900

    solve joi2024yo1bd

---
+++ joi/joi2024/joi2024yo1b/joi2024yo1bd.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int X, N;
+    readf("%d\n%d\n", X, N);
+
+    int res;
+    while (X < N) {
+        if (X % 3 == 0) ++X;
+        else if (X % 3 == 1) X *= 2;
+        else X *= 3;
+
+        ++res;
+    }
+
+    res.writeln;
+}
