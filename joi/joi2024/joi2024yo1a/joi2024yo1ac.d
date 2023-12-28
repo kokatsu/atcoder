ec36d57e9558efedbec26b0d594f677ddcab1cbb
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-12-29 00:21:58 +0900

    solve joi2024yo1ac

---
+++ joi/joi2024/joi2024yo1a/joi2024yo1ac.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int N;
+    string S, T;
+    readf("%d\n%s\n%s\n", N, S, T);
+
+    int res;
+    foreach (i; 0 .. N) {
+        if (S[i] != T[i]) ++res;
+    }
+
+    res.writeln;
+}
