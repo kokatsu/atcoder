3b28009440d225099388f4c7cfe78e209ae6d419
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-01-06 22:54:01 +0900

    solve oupc2023d

---
+++ others/oupc/oupc2023/oupc2023d.d
@@ -0,0 +1,32 @@
+import std;
+
+void main() {
+    auto S = readln.chomp.to!(dchar[]);
+
+    size_t L = S.length;
+
+    DList!dchar list;
+    foreach (i; 1 .. L-1) {
+        if (!list.empty && list.back == S[i]) {
+            list.removeBack;
+        }
+        else {
+            list.insertBack(S[i]);
+        }
+    }
+
+    list.insertFront(S[0]);
+    if (L > 1) {
+        if (list.back == S[L-1]) {
+            list.removeBack;
+        }
+        else {
+            list.insertBack(S[L-1]);
+        }
+    }
+
+    auto res = list[];
+    res.reverse;
+
+    res.writeln;
+}
