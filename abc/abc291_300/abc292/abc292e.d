d7c19ccb77b84cac7974ddaa4d90768ae383863f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-01-09 23:45:48 +0900

    solve abc292e

---
+++ abc/abc291_300/abc292/abc292e.d
@@ -0,0 +1,36 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto edges = new int[][](N);
+    foreach (_; 0 .. M) {
+        int u, v;
+        readf("%d %d\n", u, v);
+
+        edges[u-1] ~= v - 1;
+    }
+
+    int res;
+
+    void dfs(int now, ref bool[] seen) {
+        seen[now] = true;
+
+        foreach (e; edges[now]) {
+            if (seen[e]) continue;
+
+            ++res;
+            dfs(e, seen);
+        }
+    }
+
+    foreach (i; 0 .. N) {
+        auto seen = new bool[](N);
+        dfs(i, seen);
+    }
+
+    res -= M;
+
+    res.writeln;
+}
