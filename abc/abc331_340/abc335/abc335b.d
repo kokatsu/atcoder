8b918f33a40324f5acc7ada9524ab71af8d63f3e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-01-16 22:15:31 +0900

    solve abc335b

---
+++ abc/abc331_340/abc335/abc335b.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    foreach (x; 0 .. N+1) {
+        foreach (y; 0 .. N-x+1) {
+            foreach (z; 0 .. N-x-y+1) {
+                writefln("%d %d %d", x, y, z);
+            }
+        }
+    }
+}
