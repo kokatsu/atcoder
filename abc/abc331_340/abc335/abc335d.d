8ab58190f2d3d6e67abd941850cdedef3e452a98
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-01-16 22:37:38 +0900

    solve abc335d

---
+++ abc/abc331_340/abc335/abc335d.d
@@ -0,0 +1,28 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto res = new string[][](N, N);
+    res[N/2][N/2] = "T";
+
+    int x, y, dx = 1, dy;
+    string num = "1";
+    foreach (i; 0 .. N*N-1) {
+        res[x][y] = num;
+        num = num.succ;
+
+        int nx = x + dx, ny = y + dy;
+        if (0 <= nx && nx < N && 0 <= ny && ny < N && res[nx][ny] == "") {
+            x = nx, y = ny;
+        }
+        else {
+            swap(dx, dy);
+            if (dy == 0) dx *= -1;
+            x += dx, y += dy;
+        }
+    }
+
+    writefln("%(%-(%s %)\n%)", res);
+}
