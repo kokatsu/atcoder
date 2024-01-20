1dc3e34f68bc77c62c2427a4effe5915f4d46165
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-01-20 21:07:53 +0900

    solve abc337b

---
+++ abc/abc331_340/abc337/abc337b.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    auto G = S.group.array;
+
+    auto L = G.length;
+    string res = "No";
+    if (L <= 3) {
+        bool isOk = true;
+        foreach (i; 1 .. L) {
+            isOk &= G[i-1] < G[i];
+        }
+        if (isOk) res = "Yes";
+    }
+
+    res.writeln;
+}
