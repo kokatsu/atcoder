f40a519e15baed0aba8475371491d3524575e749
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-02-02 23:11:48 +0900

    solve joi2022yo1cd

---
+++ joi/joi2022/joi2022yo1c/joi2022yo1cd.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    int[] B = iota(N+1).array;
+    foreach (_; 0 .. M) {
+        int X, Y;
+        readf("%d %d\n", X, Y);
+
+        B[X] = Y;
+    }
+
+    writefln("%(%s\n%)", B[1..$]);
+}
