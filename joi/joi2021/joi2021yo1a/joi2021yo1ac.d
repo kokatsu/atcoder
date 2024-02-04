be378d36a3433b0a7e2fdad68e629bcca1df0267
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-02-05 02:33:00 +0900

    solve joi2021yo1ac

---
+++ joi/joi2021/joi2021yo1a/joi2021yo1ac.d
@@ -0,0 +1,21 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = readln.chomp.split.to!(int[]);
+    auto B = readln.chomp.split.to!(int[]);
+
+    B.sort;
+
+    auto C = A.assocArray(true.repeat);
+
+    auto res = B.uniq.filter!(x => x in C).array;
+
+    if (res.empty) return;
+
+    res.sort;
+
+    writefln("%(%d\n%)", res);
+}
