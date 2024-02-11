2be846837d7f310f88aa05179c678caf00afd376
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-02-11 23:41:53 +0900

    solve joi2021yo1cc

---
+++ joi/joi2021/joi2021yo1c/joi2021yo1cc.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = readln.chomp.split.to!(int[]);
+    auto B = readln.chomp.split.to!(int[]);
+
+    auto C = B.sort;
+
+    size_t res;
+    foreach (a; A) {
+        res += C.upperBound(a-1).length;
+    }
+
+    res.writeln;
+}
