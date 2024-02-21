5ffa9a3f682fc97fbcbaa361423765120da998f5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-02-22 03:29:24 +0900

    solve joi2020yo2b

---
+++ joi/joi2020/joi2020yo2/joi2020yo2b.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = new long[](N), T = new long[](N);
+    foreach (i; 0 .. N) readf("%d %d\n", A[i], T[i]);
+
+    zip(A, T).sort;
+
+    long res = max(A[N-1], T[N-1]);
+    foreach_reverse (i; 0 .. N-1) {
+        res = max(res+A[i+1]-A[i], T[i]);
+    }
+
+    res += A[0];
+
+    res.writeln;
+}
