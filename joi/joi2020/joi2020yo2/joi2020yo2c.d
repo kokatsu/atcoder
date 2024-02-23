196994107f89f69582d9d096bab8742169f7f05f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-02-23 23:13:59 +0900

    solve joi2020yo2c

---
+++ joi/joi2020/joi2020yo2/joi2020yo2c.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto dp = new int[](N+1);
+    dp[1..$] = 1;
+    foreach (i; 1 .. N) {
+        int ds, rem = i;
+        while (rem > 0) {
+            ds += rem % 10;
+            rem /= 10;
+        }
+
+        if (i + ds <= N) {
+            dp[i+ds] += dp[i];
+        }
+    }
+
+    int res = dp[N];
+    res.writeln;
+}
