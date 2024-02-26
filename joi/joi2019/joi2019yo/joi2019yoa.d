1ea93560c056869aa7a9f2f0be3f59d2d2ceccf6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-02-26 23:50:40 +0900

    solve joi2019yoa

---
+++ joi/joi2019/joi2019yo/joi2019yoa.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int A, B, C;
+    readf("%d %d %d\n", A, B, C);
+
+    int res, coins;
+    while (coins < C) {
+        coins += A;
+        if (++res % 7 == 0) coins += B;
+    }
+
+    res.writeln;
+}
