9a494d9663628e379e3674dedcdc082849653ee3
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-02-28 02:34:08 +0900

    solve joi2019yoc

---
+++ joi/joi2019/joi2019yo/joi2019yoc.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    string S;
+    readf("%d\n%s\n", N, S);
+
+    int res, idx;
+    while (idx < N) {
+        if (idx < N - 1 && S[idx] != S[idx+1]) {
+            ++res, idx += 2;
+        }
+        else {
+            ++idx;
+        }
+    }
+
+    res.writeln;
+}
