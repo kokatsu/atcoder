f54c5b0caca964e8a3db321f654337c51ee32193
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-01 01:50:19 +0900

    solve joi2018yob

---
+++ joi/joi2018/joi2018yo/joi2018yob.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    auto G = A.group.array;
+
+    uint res = G.length == 1 && G[0][0] == 0 ? 1 : G.filter!"a[0] == 1".maxElement!"a[1]"[1] + 1;
+    res.writeln;
+}
