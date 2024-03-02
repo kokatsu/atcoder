aff399ea9be43030a8d3aea76ab2295bc870c71d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-02 22:13:44 +0900

    solve joi2018yoc

---
+++ joi/joi2018/joi2018yo/joi2018yoc.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    int H, W;
+    readf("%d %d\n", H, W);
+
+    auto A = new int[][](H);
+    foreach (i; 0 .. H) A[i] = readln.chomp.split.to!(int[]);
+
+    int res = int.max;
+    foreach (m; 0 .. H) {
+        foreach (n; 0 .. W) {
+            int dist;
+            foreach (i; 0 .. H) {
+                foreach (j; 0 .. W) {
+                    dist += A[i][j] * min(abs(i-m), abs(j-n));
+                }
+            }
+            res = min(res, dist);
+        }
+    }
+
+    res.writeln;
+}
