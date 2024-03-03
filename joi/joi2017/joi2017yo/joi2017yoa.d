b907c504222af92c02532e2909c785d784920818
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-03 23:42:10 +0900

    solve joi2017yoa

---
+++ joi/joi2017/joi2017yo/joi2017yoa.d
@@ -0,0 +1,9 @@
+import std;
+
+void main() {
+    int A, B, C, D, E;
+    readf("%d\n%d\n%d\n%d\n%d\n", A, B, C, D, E);
+
+    int res = (A < 0 ? - A * C + D : 0) + (B - max(0, A)) * E;
+    res.writeln;
+}
