2d963ab74c86cb057e2ddacf3cf5a2af78f040e0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-08 00:28:34 +0900

    solve joi2016yob

---
+++ joi/joi2016/joi2016yo/joi2016yob.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = new int[](N);
+    foreach (i; 0 .. N) readf("%d\n", A[i]);
+
+    foreach (k; 1 .. M+1) {
+        foreach (i; 0 .. N-1) {
+            if (A[i] % k > A[i+1] % k) {
+                A.swapAt(i, i+1);
+            }
+        }
+    }
+
+    writefln("%(%s\n%)", A);
+}
