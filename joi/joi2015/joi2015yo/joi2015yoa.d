ca8ba70c9d183715275814e6ac968c86affd7201
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-10 23:50:07 +0900

    solve joi2015yoa

---
+++ joi/joi2015/joi2015yo/joi2015yoa.d
@@ -0,0 +1,11 @@
+import std;
+
+void main() {
+    int A, B, C, D, P;
+    readf("%d\n%d\n%d\n%d\n%d\n", A, B, C, D, P);
+
+    int X = A * P, Y = B + max(0, P-C) * D;
+
+    int res = min(X, Y);
+    res.writeln;
+}
