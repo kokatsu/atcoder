51ff2cbd4a7d4b78a7258eda592058d5208d79c2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-12 00:03:20 +0900

    solve joi2015yob

---
+++ joi/joi2015/joi2015yo/joi2015yob.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d\n%d\n", N, M);
+
+    auto A = readln.chomp.split.to!(int[]);
+    --A[];
+
+    auto res = new int[](N);
+    foreach (a; A) {
+        auto B = readln.chomp.split.to!(int[]);
+        --B[];
+
+        foreach (i, b; B) {
+            if (b == a) ++res[i];
+            else ++res[a];
+        }
+    }
+
+    writefln("%(%s\n%)", res);
+}
