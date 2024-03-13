0c418be7b77e1bba831f7a083018b40d9cac569a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-14 03:07:40 +0900

    solve joi2015yoc

---
+++ joi/joi2015/joi2015yo/joi2015yoc.d
@@ -0,0 +1,22 @@
+import std;
+
+void main() {
+    int H, W;
+    readf("%d %d\n", H, W);
+
+    auto res = new int[][](H, W);
+    foreach (i; 0 .. H) {
+        string S;
+        readf("%s\n", S);
+
+        int num = -1;
+        foreach (j, s; S) {
+            if (s == 'c') num = 0;
+            else if (num >= 0) ++num;
+
+            res[i][j] = num;
+        }
+    }
+
+    writefln("%(%(%s %)\n%)", res);
+}
