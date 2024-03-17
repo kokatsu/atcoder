b9e7e310e709050f919b790ca058f88f7554b6d2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-17 22:31:06 +0900

    solve joi2014yoc

---
+++ joi/joi2014/joi2014yo/joi2014yoc.d
@@ -0,0 +1,23 @@
+import std;
+
+void main() {
+    int W, H, N;
+    readf("%d %d %d\n", W, H, N);
+
+    int U, V;
+    readf("%d %d\n", U, V);
+
+    int res;
+    foreach (_; 1 .. N) {
+        int X, Y;
+        readf("%d %d\n", X, Y);
+
+        int dX = X - U, dY = Y - V;
+        if (dX.sgn == dY.sgn) res += max(dX.abs, dY.abs);
+        else res += dX.abs + dY.abs;
+
+        U = X, V = Y;
+    }
+
+    res.writeln;
+}
