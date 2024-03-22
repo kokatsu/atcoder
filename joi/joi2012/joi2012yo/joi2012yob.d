fcf61c4f0f4f13928b043c8364aa00c67f27e399
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-22 23:51:55 +0900

    solve joi2012yob

---
+++ joi/joi2012/joi2012yo/joi2012yob.d
@@ -0,0 +1,25 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    int M = N * (N - 1) / 2;
+    auto res = new int[](N);
+    foreach (_; 0 .. M) {
+        int A, B, C, D;
+        readf("%d %d %d %d\n", A, B, C, D);
+
+        --A, --B;
+
+        if (C == D) ++res[A], ++res[B];
+        else res[C>D?A:B] += 3;
+    }
+
+    auto S = res.dup.sort;
+    foreach (ref x; res) {
+        x = S.upperBound(x).length.to!int + 1;
+    }
+
+    writefln("%(%s\n%)", res);
+}
