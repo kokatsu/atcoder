4a5351f48f2109903d9c6a6696ff9838e7bb9c4c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-03 01:06:47 +0900

    solve joi2010yod

---
+++ joi/joi2010/joi2010yo/joi2010yod.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    int n, k;
+    readf("%d\n%d\n", n, k);
+
+    auto cards = new string[](n);
+    foreach (i; 0 .. n) readf("%s\n", cards[i]);
+
+    bool[string] set;
+    foreach (p; cards.permutations) {
+        set[p[0..k].join] = true;
+    }
+
+    auto res = set.length;
+    res.writeln;
+}
