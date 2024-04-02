570dad0a7511b3af01f799872256ffbb325da3d3
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-02 23:46:55 +0900

    solve joi2010yoc

---
+++ joi/joi2010/joi2010yo/joi2010yoc.d
@@ -0,0 +1,26 @@
+import std;
+
+void main() {
+    int n, m;
+    readf("%d\n%d\n", n, m);
+
+    auto friends = new int[][](n+1);
+    foreach (_; 0 .. m) {
+        int a, b;
+        readf("%d %d\n", a, b);
+
+        friends[a] ~= b, friends[b] ~= a;
+    }
+
+    auto invitation = new bool[](n+1);
+    foreach (f; friends[1]) {
+        invitation[f] = true;
+
+        foreach (g; friends[f]) {
+            if (g > 1) invitation[g] = true;
+        }
+    }
+
+    auto res = invitation.count(true);
+    res.writeln;
+}
