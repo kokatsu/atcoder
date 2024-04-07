ffeb870e5880b7fd12e788b74e6cb38dd369d988
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-07 22:52:02 +0900

    solve joi2008yob

---
+++ joi/joi2008/joi2008yo/joi2008yob.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    int joi, ioi;
+    foreach (i; 2 .. S.length) {
+        if (S[i-2..i+1] == "JOI") ++joi;
+        if (S[i-2..i+1] == "IOI") ++ioi;
+    }
+
+    string res = format("%d\n%d", joi, ioi);
+    res.writeln;
+}
