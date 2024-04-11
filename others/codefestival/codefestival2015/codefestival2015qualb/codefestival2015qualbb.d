7ceb1c67ee1c9964597af89ee6ef76fb101aa470
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-12 01:30:42 +0900

    solve codefestival2015qualbb

---
+++ others/codefestival/codefestival2015/codefestival2015qualb/codefestival2015qualbb.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int[int] dict;
+    foreach (a; A) ++dict[a];
+
+    int K = dict.byPair.maxElement!(p => p.value).key;
+
+    string res = dict[K] * 2 > N ? K.to!string() : "?";
+    res.writeln;
+}
