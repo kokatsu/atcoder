77fc52d97568b0602ad59a932506681babd4c894
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-24 23:41:48 +0900

    solve joi2006yoa

---
+++ joi/joi2006/joi2006yo/joi2006yoa.d
@@ -0,0 +1,24 @@
+import std;
+
+void main() {
+    int n;
+    readf("%d\n",n);
+
+    int A, B;
+    foreach (_; 0 .. n) {
+        int x, y;
+        readf("%d %d\n", x, y);
+
+        if (x == y) {
+            A += x, B += y;
+        }
+        else if (x > y) {
+            A += x + y;
+        }
+        else {
+            B += x + y;
+        }
+    }
+
+    writeln(A, " ", B);
+}
