a3190e51fff1c8dfa9671fc14a9cd14a459da3c0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-27 23:55:51 +0900

    solve joi2006hoa

---
+++ joi/joi2006/joi2006ho/joi2006hoa.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int n, m;
+    readf("%d %d\n", n, m);
+
+    auto c = new int[](m);
+    foreach (_; 0 .. n) {
+        auto a = readln.chomp.split.to!(int[]);
+        c[] += a[];
+    }
+
+    auto res = iota(1, m+1).array;
+
+    zip(c, res).sort!((x, y) => x[0] == y[0] ? x[1] < y[1] : x[0] > y[0]);
+
+    writefln("%(%s %)", res);
+}
