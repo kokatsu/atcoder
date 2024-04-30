86fa1470aabc3604105b03efeeee90e251ab4a72
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-01 00:32:52 +0900

    solve joi2007hob

---
+++ joi/joi2007/joi2007ho/joi2007hob.d
@@ -0,0 +1,35 @@
+import std;
+
+void main() {
+    int n, k;
+    readf("%d %d\n", n, k);
+
+    auto cards = new bool[](n+1);
+    bool blank;
+    foreach (_; 0 .. k) {
+        int c;
+        readf("%d\n", c);
+
+        if (c == 0) {
+            blank = true;
+        }
+        else {
+            cards[c] = true;
+        }
+    }
+
+    int res;
+    auto g = cards.group.array;
+    foreach (i, x; g) {
+        if (x[0] == false) continue;
+
+        int num = blank ? x[1] + 1 : x[1];
+        res = max(res, num);
+
+        if (blank && i >= 2 && g[i-1][1] == 1) {
+            res = max(res, g[i-2][1]+g[i-1][1]+x[1]);
+        }
+    }
+
+    res.writeln;
+}
