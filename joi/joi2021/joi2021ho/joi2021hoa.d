7842eaebcdf74f0badaa8e126d7d76d4579cf9d8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-06 23:40:40 +0900

    solve joi2021hoa

---
+++ joi/joi2021/joi2021ho/joi2021hoa.d
@@ -0,0 +1,31 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    auto L = new long[](N);
+    foreach (i; 1 .. N) {
+        L[i] += L[i-1];
+
+        long D = A[i-1] - A[i];
+        if (D >= 0) {
+            L[i] += D + 1;
+        }
+    }
+
+    auto R = new long[](N);
+    foreach_reverse (i; 0 .. N-1) {
+        R[i] += R[i+1];
+
+        long D = A[i+1] - A[i];
+        if (D >= 0) {
+            R[i] += D + 1;
+        }
+    }
+
+    long res = iota(N).map!(i => max(L[i], R[i])).minElement;
+    res.writeln;
+}
