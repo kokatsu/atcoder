da4e4d4250be71e5c929b976a1dd8b0ae92ae94d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-08 00:18:31 +0900

    solve codefestival2014finalb

---
+++ others/codefestival/codefestival2014/codefestival2014final/codefestival2014finalb.d
@@ -0,0 +1,11 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    auto L = S.length;
+
+    int res = iota(L).map!(i => to!int(S[i]-'0') * (i % 2 == 0 ? 1 : -1)).sum;
+    res.writeln;
+}
