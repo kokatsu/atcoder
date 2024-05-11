5a3776b3b7243359aa8db470dba0f511b4ff7811
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-11 20:34:18 +0900

    solve codefestival2014easya

---
+++ others/codefestival/codefestival2014/codefestival2014easy/codefestival2014easya.d
@@ -0,0 +1,11 @@
+import std;
+
+void main() {
+    int n;
+    readf("%d\n", n);
+
+    auto a = readln.chomp.split.to!(real[]);
+
+    real res = iota(n-1).map!(i => a[i+1] - a[i]).mean;
+    writefln("%.10f", res);
+}
