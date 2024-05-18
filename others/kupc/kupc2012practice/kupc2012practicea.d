a9f506f4f65fae42953cc15f5128c416f47b333b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-18 23:44:42 +0900

    solve kupc2012practicea

---
+++ others/kupc/kupc2012practice/kupc2012practicea.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    long m, n;
+    readf("%d %d\n", m, n);
+
+    long res = n + 1;
+    if (m == 1) res = n + 2;
+    if (m == 2) res = n * 2 + 3;
+    if (m == 3) res = 2 ^^ (n + 3) - 3;
+
+    res.writeln;
+}
