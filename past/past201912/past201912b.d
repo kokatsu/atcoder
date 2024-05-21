3d5f8a6f3feeb4875c4d3f112ad9c8f41eb5188f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-22 02:44:11 +0900

    solve past201912b

---
+++ past/past201912/past201912b.d
@@ -0,0 +1,21 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = new int[N];
+    foreach (i; 0 .. N) readf("%d\n", A[i]);
+
+    foreach (i; 1 .. N) {
+        if (A[i] == A[i-1]) {
+            "stay".writeln;
+        }
+        else if (A[i] < A[i-1]) {
+            writeln("down ", A[i-1] - A[i]);
+        }
+        else {
+            writeln("up ", A[i] - A[i-1]);
+        }
+    }
+}
