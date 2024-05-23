dc188b0b986a7fba35a8ec63a2efbf2351f66aeb
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-24 00:38:43 +0900

    solve past201912d

---
+++ past/past201912/past201912d.d
@@ -0,0 +1,29 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto cnts = new int[](N+1);
+    cnts[0] = 1;
+
+    int T;
+    foreach (_; 0 .. N) {
+        int A;
+        readf("%d\n", A);
+
+        ++cnts[A];
+
+        if (cnts[A] > 1) {
+            T = A;
+        }
+    }
+
+    string res = "Correct";
+    if (T > 0) {
+        auto F = cnts.minIndex;
+        res = format("%d %d", T, F);
+    }
+
+    res.writeln;
+}
