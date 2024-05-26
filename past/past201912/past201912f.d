21169c3b20bea602f1229fb04f89f65090000127
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-27 00:36:52 +0900

    solve past201912f

---
+++ past/past201912/past201912f.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    auto words = S.splitter!(Yes.keepSeparators)(regex(r"[A-Z][a-z]*[A-Z]"))
+        .filter!(x => !x.empty)
+        .array;
+
+    words.sort!((a, b) => a.toUpper < b.toUpper);
+
+    auto res = words.join;
+    res.writeln;
+}
