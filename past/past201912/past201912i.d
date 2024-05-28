4f26fe1cd0845a4c607b49df725c1a4fa99be84a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-29 01:47:50 +0900

    solve past201912i

---
+++ past/past201912/past201912i.d
@@ -0,0 +1,28 @@
+import std;
+
+void main() {
+    long N, M;
+    readf("%d %d\n", N, M);
+
+    auto S = new string[](M);
+    auto C = new long[](M);
+    foreach (i; 0 .. M) readf("%s %d\n", S[i], C[i]);
+
+    long L = 1 << N;
+    auto dp = new long[](L);
+    dp[1..L] = long.max / 2;
+    foreach (i; 0 .. M) {
+        long pos;
+
+        foreach (j, x; S[i]) {
+            if (x == 'Y') pos |= 1 << j;
+        }
+
+        foreach (j; 0 .. L) {
+            dp[j|pos] = min(dp[j|pos], dp[j]+C[i]);
+        }
+    }
+
+    long res = dp[L-1] < long.max / 2 ? dp[L-1] : -1;
+    res.writeln;
+}
