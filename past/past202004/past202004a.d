67d2cbed6f0132dc77bd954dbfbd01c8725f704d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-31 02:23:54 +0900

    solve past202004a

---
+++ past/past202004/past202004a.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    string S, T;
+    readf("%s %s\n", S, T);
+
+    int floor(string str) {
+        if (str[0] == 'B') {
+            return -(str[1] - '0');
+        }
+        return str[0] - '1';
+    }
+
+    int res = abs(floor(S)-floor(T));
+    res.writeln;
+}
