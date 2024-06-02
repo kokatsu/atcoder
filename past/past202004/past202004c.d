2525951805a2c0008ea2c699896fb109a35e3f25
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-06-02 23:50:10 +0900

    solve past202004c

---
+++ past/past202004/past202004c.d
@@ -0,0 +1,19 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto S = new dchar[][](N);
+    foreach (i; 0 .. N) S[i] = readln.chomp.to!(dchar[]);
+
+    foreach_reverse (i; 0 .. N-1) {
+        foreach (j; N-i-1 .. N+i) {
+            if (S[i+1][j-1..j+2].canFind('X')) {
+                S[i][j] = 'X';
+            }
+        }
+    }
+
+    writefln("%(%-(%s%)\n%)", S);
+}
