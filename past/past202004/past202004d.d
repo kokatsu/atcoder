a62b2bc56b38f1e983f7eb490cc46812ad56edb5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-06-03 22:53:16 +0900

    solve past202004d

---
+++ past/past202004/past202004d.d
@@ -0,0 +1,28 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    auto L = min(3, S.length);
+    string C = lowercase ~ ".";
+    int res;
+
+    void f(string re = "", int len = 0) {
+        if (len >= L) {
+            return;
+        }
+
+        foreach (x; C) {
+            string t = re ~ x;
+            if (S.matchFirst(t)) {
+                ++res;
+                f(t, len+1);
+            }
+        }
+    }
+
+    f();
+
+    res.writeln;
+}
