22c8cfe9acbfedf0b10b9fe2859e573eb017a619
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-06-05 01:25:43 +0900

    solve past202004f

---
+++ past/past202004/past202004f.d
@@ -0,0 +1,30 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto T = new int[][](N);
+    foreach (_; 0 .. N) {
+        int A, B;
+        readf("%d %d\n", A, B);
+
+        T[A-1] ~= B;
+    }
+
+    auto heap = new BinaryHeap!(Array!int, "a < b");
+
+    auto res = new int[](N);
+    foreach (i; 0 .. N) {
+        if (i > 0) {
+            res[i] += res[i-1];
+        }
+
+        foreach (x; T[i]) heap.insert(x);
+
+        res[i] += heap.front;
+        heap.popFront;
+    }
+
+    writefln("%(%d\n%)", res);
+}
