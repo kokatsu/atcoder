f9d529f95ec6a1c669444e6fe06846614ff9f69f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-06-10 02:51:39 +0900

    solve past202005c

---
+++ past/past202005/past202005c.d
@@ -0,0 +1,19 @@
+import std;
+
+enum long L = 10L ^^ 9;
+
+void main() {
+    long A, R, N;
+    readf("%d %d %d\n", A, R, N);
+
+    long M = min(N-1, 32);
+    foreach (_; 0 .. M) {
+        A *= R;
+        if (A > L) break;
+    }
+
+    string res = "large";
+    if (A <= L) res = A.to!string;
+
+    res.writeln;
+}
