685ec6af2cc0906df3b6d4dd05736535c38a58e1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-06-09 23:45:44 +0900

    solve past202005b

---
+++ past/past202005/past202005b.d
@@ -0,0 +1,27 @@
+import std;
+
+void main() {
+    int N, M, Q;
+    readf("%d %d %d\n", N, M, Q);
+
+    auto C = new int[](M);
+    C[] = N;
+    auto S = new bool[][](N, M);
+    foreach (_; 0 .. Q) {
+        auto P = readln.chomp.split.to!(int[]);
+
+        if (P[0] == 1) {
+            int score;
+            foreach (i; 0 .. M) {
+                if (S[P[1]-1][i]) {
+                    score += C[i];
+                }
+            }
+            score.writeln;
+        }
+        else {
+            --C[P[2]-1];
+            S[P[1]-1][P[2]-1] = true;
+        }
+    }
+}
