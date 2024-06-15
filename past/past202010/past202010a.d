4b74a8fd823029dffd531f72c09e74b90e4a2b41
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-06-15 23:59:58 +0900

    solve past202010a

---
+++ past/past202010/past202010a.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int A, B, C;
+    readf("%d %d %d\n", A, B, C);
+
+    int num = A + B + C - max(A, B, C) - min(A, B, C);
+
+    string res = "A";
+    if (num == B) res = "B";
+    if (num == C) res = "C";
+
+    res.writeln;
+}
