6c130c5c31611b3a76797a1ed8f4600d10bae7aa
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-06-22 21:06:21 +0900

    solve abc359b

---
+++ abc/abc351_360/abc359/abc359b.d
@@ -0,0 +1,15 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+
+    int res;
+    foreach (i; 1 .. N*2-1) {
+        if (A[i-1] == A[i+1]) ++res;
+    }
+
+    res.writeln;
+}
