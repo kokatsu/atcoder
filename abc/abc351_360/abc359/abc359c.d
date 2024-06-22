bf59ed893f7042c2ff094d981ce5ef047770b7d5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-06-22 22:32:57 +0900

    solve abc359c

---
+++ abc/abc351_360/abc359/abc359c.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    long Sx, Sy, Tx, Ty;
+    readf("%d %d\n%d %d\n", Sx, Sy, Tx, Ty);
+
+    long dy = abs(Sy-Ty);
+    if (Sx % 2 == Sy % 2) ++Sx;
+    if (Tx % 2 == Ty % 2) ++Tx;
+    long tx = max(0, abs(Sx-Tx)-dy);
+
+    long res = tx / 2 + dy;
+    res.writeln;
+}
