f69b01c36ca195bdfb25249403ace860cbc24272
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-07-03 00:27:05 +0900

    solve past202212e

---
+++ past/past202212/past202212e.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    int cnt;
+    bool isOk = true;
+    foreach (s; S) {
+        if (s == '(') ++cnt;
+        else --cnt;
+
+        isOk &= (cnt >= 0);
+    }
+
+    isOk &= (cnt == 0);
+
+    string res = isOk ? "Yes" : "No";
+    res.writeln;
+}
