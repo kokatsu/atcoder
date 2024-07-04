e029db47b307f02066d261064f9b50eda3cee4eb
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-07-05 02:25:57 +0900

    solve past202212g

---
+++ past/past202212/past202212g.d
@@ -0,0 +1,17 @@
+import std;
+
+void main() {
+    long N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(long[]);
+
+    long L = A[0], R = A[0];
+    foreach (i; 1 .. N) {
+        if (L < R) L = R;
+        R = (R + A[i] >= A[i] ? R + A[i] : A[i]);
+    }
+
+    long res = max(L, R);
+    res.writeln;
+}
