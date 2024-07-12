77138ce8f1bc4da541834349831830db50e37f31
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-07-13 02:59:39 +0900

    solve past202104c

---
+++ past/past202104/past202104c.d
@@ -0,0 +1,29 @@
+import std;
+
+void main() {
+    int N, M;
+    readf("%d %d\n", N, M);
+
+    auto table = new bool[][](N, M);
+    foreach (i; 0 .. N) {
+        auto A = readln.chomp.split.to!(int[]);
+
+        foreach (a; A[1..$]) {
+            table[i][a-1] = true;
+        }
+    }
+
+    int P, Q;
+    readf("%d %d\n", P, Q);
+
+    auto B = readln.chomp.split.to!(int[]);
+
+    int res;
+    foreach (i; 0 .. N) {
+        if (B.filter!(b => table[i][b-1]).array.length >= Q) {
+            ++res;
+        }
+    }
+
+    res.writeln;
+}
