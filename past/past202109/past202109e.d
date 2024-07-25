9a9478bbb7dd561a605cb802597963649653b9c4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-07-26 01:17:45 +0900

    solve past202109e

---
+++ past/past202109/past202109e.d
@@ -0,0 +1,25 @@
+import std;
+
+void main() {
+    long N, K;
+    readf("%d %d\n", N, K);
+
+    auto c = readln.chomp.split.to!(long[]);
+    auto p = readln.chomp.split.to!(long[]);
+
+    long[long] dict;
+    foreach (x, y; zip(c, p)) {
+        if (x !in dict) dict[x] = long.max;
+        dict[x] = dict[x].min(y);
+    }
+
+    auto v = dict.values;
+
+    long res = -1;
+    if (v.length >= K) {
+        v.sort;
+        res = v[0..K].sum;
+    }
+
+    res.writeln;
+}
