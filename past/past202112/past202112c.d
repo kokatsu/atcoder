64e3e4ab9832b5d33d79ededcfc97729d1a4ce97
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-07-31 02:00:35 +0900

    solve past202112c

---
+++ past/past202112/past202112c.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto res = new int[](6);
+
+    foreach (i; 0 .. N) {
+        dchar P;
+        string V;
+        readf("%c %s\n", P, V);
+
+        if (V == "AC" && res[P-'A'] == 0) {
+            res[P-'A'] = i + 1;
+        }
+    }
+
+    writefln("%(%s\n%)", res);
+}
