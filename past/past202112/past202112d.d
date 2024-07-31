3c9c0bbe0b499e8dc467c6d583326b1c4b125696
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-08-01 02:30:56 +0900

    solve past202112d

---
+++ past/past202112/past202112d.d
@@ -0,0 +1,42 @@
+import std;
+
+struct Student {
+    int id, math, english;
+
+    int opCmp(const Student that) const {
+        int a = this.math + this.english;
+        int b = that.math + that.english;
+        if (a == b) {
+            if (this.math == that.math) {
+                return that.id - this.id;
+            }
+            else {
+                return this.math - that.math;
+            }
+        }
+        else {
+            return a - b;
+        }
+    }
+
+    string toString() const @safe pure nothrow {
+        return this.id.to!string;
+    }
+}
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto A = readln.chomp.split.to!(int[]);
+    auto B = readln.chomp.split.to!(int[]);
+
+    auto students = new Student[](N);
+    foreach (i; 0 .. N) {
+        students[i] = Student(i+1, A[i], B[i]);
+    }
+
+    students.sort!"a > b";
+
+    writefln("%(%s %)", students);
+}
