895b8028d6942e707371808ac6db6b6a10508e9f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-08-05 23:48:21 +0900

    solve past202203c

---
+++ past/past202203/past202203c.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    string alpha;
+    readf("%s\n", alpha);
+
+    size_t L = alpha.length;
+    size_t D = L / 3, R = L % 3, T = (R == 0 ? D - 1 : D);
+    size_t N = L - T * 3;
+
+    string res = alpha[0..N] ~ to!dchar('a'+T-1).to!string;
+    res.writeln;
+}
