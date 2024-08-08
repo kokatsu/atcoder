f1e414135a5d13da26ead76b5a28f62dfcb59cbe
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-08-09 01:17:35 +0900

    solve past202206a

---
+++ past/past202206/past202206a.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int X, A, B, C;
+    readf("%d %d %d %d\n", X, A, B, C);
+
+    int hare = (A * C + X) * B, tortoise = A * X;
+
+    string res = "Tie";
+    if (hare < tortoise) res = "Hare";
+    if (hare > tortoise) res = "Tortoise";
+
+    res.writeln;
+}
