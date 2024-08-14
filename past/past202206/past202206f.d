71b2f053156d4175e5054d5e67247f6c4802bf9e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-08-15 01:19:15 +0900

    solve past202206f

---
+++ past/past202206/past202206f.d
@@ -0,0 +1,30 @@
+import std;
+
+void main() {
+    int H, W;
+    readf("%d %d\n", H, W);
+
+    auto S = new int[][](H, W);
+    foreach (i; 0 .. H) {
+        S[i] = readln.chomp.split.to!(int[]);
+    }
+
+    int N;
+    readf("%d\n", N);
+
+    foreach (_; 0 .. N) {
+        int r, c;
+        readf("%d %d\n", r, c);
+
+        --c;
+
+        foreach_reverse (i; 0 .. r) {
+            S[i][c] = 0;
+            if (i > 0) {
+                S[i][c] = S[i-1][c];
+            }
+        }
+    }
+
+    writefln("%(%(%d %)\n%)", S);
+}
