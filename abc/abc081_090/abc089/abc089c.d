2b10d0ff9332bf691b3b5683aaf60b18aad00ac3
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-08-25 23:33:26 +0900

    solve abc089c

---
+++ abc/abc081_090/abc089/abc089c.d
@@ -0,0 +1,31 @@
+import std;
+
+enum dchar[] T = "MARCH".to!(dchar[]);
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto C = new long[](26);
+    foreach (_; 0 .. N) {
+        string S;
+        readf("%s\n", S);
+
+        ++C[S[0]-'A'];
+    }
+
+    bool[string] used;
+    long res;
+    foreach (p; T.permutations) {
+        auto q = p[0..3].array.dup;
+        q.sort;
+        string r = q.to!string;
+        if (r in used) {
+            continue;
+        }
+        used[r] = true;
+        res += C[q[0]-'A'] * C[q[1]-'A'] * C[q[2]-'A'];
+    }
+
+    res.writeln;
+}
