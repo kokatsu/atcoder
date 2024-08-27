df54c629e17b2216169fc79d162b056086824e92
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-08-28 00:37:51 +0900

    solve abc088c

---
+++ abc/abc081_090/abc088/abc088c.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    auto c = new int[][](3, 3);
+    foreach (i; 0 .. 3) {
+        c[i] = readln.chomp.split.to!(int[]);
+    }
+
+    bool isOk = true;
+    foreach (i; 1 .. 3) {
+        isOk &= 3.iota.map!(j => c[i-1][j] - c[i][j]).uniq.walkLength == 1;
+    }
+
+    string res = isOk ? "Yes" : "No";
+    res.writeln;
+}
