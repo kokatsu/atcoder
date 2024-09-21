bf27cb2d29fac7b4c8a4c622ce14c92291f87000
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-09-21 22:36:46 +0900

    solve abc372b

---
+++ abc/abc371_380/abc372/abc372b.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int M;
+    readf("%d\n", M);
+
+    string[] A;
+    foreach_reverse(i; 0 .. 11) {
+        int P = 3 ^^ i;
+        while(M >= P) {
+            M -= P;
+            A ~= i.to!string;
+        }
+    }
+
+    string res = A.length.to!string ~ "\n" ~ A.join(" ");
+    res.writeln;
+}
