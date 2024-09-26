94e89c44ed3b83fc611bfdd0cc33ffa8a5d6ac4e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-09-27 02:38:10 +0900

    solve abc069c

---
+++ abc/abc061_070/abc069/abc069c.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto a = readln.chomp.split.to!(int[]);
+
+    auto cnt4 = a.count!(x => x % 4 == 0);
+    auto cnt2 = a.count!(x => x % 4 != 0 && x % 2 == 0);
+
+    string res = (cnt4 >= min(N, N-cnt2+1) / 2) ? "Yes" : "No";
+    res.writeln;
+}
