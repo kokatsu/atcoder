7ce2b49019aca02d103cfab4843f0a031e68d0cc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-09-30 02:27:31 +0900

    solve abc067c

---
+++ abc/abc061_070/abc067/abc067c.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto a = readln.chomp.split.to!(long[]);
+
+    long l, r = a.sum;
+
+    long res = long.max;
+    foreach (i; 0 .. N-1) {
+        l += a[i], r -= a[i];
+        res = min(res, abs(l-r));
+    }
+
+    res.writeln;
+}
