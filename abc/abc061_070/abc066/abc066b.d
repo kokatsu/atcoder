0cbdcc8a85191e8dfa5c9060896e3d764f468ad5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-10-04 01:08:32 +0900

    solve abc066b

---
+++ abc/abc061_070/abc066/abc066b.d
@@ -0,0 +1,13 @@
+import std;
+
+void main() {
+    string S;
+    readf("%s\n", S);
+
+    int res = S.length.to!int - 2;
+    while (S[0..res/2] != S[res/2..res]) {
+        res -= 2;
+    }
+
+    res.writeln;
+}
