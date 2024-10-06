2304589c915ceecc2e853f92f04845a253856490
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-10-06 23:58:35 +0900

    solve abc064c

---
+++ abc/abc061_070/abc064/abc064c.d
@@ -0,0 +1,18 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto a = readln.chomp.split.to!(int[]);
+
+    int[] colors = new int[](9);
+    foreach (x; a) {
+        ++colors[min(x/400, 8)];
+    }
+
+    int m = colors[0..8].count!(x => x > 0).to!int;
+    int min = m > 0 ? m : 1;
+    int max = m + colors[8];
+    writeln(min, " ", max);
+}
