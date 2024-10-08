433a0db23754e50fc07e44c3cb959135d3f62862
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-10-09 02:10:41 +0900

    solve abc063c

---
+++ abc/abc061_070/abc063/abc063c.d
@@ -0,0 +1,24 @@
+import std;
+
+enum int L = 10_001;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto memo = new bool[](L);
+    memo[0] = true;
+    foreach (_; 0 .. N) {
+        int s;
+        readf("%d\n", s);
+
+        foreach_reverse (i; s .. L) {
+            memo[i] |= memo[i-s];
+        }
+    }
+
+    auto f = iota(L-1, -1, -1).filter!(i => i % 10 != 0 && memo[i]);
+
+    int res = f.empty ? 0 : f.front;
+    res.writeln;
+}
