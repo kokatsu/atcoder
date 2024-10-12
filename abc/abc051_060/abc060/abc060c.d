3e2c399110c724f7a8f85ca388babd461a8bc38c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-10-12 23:57:02 +0900

    solve abc060c

---
+++ abc/abc051_060/abc060/abc060c.d
@@ -0,0 +1,16 @@
+import std;
+
+void main() {
+    int N, T;
+    readf("%d %d\n", N, T);
+
+    auto t = readln.chomp.split.to!(int[]);
+
+    int res = T, pos;
+    foreach (x; t) {
+        res += min(T, x-pos);
+        pos = x;
+    }
+
+    res.writeln;
+}
