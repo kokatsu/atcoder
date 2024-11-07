15a62b40294b8645f12c278ce22687c9fe70f7b7
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-11-08 00:10:32 +0900

    solve abc041c

---
+++ abc/abc041_050/abc041/abc041c.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    auto a = readln.chomp.split.to!(int[]);
+
+    auto res = iota(1, N+1).array;
+
+    zip(a, res).sort!"a[0] > b[0]";
+
+    writefln("%(%d\n%)", res);
+}
