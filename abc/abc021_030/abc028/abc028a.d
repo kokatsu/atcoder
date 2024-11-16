c2039b6e3f67fb73c8ed96d7c9d70fcc7248a437
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-11-16 18:58:32 +0900

    solve abc028a

---
+++ abc/abc021_030/abc028/abc028a.d
@@ -0,0 +1,17 @@
+import std;
+
+string check(int x)
+in (0 <= x && x <= 100) {
+    if (x < 60) return "Bad";
+    if (x < 90) return "Good";
+    if (x < 100) return "Great";
+    return "Perfect";
+}
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    string res = check(N);
+    res.writeln;
+}
