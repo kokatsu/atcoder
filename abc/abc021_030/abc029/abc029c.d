849216d64a5d2e2049baaff4fbb3c40459871330
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-11-18 02:09:28 +0900

    solve abc029c

---
+++ abc/abc021_030/abc029/abc029c.d
@@ -0,0 +1,38 @@
+import std;
+
+string toTernary(int number, int digits) {
+    string result;
+
+    do {
+        result = (number % 3).to!string ~ result;
+        number /= 3;
+    } while (number > 0);
+
+    if (result.length < digits) {
+        result = replicate("0", digits-result.length) ~ result;
+    }
+
+    return result;
+}
+
+string makePassword(string ternary) {
+    string result;
+
+    foreach (t; ternary) {
+        if (t == '0') result ~= 'a';
+        else if (t == '1') result ~= 'b';
+        else result ~= 'c';
+    }
+
+    return result;
+}
+
+void main() {
+    int N;
+    readf("%d\n", N);
+
+    string[] list = iota(3^^N).map!(i => makePassword(toTernary(i, N))).array;
+
+    string res = format("%-(%s\n%)", list);
+    res.writeln;
+}
