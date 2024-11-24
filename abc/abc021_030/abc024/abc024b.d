cc2d315ac894d77e3a78580b9ab268ac43888cc4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-11-25 00:41:45 +0900

    solve abc024b

---
+++ abc/abc021_030/abc024/abc024b.d
@@ -0,0 +1,20 @@
+import std;
+
+void main() {
+    long N, T;
+    readf("%d %d\n", N, T);
+
+    long res = T, pre;
+    foreach (i; 0 .. N) {
+        int A;
+        readf("%d\n", A);
+
+        if (i > 0) {
+            res += T.min(A-pre);
+        }
+
+        pre = A;
+    }
+
+    res.writeln;
+}
