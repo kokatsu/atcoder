8ef2749e4ec110e76392a36529dc87af31a1ae27
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-12-13 00:12:28 +0900

    solve abc083a

---
+++ abc/abc081_090/abc083/abc083a.d
@@ -0,0 +1,21 @@
+import std;
+
+string libra(int x, int y) {
+    if (x > y) {
+        return "Left";
+    }
+    else if (x < y) {
+        return "Right";
+    }
+    else {
+        return "Balanced";
+    }
+}
+
+void main() {
+    int A, B, C, D;
+    readf("%d %d %d %d\n", A, B, C, D);
+
+    string res = libra(A+B, C+D);
+    res.writeln;
+}
