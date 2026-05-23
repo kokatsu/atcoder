7957e66fc85d510bcc91f19550a4d04b80f7e36f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2026-05-23 22:11:38 +0900

    solve abc459c

---
+++ abc/abc451_460/abc459/abc459c.d
@@ -0,0 +1,95 @@
+import std;
+
+void main() {
+    int N, Q;
+    readfln("%d %d", N, Q);
+
+    int[] blocks = new int[](N);
+    blocks[] = 1;
+    auto ft = new FenwickTree!int(10 ^^ 6);
+    ft.add(1, N);
+
+    int D = 1;
+    size_t[] R;
+    foreach (_; 0 .. Q) {
+        int q, x;
+        readfln("%d %d", q, x);
+
+        if (q == 1) {
+            --x;
+            ft.add(blocks[x], -1);
+            ++blocks[x];
+            ft.add(blocks[x], 1);
+            if (ft.sum(D) == 0) {
+                ++D;
+            }
+        }
+        else {
+            R ~= N - ft.sum(x + D - 1);
+        }
+    }
+
+    string res = format("%(%d\n%)", R);
+    res.writeln;
+}
+
+struct FenwickTree(T) if (isIntegral!T) {
+
+    /// Constructor
+    this(U)(U n) if (isIntegral!U) {
+        size = n.to!T + 1;
+        data.length = size;
+    }
+
+    /// Adds val to data[idx].
+    void add(U)(U idx, T val) if (isIntegral!U)
+    in (0 < idx && idx < size) {
+        U i = idx;
+        while (i < size) {
+            data[i] += val;
+            i += i & -i;
+        }
+    }
+
+    /// Returns sum(data[1], ..., data[idx]).
+    T sum(U)(U idx) if (isIntegral!U)
+    in (0 <= idx && idx < size) {
+        if (idx == 0) {
+            return 0;
+        }
+
+        T res;
+        U i = idx;
+        while (i > 0) {
+            res += data[i];
+            i -= i & -i;
+        }
+        return res;
+    }
+
+    /// Returns the smallest pos that satisfies sum(_tree[1], ..., _tree[pos]) >= x.
+    U lowerBound(U = T)(T x) {
+        import core.bitop : bsr;
+
+        if (x <= 0) {
+            return 0;
+        }
+
+        U pos, i = 1 << size.bsr;
+        while (i > 0) {
+            U t = pos + i;
+            if (t < size && data[t] < x) {
+                pos = t;
+                x -= data[t];
+            }
+            i >>= 1;
+        }
+        ++pos;
+
+        return pos;
+    }
+
+private:
+    T size;
+    T[] data;
+}
